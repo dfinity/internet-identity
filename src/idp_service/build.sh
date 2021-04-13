a74ad7532158ff7eac1b613eb0e0b52406e13c97
Dimitris Sarlis <dimitris@dfinity.org>
2021-04-13 10:58:37 +0200

    Setup ic-cdk-optimizer

---
+++ src/idp_service/build.sh
@@ -0,0 +1,17 @@
+IDP_DIR="$(dirname "$0")"
+
+cargo build --manifest-path "$IDP_DIR/Cargo.toml" --target wasm32-unknown-unknown --release
+
+cargo install ic-cdk-optimizer --root "$IDP_DIR"/../../target
+STATUS=$?
+
+if [ "$STATUS" -eq "0" ]; then
+      "$IDP_DIR"/../../target/bin/ic-cdk-optimizer \
+      "$IDP_DIR"/../../target/wasm32-unknown-unknown/release/idp_service.wasm \
+      -o "$IDP_DIR"/../../target/wasm32-unknown-unknown/release/idp_service.wasm
+
+  true
+else
+  echo Could not install ic-cdk-optimizer.
+  false
+fi
