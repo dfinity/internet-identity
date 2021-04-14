dfd232e5b07dd8edd22de0778a104d9660860118
Dimitris Sarlis <dimitris@dfinity.org>
2021-04-14 10:22:27 +0200

    Apply suggestions from code review
    Co-authored-by: Daniel Stefan Dietiker <stefan@dfinity.org>

--- src/idp_service/build.sh
+++ src/idp_service/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 IDP_DIR="$(dirname "$0")"
-
-cargo build --manifest-path "$IDP_DIR/Cargo.toml" --target wasm32-unknown-unknown --release
+TARGET="wasm32-unknown-unknown"
+cargo build --manifest-path "$IDP_DIR/Cargo.toml" --target $TARGET --release
 
 cargo install ic-cdk-optimizer --root "$IDP_DIR"/../../target
 STATUS=$?
 
 if [ "$STATUS" -eq "0" ]; then
       "$IDP_DIR"/../../target/bin/ic-cdk-optimizer \
-      "$IDP_DIR"/../../target/wasm32-unknown-unknown/release/idp_service.wasm \
-      -o "$IDP_DIR"/../../target/wasm32-unknown-unknown/release/idp_service.wasm
+      "$IDP_DIR/../../target/$TARGET/release/idp_service.wasm" \
+      -o "$IDP_DIR/../../target/$TARGET/release/idp_service.wasm"
 
   true
 else
