ce2c922ec3c96578b08ffd03d3d0c294a190fa1d
Kyle Peacock <kylpeacock@gmail.com>
2021-04-13 17:09:57 -0700

    authenticating actor methods

--- src/frontend/src/utils/actor.ts
+++ src/frontend/src/utils/actor.ts
@@ -4,6 +4,7 @@ import {
   canisterId as idp_canister_id,
 } from "dfx-generated/idp_service";
 import _SERVICE, { UserId, Alias, PublicKey, CredentialId } from "../typings";
+import { authenticate } from "./handleAuthentication";
 
 const agent = new HttpAgent();
 export const baseActor = Actor.createActor<_SERVICE>(idp_idl, {
@@ -16,36 +17,38 @@ export class IDPActor {
   constructor(overrideActor?: _SERVICE) {
     this.actor = overrideActor ?? baseActor;
   }
-  register = (
-    userId: UserId,
-    alias: Alias,
-    publicKey: PublicKey,
-    credentialId?: string
-  ) => {
+  register = async (userId: UserId, alias: Alias, credentialId?: string) => {
+    const identity = await authenticate();
+    const key = Array.from(new TextEncoder().encode(identity.publicKey));
     return this.actor.register(
       userId,
       alias,
-      publicKey,
+      key,
       credentialId ? [Array.from(new TextEncoder().encode(credentialId))] : []
     );
   };
-  add = (
-    userId: UserId,
-    alias: Alias,
-    publicKey: PublicKey,
-    credentialId?: string
-  ) => {
-    return this.actor.add(
-      userId,
-      alias,
-      publicKey,
-      credentialId ? [Array.from(new TextEncoder().encode(credentialId))] : []
-    );
+  add = async (userId: UserId, alias: Alias, credentialId?: string) => {
+    const identity = await authenticate();
+    const key = Array.from(new TextEncoder().encode(identity.publicKey));
+    return this.actor
+      .add(
+        userId,
+        alias,
+        key,
+        credentialId ? [Array.from(new TextEncoder().encode(credentialId))] : []
+      )
+      .then(async () => {
+        const update = await this.actor.lookup(userId);
+        debugger;
+      });
   };
-  remove = (userId: UserId, publicKey: PublicKey) => {
-    return this.actor.remove(userId, publicKey);
+  remove = async (userId: UserId) => {
+    const identity = await authenticate();
+    const key = Array.from(new TextEncoder().encode(identity.publicKey));
+    return this.actor.remove(userId, key);
   };
   lookup = (userId: UserId) => {
+    console.log(userId);
     return this.actor.lookup(userId);
   };
 }
