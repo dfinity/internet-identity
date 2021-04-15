7fd68174e6689f90f9d15291b03e5ef721cefee3
Christoph Hegemann <6189397+kritzcreek@users.noreply.github.com>
2021-04-15 15:17:37 +0200

    Generates and decodes a link carrying a new device identity (#45)
    * Generates and decodes a link carrying a new device identity
    
    * moves the userId input up one level
    
    This is because we need the user id both for linking a new device or
    recovering a login
    
    * Also encode the userID in the addDeviceLink
    
    * update documentation comment
    
    * remove description that seems out of place

--- src/frontend/src/flows/manageIdentities.ts
+++ src/frontend/src/flows/manageIdentities.ts
@@ -1,6 +1,32 @@
+import { BinaryBlob, blobFromHex, derBlobFromBlob, DerEncodedBlob } from "@dfinity/agent";
+
 export const initManageIdentities = () => {
   // TODO - Check alias for current identity, and populate #nameSpan
   // TODO - Lookup identities then render them based on the result.
   //   TODO - check if authenticated, handle login if needed
-  // TODO - check URL if user has pasted in an Add Identity link
+  // Check URL if user has pasted in an Add Identity link
+  const url = new URL(document.URL);
+  const newDevice = url.searchParams.get("device");
+  if (newDevice !== null) {
+    const parsedParams = parseNewDeviceParam(newDevice);
+    if (parsedParams !== null) {
+      const { userId, publicKey, rawId } = parsedParams;
+      console.log("Adding new device with:", parsedParams)
+      // TODO: Prompt the user for an alias and let them add the new device to their
+      // existing identity
+    }
+  }
 };
+
+const parseNewDeviceParam = (param: string): { userId: BigInt, publicKey: DerEncodedBlob, rawId: BinaryBlob } | null => {
+  const segments = param.split(";");
+  if (segments.length !== 3) {
+    // TODO: Decent error handling
+    console.error("This is not a valid pasted link");
+    return null
+  }
+  const userId = BigInt(segments[0]);
+  const publicKey = derBlobFromBlob(blobFromHex(segments[1]));
+  const rawId = blobFromHex(segments[2]);
+  return { userId, publicKey, rawId }
+}
