4c6ecde7173984fbe5cb41a2a0cf94205f57b8d4
Islam El-Ashi <islam.elashi@dfinity.org>
2021-04-15 13:30:55 +0200

    PR comments.

--- src/frontend/src/utils/idp_actor.ts
+++ src/frontend/src/utils/idp_actor.ts
@@ -19,24 +19,24 @@ export class IDPActor {
     this.actor = overrideActor ?? baseActor;
   }
   register = async (userId: UserId, alias: Alias, credentialId?: string) => {
-    console.log("Registering user " + userId + " with alias " + alias);
+    console.log(`register(user_id = ${userId}, alias: ${alias}`);
     const identity = await authenticate();
-    const key = Array.from(new TextEncoder().encode(identity.publicKey));
+    const publicKey = Array.from(identity.getPublicKey().toDer());
     return this.actor.register(
       userId,
       alias,
-      key,
+      publicKey,
       credentialId ? [Array.from(new TextEncoder().encode(credentialId))] : []
     );
   };
   add = async (userId: UserId, alias: Alias, credentialId?: string) => {
     const identity = await authenticate();
-    const key = Array.from(new TextEncoder().encode(identity.publicKey));
+    const publicKey = Array.from(identity.getPublicKey().toDer());
     return this.actor
       .add(
         userId,
         alias,
-        key,
+        publicKey,
         credentialId ? [Array.from(new TextEncoder().encode(credentialId))] : []
       )
       .then(async () => {
@@ -46,16 +46,16 @@ export class IDPActor {
   };
   remove = async (userId: UserId) => {
     const identity = await authenticate();
-    const key = Array.from(new TextEncoder().encode(identity.publicKey));
-    return this.actor.remove(userId, key);
+    const publicKey = Array.from(identity.getPublicKey().toDer());
+    return this.actor.remove(userId, publicKey);
   };
   lookup = (userId: UserId) => {
     console.log(userId);
     return this.actor.lookup(userId);
   };
   get_delegation = (userId: UserId, identity: WebAuthnIdentity) => {
-    const publicKey = Array.from(new TextEncoder().encode(identity.toJSON().publicKey));
-    console.log("get_delegation(user_id = " + userId + ", publicKey: " + publicKey);
+    const publicKey = Array.from(identity.getPublicKey().toDer());
+    console.log(`get_delegation(user_id = ${userId}, publicKey: ${publicKey}`);
     return this.actor.get_delegation(userId, publicKey);
   };
 }
