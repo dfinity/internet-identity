a07c0a72e1412485e99b276a8b3a19d97faeb714
Roman Kashitsyn <roman@dfinity.org>
2021-04-21 23:39:19 +0200

    fix hash size to be 32, apply review feedback

--- src/cubehash/src/lib.rs
+++ src/cubehash/src/lib.rs
@@ -3,10 +3,12 @@
 //! https://github.com/floodyberry/supercop/blob/master/crypto_hash/cubehash1632/simple/cubehash.c
 use std::num::Wrapping;
 
+pub const HASH_BYTES: usize = 32;
+
 const CUBEHASH_ROUNDS: u16 = 16;
 const CUBEHASH_BLOCKBYTES: u16 = 32;
 
-// CubeHash 160+16/32+160-512
+// CubeHash 160+16/32+160-256
 
 #[inline]
 fn rotate(a: Wrapping<u32>, b: usize) -> Wrapping<u32> {
@@ -58,32 +60,58 @@ fn transform(state: &mut [Wrapping<u32>; 32]) {
 
 pub struct CubeHash {
     state: [Wrapping<u32>; 32],
-    hash_bytes: u16,
     pos: u16,
 }
 
 impl CubeHash {
     /// Constructs a new CubeHash state that produces a hash of the
     /// specified length.
-    ///
-    /// Panics if hash_bytes > 64
-    pub fn new(hash_bytes: u16) -> Self {
-        assert!(hash_bytes <= 64);
-
-        let mut state = [Wrapping(0); 32];
-        state[0] = Wrapping(hash_bytes as u32);
-        state[1] = Wrapping(CUBEHASH_BLOCKBYTES as u32);
-        state[2] = Wrapping(CUBEHASH_ROUNDS as u32);
+    pub fn new() -> Self {
+        let mut state_t = [Wrapping(0); 32];
+        state_t[0] = Wrapping(HASH_BYTES as u32);
+        state_t[1] = Wrapping(CUBEHASH_BLOCKBYTES as u32);
+        state_t[2] = Wrapping(CUBEHASH_ROUNDS as u32);
 
         for _i in 0..10 {
-            transform(&mut state);
+            transform(&mut state_t);
         }
 
-        Self {
-            state,
-            hash_bytes,
-            pos: 0,
-        }
+        let state: [Wrapping<u32>; 32] = [
+            Wrapping(0xea2bd4b4),
+            Wrapping(0xccd6f29f),
+            Wrapping(0x63117e71),
+            Wrapping(0x35481eae),
+            Wrapping(0x22512d5b),
+            Wrapping(0xe5d94e63),
+            Wrapping(0x7e624131),
+            Wrapping(0xf4cc12be),
+            Wrapping(0xc2d0b696),
+            Wrapping(0x42af2070),
+            Wrapping(0xd0720c35),
+            Wrapping(0x3361da8c),
+            Wrapping(0x28cceca4),
+            Wrapping(0x8ef8ad83),
+            Wrapping(0x4680ac00),
+            Wrapping(0x40e5fbab),
+            Wrapping(0xd89041c3),
+            Wrapping(0x6107fbd5),
+            Wrapping(0x6c859d41),
+            Wrapping(0xf0b26679),
+            Wrapping(0x09392549),
+            Wrapping(0x5fa25603),
+            Wrapping(0x65c892fd),
+            Wrapping(0x93cb6285),
+            Wrapping(0x2af2b5ae),
+            Wrapping(0x9e4b4e60),
+            Wrapping(0x774abfdd),
+            Wrapping(0x85254725),
+            Wrapping(0x15815aeb),
+            Wrapping(0x4ab6aad6),
+            Wrapping(0x9cdaf8af),
+            Wrapping(0xd6032c0a),
+        ];
+        assert_eq!(state_t, state);
+        Self { state, pos: 0 }
     }
 
     pub fn update(&mut self, data: &[u8]) {
@@ -98,9 +126,7 @@ impl CubeHash {
         }
     }
 
-    pub fn finalize(mut self) -> Vec<u8> {
-        let mut buf = vec![0; self.hash_bytes as usize];
-
+    pub fn finalize(mut self) -> [u8; HASH_BYTES] {
         let u = Wrapping(128u32) << (8 * (self.pos % 4) as usize);
         self.state[(self.pos / 4) as usize] ^= u;
         transform(&mut self.state);
@@ -108,7 +134,9 @@ impl CubeHash {
         for _ in 0..10 {
             transform(&mut self.state);
         }
-        for i in 0..self.hash_bytes as usize {
+
+        let mut buf: [u8; HASH_BYTES] = [0; HASH_BYTES];
+        for i in 0..HASH_BYTES {
             buf[i] = (self.state[i / 4] >> (8 * (i % 4))).0 as u8;
         }
         buf
--- src/cubehash/src/test.rs
+++ src/cubehash/src/test.rs
@@ -1,18 +1,39 @@
 use super::*;
 
-fn hash64(data: &[u8]) -> String {
-    let mut h = CubeHash::new(64);
+fn hash(data: &[u8]) -> String {
+    let mut h = CubeHash::new();
     h.update(data);
     hex::encode(&h.finalize())
 }
 
+// Note:
+// The test vectors are mostly coming from https://en.wikipedia.org/wiki/CubeHash,
+// but they were also verified by running the reference C implementation.
+
+#[test]
+fn test_empty() {
+    assert_eq!(
+        hash(b""),
+        "44c6de3ac6c73c391bf0906cb7482600ec06b216c7c54a2a8688a6a42676577d".to_string()
+    );
+}
+
 #[test]
 fn test_hello() {
-    assert_eq!(hash64(b"hello"), "f7acd519f51a6caa5387ae730ed999c4c31766d8477e4e1eef4275e9df07dc4c08adc4b64c9dc8359d711020f78627a4d1bcfecadd28a5263c05faf75e96555a".to_string());
-    assert_eq!(hash64(b"Hello"), "dcc0503aae279a3c8c95fa1181d37c418783204e2e3048a081392fd61bace883a1f7c4c96b16b4060c42104f1ce45a622f1a9abaeb994beb107fed53a78f588c".to_string());
+    assert_eq!(
+        hash(b"hello"),
+        "fb638723f74a25864c5ffb1c3480a1e72178bd55337a4248340776aa46f46f10".to_string()
+    );
+    assert_eq!(
+        hash(b"Hello"),
+        "e712139e3b892f2f5fe52d0f30d78a0cb16b51b217da0e4acb103dd0856f2db0".to_string()
+    );
 }
 
 #[test]
 fn test_quick_brown_fox() {
-    assert_eq!(hash64(b"The quick brown fox jumps over the lazy dog"), "bdba44a28cd16b774bdf3c9511def1a2baf39d4ef98b92c27cf5e37beb8990b7cdb6575dae1a548330780810618b8a5c351c1368904db7ebdf8857d596083a86".to_string());
+    assert_eq!(
+        hash(b"The quick brown fox jumps over the lazy dog"),
+        "5151e251e348cbbfee46538651c06b138b10eeb71cf6ea6054d7ca5fec82eb79".to_string()
+    );
 }
