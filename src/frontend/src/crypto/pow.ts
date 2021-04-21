76c3ae9418a4465a1a971064151485593a51b5db
Islam El-Ashi <islam.elashi@dfinity.org>
2021-04-21 16:59:43 +0200

    PR comments.

--- src/frontend/src/crypto/pow.ts
+++ src/frontend/src/crypto/pow.ts
@@ -1,19 +1,17 @@
 import { ProofOfWork } from "../typings";
-import {
-  canisterId as idp_canister_id,
-} from "dfx-generated/idp_service";
 import CubeHash from "cubehash";
 import { Principal } from "@dfinity/agent";
 import bigUintLE from "biguintle";
 
+const IDP_CANISTER_ID: string = process.env.CANISTER_ID!;
 const DIFFICULTY = 2; // Number of leading bytes that must equal zero in the hash.
 const DOMAIN = "ic-proof-of-work";
-const CANISTER_ID_BLOB = Principal.from(idp_canister_id).toBlob();
+const CANISTER_ID_BLOB = Principal.from(IDP_CANISTER_ID).toBlob();
 const NONCE_OFFSET = DOMAIN.length + 1 /* domain + prefix */ + 8 /* timestamp length */;
 
 export default function(): ProofOfWork {
   console.time('PoW');
-  const timestamp = BigInt(Date.now());
+  const timestamp = BigInt(Date.now()) * BigInt(1000); // Timestamp in ns.
   let nonce = BigInt(0);
 
   let message = Buffer.concat([
