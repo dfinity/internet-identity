c349090fecd8d995d2478f9d620ac25528f7a0af
Dimitris Sarlis <dimitris@dfinity.org>
2021-04-22 14:31:46 +0200

    Review comments

--- src/idp_service/src/main.rs
+++ src/idp_service/src/main.rs
@@ -178,10 +178,6 @@ async fn add(user_number: UserNumber, device_data: DeviceData) {
     ensure_salt_set().await;
 
     STATE.with(|s| {
-        if s.storage.borrow().salt() == EMPTY_SALT {
-            trap("Salt is not set. Try calling init_salt() to set it.");
-        }
-
         let mut entries = s.storage.borrow().read(user_number).unwrap_or_else(|err| {
             trap(&format!(
                 "failed to read device data of user {}: {}",
@@ -223,10 +219,6 @@ async fn add(user_number: UserNumber, device_data: DeviceData) {
 async fn remove(user_number: UserNumber, device_key: DeviceKey) {
     ensure_salt_set().await;
     STATE.with(|s| {
-        if s.storage.borrow().salt() == EMPTY_SALT {
-            trap("Salt is not set. Try calling init_salt() to set it.");
-        }
-
         prune_expired_signatures(&mut s.sigs.borrow_mut());
 
         let mut entries = s.storage.borrow().read(user_number).unwrap_or_else(|err| {
@@ -268,10 +260,6 @@ async fn prepare_delegation(
     ensure_salt_set().await;
 
     STATE.with(|s| {
-        if s.storage.borrow().salt() == EMPTY_SALT {
-            trap("Salt is not set. Try calling init_salt() to set it.");
-        }
-
         let entries = s.storage.borrow().read(user_number).unwrap_or_else(|err| {
             trap(&format!(
                 "failed to read device data of user {}: {}",
@@ -600,6 +588,12 @@ async fn ensure_salt_set() {
     if salt == EMPTY_SALT {
         init_salt().await;
     }
+
+    STATE.with(|s| {
+        if s.storage.borrow().salt() == EMPTY_SALT {
+            trap("Salt is not set. Try calling init_salt() to set it");
+        }
+    });
 }
 
 fn main() {}
