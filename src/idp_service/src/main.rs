dfinity/internet-identity

commit 9142c5a5add568052603dcb32fff2f7277ca3b52

apply my own late feedback for #99 (#117)
* Use fixed size arrays instead of vectors when possible.
* Use Option<&Salt> instead of a magic value.

src/idp_service/src/main.rs
@@ -8,7 +8,8 @@ use idp_service::signature_map::SignatureMap;
 use serde::Serialize;
 use std::cell::RefCell;
 use std::collections::HashMap;
-use storage::{Storage, EMPTY_SALT};
+use std::convert::TryInto;
+use storage::{Salt, Storage};
 
 const fn secs_to_nanos(secs: u64) -> u64 {
     secs * 1_000_000_000
@@ -137,7 +138,7 @@ thread_local! {
 #[update]
 async fn init_salt() {
     STATE.with(|s| {
-        if s.storage.borrow().salt() != EMPTY_SALT {
+        if s.storage.borrow().salt().is_some() {
             trap("Salt already set");
         }
     });
@@ -146,10 +147,16 @@ async fn init_salt() {
         Ok((res,)) => res,
         Err((_, err)) => trap(&format!("failed to get salt: {}", err)),
     };
+    let salt: Salt = res[..].try_into().unwrap_or_else(|_| {
+        trap(&format!(
+            "expected raw randomness to be of length 32, got {}",
+            res.len()
+        ));
+    });
 
     STATE.with(|s| {
         let mut store = s.storage.borrow_mut();
-        store.update_salt(res); // update_salt() traps if salt has already been set
+        store.update_salt(salt); // update_salt() traps if salt has already been set
     });
 }
 
@@ -433,14 +440,13 @@ fn retrieve_data() {
 }
 
 fn calculate_seed(user_number: UserNumber, frontend: &FrontendHostname) -> Hash {
-    let salt = STATE.with(|s| s.storage.borrow().salt().to_vec());
-    if salt == EMPTY_SALT {
-        trap("Salt is not set. Try calling init_salt() to set it");
-    }
+    let salt = STATE
+        .with(|s| s.storage.borrow().salt().cloned())
+        .unwrap_or_else(|| trap("Salt is not set. Try calling init_salt() to set it"));
 
     let mut blob: Vec<u8> = vec![];
     blob.push(salt.len() as u8);
-    blob.extend(salt);
+    blob.extend_from_slice(&salt);
 
     let user_number_str = user_number.to_string();
     let user_number_blob = user_number_str.bytes();
@@ -651,13 +657,13 @@ fn check_proof_of_work(pow: &ProofOfWork, now: Timestamp) {
 
 // Checks if salt is empty and calls `init_salt` to set it.
 async fn ensure_salt_set() {
-    let salt = STATE.with(|s| s.storage.borrow().salt().to_vec());
-    if salt == EMPTY_SALT {
+    let salt = STATE.with(|s| s.storage.borrow().salt().cloned());
+    if salt.is_none() {
         init_salt().await;
     }
 
     STATE.with(|s| {
-        if s.storage.borrow().salt() == EMPTY_SALT {
+        if s.storage.borrow().salt().is_none() {
             trap("Salt is not set. Try calling init_salt() to set it");
         }
     });

src/idp_service/src/storage.rs
@@ -10,15 +10,17 @@ use std::marker::PhantomData;
 
 const HEADER_SIZE: u32 = 512;
 const DEFAULT_ENTRY_SIZE: u16 = 2048;
-pub const EMPTY_SALT: &'static [u8] = &[0; 32];
+const EMPTY_SALT: [u8; 32] = [0; 32];
 const WASM_PAGE_SIZE: u32 = 65536;
+
+pub type Salt = [u8; 32];
 
 /// Data type responsible for managing user data in stable memory.
 pub struct Storage<T> {
     num_users: u32,
     user_number_range: (UserNumber, UserNumber),
     entry_size: u16,
-    salt: Vec<u8>,
+    salt: Salt,
     _marker: PhantomData<T>,
 }
 
@@ -30,18 +32,22 @@ impl<T: candid::CandidType + serde::de::DeserializeOwned> Storage<T> {
             num_users: 0,
             user_number_range,
             entry_size: DEFAULT_ENTRY_SIZE,
-            salt: EMPTY_SALT.to_vec(),
+            salt: EMPTY_SALT,
             _marker: PhantomData,
         };
         storage
     }
 
-    pub fn salt(&self) -> &[u8] {
-        &self.salt
+    pub fn salt(&self) -> Option<&Salt> {
+        if self.salt == EMPTY_SALT {
+            None
+        } else {
+            Some(&self.salt)
+        }
     }
 
-    pub fn update_salt(&mut self, salt: Vec<u8>) {
-        if self.salt != EMPTY_SALT {
+    pub fn update_salt(&mut self, salt: Salt) {
+        if self.salt().is_some() {
             trap("Attempted to set the salt twice.");
         }
         self.salt = salt;
@@ -74,7 +80,9 @@ impl<T: candid::CandidType + serde::de::DeserializeOwned> Storage<T> {
         let id_range_lo = u64::from_le_bytes(buf[8..16].try_into().unwrap());
         let id_range_hi = u64::from_le_bytes(buf[16..24].try_into().unwrap());
         let entry_size = u16::from_le_bytes(buf[24..26].try_into().unwrap());
-        let salt = buf[26..58].to_vec();
+        let salt = buf[26..58].try_into().unwrap_or_else(|| {
+            trap("unreachable: failed to extract 32 byte array from 32 byte a slice")
+        });
         Some(Self {
             num_users,
             user_number_range: (id_range_lo, id_range_hi),

commit e1726219d9ba1ee4837f3d6326bd7f279f9fddd2

fix compile errors and warnings
* Fix a compile error introduced in #117
* Fix unused function warnings

src/idp_service/src/main.rs
@@ -64,7 +64,6 @@ enum GetDelegationResponse {
 }
 
 mod hash;
-mod nonce_cache;
 mod storage;
 
 #[derive(Clone, Debug, CandidType, Deserialize)]

src/idp_service/src/storage.rs
@@ -80,7 +80,7 @@ impl<T: candid::CandidType + serde::de::DeserializeOwned> Storage<T> {
         let id_range_lo = u64::from_le_bytes(buf[8..16].try_into().unwrap());
         let id_range_hi = u64::from_le_bytes(buf[16..24].try_into().unwrap());
         let entry_size = u16::from_le_bytes(buf[24..26].try_into().unwrap());
-        let salt = buf[26..58].try_into().unwrap_or_else(|| {
+        let salt = buf[26..58].try_into().unwrap_or_else(|_| {
             trap("unreachable: failed to extract 32 byte array from 32 byte a slice")
         });
         Some(Self {