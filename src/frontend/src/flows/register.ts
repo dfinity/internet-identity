688f461ad37da2022852152cb69cdc97847dc41a
Kyle Peacock <kylpeacock@gmail.com>
2021-04-26 14:48:54 -0700

    fix: register and add device flows working for ios

--- src/frontend/src/flows/register.ts
+++ src/frontend/src/flows/register.ts
@@ -12,45 +12,48 @@ const pageContent = html`
     <h1>Register your new Internet Identity</h1>
     <form id="registerForm">
       <p>What should we call this device?</p>
-      <input id="registerAlias" placeholder="Device name">
+      <input id="registerAlias" placeholder="Device name" />
       <button type="submit" class="primary">Register</button>
       <button id="registerCancel" type="button">Cancel</button>
     </form>
   </div>
-  `;
+`;
 
 export const register = async (): Promise<LoginResult | null> => {
   const container = document.getElementById("pageContent") as HTMLElement;
   render(pageContent, container);
-  return init()
-}
+  return init();
+};
 
-const init = (): Promise<LoginResult | null> => new Promise(resolve => {
-  const form = document.getElementById("registerForm") as HTMLFormElement;
-  const registerCancel = document.getElementById("registerCancel") as HTMLButtonElement;
-  
-  registerCancel.onclick = () => resolve(null)
-  form.onsubmit = async (e) => {
-    // Enter pending state
-    e.preventDefault();
-    e.stopPropagation();
+const init = (): Promise<LoginResult | null> =>
+  new Promise((resolve) => {
+    const form = document.getElementById("registerForm") as HTMLFormElement;
+    const registerCancel = document.getElementById(
+      "registerCancel"
+    ) as HTMLButtonElement;
 
-    // Read values from inputs
-    const registerAlias = form.querySelector(
-      "#registerAlias"
-    ) as HTMLInputElement;
+    registerCancel.onclick = () => resolve(null);
+    form.onsubmit = async (e) => {
+      // Enter pending state
+      e.preventDefault();
+      e.stopPropagation();
 
-    const identity = await WebAuthnIdentity.create();
-    if (await confirmRegister()) {
-      // Send values through actor
-      const { userNumber, connection } = await withLoader(
-        async () => IDPActor.register(identity, registerAlias.value)
-      );
-      setUserNumber(userNumber);
-      await displayUserNumber(userNumber)
-      resolve({ tag: "ok", connection, userNumber })
-    } else {
-      resolve(null)
-    }
-  };
-})
+      // Read values from inputs
+      const registerAlias = form.querySelector(
+        "#registerAlias"
+      ) as HTMLInputElement;
+
+      const identity = await WebAuthnIdentity.create();
+      if (await confirmRegister()) {
+        // Send values through actor
+        const { userNumber, connection } = await withLoader(async () =>
+          IDPActor.register(identity, registerAlias.value)
+        );
+        setUserNumber(userNumber);
+        await displayUserNumber(userNumber);
+        resolve({ tag: "ok", connection, userNumber });
+      } else {
+        resolve(null);
+      }
+    };
+  });
