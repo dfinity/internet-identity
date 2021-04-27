4570854ecd3d5e5081fdd29c8007d660235994e3
Joachim Breitner <mail@joachim-breitner.de>
2021-04-27 11:51:46 +0200

    Selenium tests, automatic screenshots, test app CI (#156)
    * Add automated UI test for simple login and OAuth flow
    
    * Update package.log
    
    * Try to run selenium on CI
    
    * Add dfx to path
    
    * Test just one of the tests
    
    * Separate cargo cache key for this workflow
    
    * Update test to new ui element names
    
    * Disable darwin
    
    * Wait for regiser button
    
    * Make tests more robust by waiting up to three seconds after driver.get has finished.
    
    * Create screenshots with selenium
    
    These screenshots are generated by running the selenium tests, e.g. using
    
        npm test -- -t Screenshot
    
    That will override screenshots, but not delete files that are no longer
    generated by the test; in that case, run `rm -f screenshots/*.png` before
    running this script.
    
    We commit these screenshots so that changes to the UI can be reviewed nicely in
    the PR.
    
    At some point we might
     * make CI fail if the screenshots are not up-to-date
       (assuming they can be made reproducible on different machines, or else
       diffed liberaly)
     * add a bot that updates these automatically
    
    * Update selenium tests, test oauth using demo app
    
    * Try to set up CI
    
    * Try using npm install
    
    * Get more screenshots
    
    * More screenshots
    
    * Bump delay?
    
    * Patch usernumber out of screenshot
    
    * More wait times
    
    Co-authored-by: Franz-Stefan Preiss <franzstefan@dfinity.org>

---
+++ src/frontend/src/selenium.test.ts
@@ -0,0 +1,152 @@
+import { Builder, By, until, ThenableWebDriver, Key} from 'selenium-webdriver';
+import { Executor, Command } from 'selenium-webdriver/lib/command';
+import { Options as ChromeOptions } from 'selenium-webdriver/chrome';
+import { writeFile } from 'fs/promises';
+
+const IDP_SERVICE_URL = 'http://localhost:8000/?canisterId=rrkah-fqaaa-aaaaa-aaaaq-cai';
+const IDP_AUTH_URL = 'http://localhost:8000/authorize?canisterId=rrkah-fqaaa-aaaaa-aaaaq-cai';
+const DEMO_APP_URL = 'http://localhost:8080/';
+
+const DEVICE_NAME1 = 'Virtual WebAuthn device';
+
+test('Screenshots', async () => {
+    await run_in_browser_with_virtual_authenticator(async (driver) => {
+        await driver.get(IDP_SERVICE_URL);
+        await screenshot('00-welcome', driver);
+
+        await driver.wait(until.elementLocated(By.id('registerButton')), 3_000).click();
+        await screenshot('01-register', driver);
+
+        await driver.findElement(By.id('registerAlias')).sendKeys('Virtual WebAuthn device', Key.RETURN);
+        // TODO: More reliable recognize this view
+        await driver.wait(until.elementLocated(By.xpath("//p[text()='Now confirm your security device one more time to register.']")), 5_000);
+        await screenshot('02-register-confirm', driver);
+
+        await driver.findElement(By.id('registerButton')).click();
+        let continueButton = await driver.wait(until.elementLocated(By.id('displayUserContinue')), 15_000);
+        let userNumberElem = await driver.findElement(By.className("highlightBox"));
+        let userNumber = await userNumberElem.getText();
+        // replace the user number for a reproducible screenshot
+        await driver.executeScript("arguments[0].innerText = arguments[1];", userNumberElem, '12345');
+        await screenshot('03-register-user-number', driver);
+
+        await continueButton.click();
+        // wait for device list to load
+        await driver.wait(until.elementLocated(By.xpath(`//span[string()='${DEVICE_NAME1}']`)), 3_000);
+        // replace the user number for a reproducible screenshot
+        let h3 = await driver.wait(until.elementLocated(By.xpath("//h3[string()='Your User Number is "+userNumber+"']")), 15_000);
+        await driver.executeScript("arguments[0].innerText = arguments[1];", h3, 'Your User Number is 12345');
+        await screenshot('04-main', driver);
+
+        await driver.findElement(By.id('logoutButton')).click();
+        await driver.wait(until.elementLocated(By.id('registerButton')), 3_000);
+        // should be at welcome again, no point taking screenshot
+
+        await driver.findElement(By.id('registerUserNumber'), 3_000).sendKeys(userNumber);
+        await driver.findElement(By.id('loginButton')).click();
+        // wait for device list to load
+        await driver.wait(until.elementLocated(By.xpath(`//span[string()='${DEVICE_NAME1}']`)), 3_000);
+        // should be logged in again, no point taking screenshot
+
+        await driver.get(IDP_SERVICE_URL);
+        let userNumberElem2 = await driver.findElement(By.className("highlightBox"));
+        let userNumber2 = await userNumberElem2.getText();
+        // replace the user number for a reproducible screenshot
+        await driver.executeScript("arguments[0].innerText = arguments[1];", userNumberElem2, '12345');
+        expect(userNumber2).toBe(userNumber);
+        await screenshot('05-welcome-back', driver);
+
+        await driver.findElement(By.id('login')).click();
+        // wait for device list to load
+        await driver.wait(until.elementLocated(By.xpath(`//span[string()='${DEVICE_NAME1}']`)), 3_000);
+        // should be logged in again, no point taking screenshot
+
+    })
+}, 300_000);
+
+
+test('Register new identity and login with it', async () => {
+    await run_in_browser_with_virtual_authenticator(async (driver) => {
+        await driver.get(IDP_SERVICE_URL);
+        let userNumber = await registerNewIdentity(driver);
+        await logout(driver);
+        await login(userNumber, driver);
+    })
+}, 300_000);
+
+test('Log into client application, after registration', async () => {
+    await run_in_browser_with_virtual_authenticator(async (driver) => {
+        await driver.get(DEMO_APP_URL);
+        await driver.findElement(By.id('idpUrl')).sendKeys(Key.CONTROL + "a");
+        await driver.findElement(By.id('idpUrl')).sendKeys(Key.DELETE);
+        await driver.findElement(By.id('idpUrl')).sendKeys(IDP_AUTH_URL);
+        await driver.findElement(By.id('signinBtn')).click();
+
+        let userNumber = await registerNewIdentity(driver);
+        await driver.findElement(By.xpath("//button[text()='Yes']")).click();
+
+        // check that we are indeed being redirected back
+        let principal = await driver.wait(until.elementLocated(By.id('principal')), 10_000).getText();
+        expect(principal).not.toBe('2vxsx-fae');
+        // TODO: Use a whoami service to check that loggin in works
+    })
+}, 30_000);
+
+async function run_in_browser_with_virtual_authenticator(test) {
+    const driver = new Builder().forBrowser('chrome')
+        .setChromeOptions(new ChromeOptions()
+        .headless() // hides the click show: uncomment to watch it
+        .windowSize({width: 1024, height: 768}))
+        .build();
+    try {
+        const session = await driver.getSession();
+        await addVirtualAuthenticator(driver.getExecutor(), session.getId());
+        await test(driver);
+    } catch (e) {
+        console.log(await driver.getPageSource());
+        throw e;
+    } finally {
+        await driver.quit();
+    }
+};
+
+async function registerNewIdentity(driver: ThenableWebDriver): Promise<string> {
+    await driver.wait(until.elementLocated(By.id('registerButton')), 5_000).click();
+    await driver.findElement(By.id('registerAlias')).sendKeys('Virtual WebAuthn device', Key.RETURN);
+    // TODO: More reliable recognize this view
+    await driver.wait(until.elementLocated(By.xpath("//p[text()='Now confirm your security device one more time to register.']")), 5_000);
+    await driver.findElement(By.id('registerButton')).click();
+
+    let continueButton = await driver.wait(until.elementLocated(By.id('displayUserContinue')), 15_000);
+    let userNumber = await driver.findElement(By.className("highlightBox")).getText();
+    await continueButton.click();
+    return userNumber;
+}
+
+async function logout(driver: ThenableWebDriver) {
+    await driver.findElement(By.id('logoutButton')).click();
+}
+
+async function login(userNumber: string, driver: ThenableWebDriver) {
+    await driver.findElement(By.id('registerUserNumber')).sendKeys(userNumber, Key.RETURN);
+    await driver.findElement(By.id('loginButton')).click();
+    await driver.wait(until.elementLocated(By.xpath("//h3[string()='Your User Number is "+userNumber+"']")), 15_000);
+}
+
+async function addVirtualAuthenticator(executor: Executor, sessionId: string) {
+    executor.defineCommand("AddVirtualAuthenticator", "POST", "/session/:sessionId/webauthn/authenticator");
+    const cmd = new Command('AddVirtualAuthenticator');
+    cmd.setParameter('protocol', 'ctap2');
+    cmd.setParameter('transport', 'usb');
+    cmd.setParameter('hasResidentKey', true);
+    cmd.setParameter('isUserConsenting', true);
+    cmd.setParameter('sessionId', sessionId);
+    await executor.execute(cmd);
+}
+
+async function screenshot(name : string, driver: ThenableWebDriver) {
+    let image = await driver.takeScreenshot();
+    // writing to a subdirectory has the nice property that it fails if
+    // this is run in the wrong directory
+    await writeFile(`screenshots/${name}.png`, image, 'base64');
+}
