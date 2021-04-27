0c22db61ac76ed299a3a3601b66cec2d5496aacc
Christoph <christoph.hegemann1337@gmail.com>
2021-04-27 13:56:15 +0200

    optionally display a QR code for device link

--- src/frontend/src/flows/displayAddDeviceLink.ts
+++ src/frontend/src/flows/displayAddDeviceLink.ts
@@ -1,6 +1,7 @@
 import { html, render } from "lit-html";
 import ClipboardJS from "clipboard";
 import { checkmarkIcon } from "../components/icons";
+import kjua from "kjua";
 
 const pageContent = (link: string) => html`
   <style>
@@ -23,19 +24,54 @@ const pageContent = (link: string) => html`
       <input id="linkText" value="${link}" readonly>
       <button id="linkCopy" data-clipboard-target="#linkText">Copy</button>
     </div>
+    <button id="showQR">Display as QR Code</button>
   </div>
   `;
+
+const qrContent = (qrcode) => html`
+  <style>
+    #qrBox {
+        display: flex;
+        justify-content: center;
+        margin: 1rem 0;
+      }
+  </style>
+  <div class="container">
+    <h1>New device</h1>
+    <p>Scan the QR Code below from your authenticated device. This page will automatically update when you have succeeded.</p>
+    <label>QR Code</label>
+    <div id="qrBox">
+      ${qrcode}
+    </div>
+    <button id="showURL">Display URL</button>
+  </div>
+`
 
 export const displayAddDeviceLink = (link: string) => {
   const container = document.getElementById("pageContent") as HTMLElement;
   render(pageContent(link), container);
-  init()
+  init(link)
 }
 
-const init = () => {
+const displayAddDeviceQR = (link: string) => {
+  const container = document.getElementById("pageContent") as HTMLElement;
+  var el = kjua({text: link, render: "svg"});
+  render(qrContent(el), container);
+  initQR(link) 
+}
+
+const init = (link: string) => {
   const linkCopy = document.getElementById("linkCopy") as HTMLButtonElement;
+  const showQR = document.getElementById("showQR") as HTMLButtonElement;
+
   new ClipboardJS(linkCopy).on('success', () => {
     const linkCopy = document.getElementById("linkCopy") as HTMLButtonElement;
     render(checkmarkIcon, linkCopy)
   })
+  showQR.onclick = () => displayAddDeviceQR(link)
 };
+
+const initQR = (link: string) => {
+  const showURL = document.getElementById("showURL") as HTMLButtonElement;
+  showURL.onclick = () => displayAddDeviceLink(link)
+}
