b538ba0f9bc4d9167e9d8c0d5c3e08131b9d1665
Joachim Breitner <mail@joachim-breitner.de>
2021-04-28 18:58:49 +0200

    Change function names a bit. More readable?

--- src/frontend/src/selenium.test.ts
+++ src/frontend/src/selenium.test.ts
@@ -10,16 +10,16 @@ import { writeFile } from 'fs/promises';
 
  - Assertions and actions, grouped by View
 
-   - nowOnX() assertions:
+   - on_X() assertions:
      These wait for a characteristic element of view `X`
      Some return useful data from view `X`
 
-   - onXY() actions:
-     These should be run on `X`, and they perform the action `Y`
+   - on_X_Y() actions:
+     These should be run on_ `X`, and they perform the action `Y`
      Most actions will go to a different view, but not all of them.
 
-   - onXFixup() actions:
-     These should be run on view `X`, and they replace variable data
+   - on_X_Fixup() actions:
+     These should be run on_ view `X`, and they replace variable data
      (mostly usernames) with fixed data for screenshots
 
  - Setup helpers (getting web driver, waiting for fonts to be loaded)
@@ -54,73 +54,73 @@ const DEVICE_NAME2 = 'Other WebAuthn device';
 
 // View: Welcome
 
-async function nowOnWelcome(driver: ThenableWebDriver) {
+async function on_Welcome(driver: ThenableWebDriver) {
     await driver.wait(until.elementLocated(By.id('registerUserNumber')), 3_000);
 }
 
-async function onWelcomeTypeUserNumber(user_number: string, driver: ThenableWebDriver) {
+async function on_Welcome_TypeUserNumber(user_number: string, driver: ThenableWebDriver) {
     await driver.findElement(By.id('registerUserNumber'), 3_000).sendKeys(user_number);
 }
 
-async function onWelcomeLogin(driver: ThenableWebDriver) {
+async function on_Welcome_Login(driver: ThenableWebDriver) {
     await driver.findElement(By.id('loginButton')).click();
 }
 
-async function onWelcomeRegister(driver: ThenableWebDriver) {
+async function on_Welcome_Register(driver: ThenableWebDriver) {
     await driver.findElement(By.id('registerButton')).click();
 }
 
-async function onWelcomeAddDevice(driver: ThenableWebDriver) {
+async function on_Welcome_AddDevice(driver: ThenableWebDriver) {
     await driver.findElement(By.id('addNewDeviceButton')).click();
 }
 
-// View: Register
+// View: _Register
 
-async function nowOnRegister(driver: ThenableWebDriver) {
+async function on_Register(driver: ThenableWebDriver) {
 }
 
-async function onRegisterTypeAliasEnter(alias : string, driver: ThenableWebDriver) {
+async function on_Register_TypeAliasEnter(alias : string, driver: ThenableWebDriver) {
     await driver.findElement(By.id('registerAlias')).sendKeys(alias, Key.RETURN);
 }
 
-// View: Register confirmation
+// View: _Register confirmation
 
-async function nowOnRegisterConfirm(driver: ThenableWebDriver) {
+async function on_RegisterConfirm(driver: ThenableWebDriver) {
     await driver.wait(until.elementLocated(By.id('confirmRegisterButton')), 5_000);
 }
 
-async function onRegisterConfirmConfirm(driver: ThenableWebDriver) {
+async function on_RegisterConfirm_Confirm(driver: ThenableWebDriver) {
     await driver.findElement(By.id('confirmRegisterButton')).click();
 }
 
-// View: Register Show Number
+// View: _Register Show Number
 
-async function nowOnRegisterShowNumber(driver: ThenableWebDriver) : Promise<string> {
-  await driver.wait(until.elementLocated(By.id('displayUserContinue')), 15_000);
+async function on_RegisterShowNumber(driver: ThenableWebDriver) : Promise<string> {
+  await driver.wait(until.elementLocated(By.id('displayUser_Continue')), 15_000);
   return await driver.findElement(By.className("highlightBox")).getText();
 }
 
-async function onRegisterShowNumberContinue(driver: ThenableWebDriver) {
-  await driver.findElement(By.id('displayUserContinue')).click();
+async function on_RegisterShowNumber_Continue(driver: ThenableWebDriver) {
+  await driver.findElement(By.id('displayUser_Continue')).click();
 }
 
-async function onRegisterShowNumberFixup(driver: ThenableWebDriver) {
+async function on_RegisterShowNumber_Fixup(driver: ThenableWebDriver) {
   const elem = await driver.findElement(By.className("highlightBox"));
   await driver.executeScript("arguments[0].innerText = arguments[1];", elem, '12345');
 }
 
 // View: Main view
 
-async function nowOnMain(device_name : string, driver : ThenableWebDriver) {
+async function on_Main(device_name : string, driver : ThenableWebDriver) {
     // wait for device list to load
     await driver.wait(until.elementLocated(By.xpath(`//span[string()='${device_name}']`)), 3_000);
 }
 
-async function onMainLogout(driver : ThenableWebDriver) {
+async function on_Main_Logout(driver : ThenableWebDriver) {
     await driver.findElement(By.id('logoutButton')).click();
 }
 
-async function onMainFixup(driver : ThenableWebDriver) {
+async function on_Main_Fixup(driver : ThenableWebDriver) {
     // replace the user number for a reproducible screenshot
     let elem = await driver.findElement(By.id('userNumberSpan'));
     await driver.executeScript("arguments[0].innerText = arguments[1];", elem, '12345');
@@ -128,73 +128,73 @@ async function onMainFixup(driver : ThenableWebDriver) {
 
 // View: Welcome back
 
-async function nowOnWelcomeBack(driver: ThenableWebDriver) : Promise<string> {
+async function on_WelcomeBack(driver: ThenableWebDriver) : Promise<string> {
     await driver.wait(until.elementLocated(By.id('loginDifferent')), 15_000);
     return await driver.findElement(By.className("highlightBox")).getText();
 }
 
-async function onWelcomeBackFixup(driver: ThenableWebDriver) {
+async function on_WelcomeBack_Fixup(driver: ThenableWebDriver) {
     const elem = await driver.findElement(By.className("highlightBox"));
     await driver.executeScript("arguments[0].innerText = arguments[1];", elem, '12345');
 }
 
-async function onWelcomeBackLogin(driver: ThenableWebDriver) {
+async function on_WelcomeBack_Login(driver: ThenableWebDriver) {
     await driver.findElement(By.id('login')).click();
 }
 
 // View: Add device
 
-async function nowOnAddDevice(driver: ThenableWebDriver): Promise<string> {
+async function on_AddDevice(driver: ThenableWebDriver): Promise<string> {
     return await driver.wait(until.elementLocated(By.id("linkText")), 3_000).getAttribute('value');
 }
 
-async function onAddDeviceFixup(driver : ThenableWebDriver) {
+async function on_AddDevice_Fixup(driver : ThenableWebDriver) {
     const elem = await driver.wait(until.elementLocated(By.id("linkText")), 3_000);
     await driver.executeScript("arguments[0].value = arguments[1];", elem, '(link removed from screenshot)');
 }
 
 // View: Add device confirm
 
-async function nowOnAddDeviceConfirm(driver: ThenableWebDriver) {
+async function on_AddDeviceConfirm(driver: ThenableWebDriver) {
     await driver.wait(until.elementLocated(By.id('addDevice')), 3_000);
 }
 
-async function onAddDeviceConfirmConfirm(driver: ThenableWebDriver) {
+async function on_AddDeviceConfirm_Confirm(driver: ThenableWebDriver) {
     await driver.findElement(By.id('addDevice')).click();
 }
 
-async function onAddDeviceConfirmFixup(driver : ThenableWebDriver) {
+async function on_AddDeviceConfirm_Fixup(driver : ThenableWebDriver) {
     const userNumberElem = await driver.findElement(By.className("highlightBox"));
     await driver.executeScript("arguments[0].innerText = arguments[1];", userNumberElem, '12345');
 }
 
 // View: Add device alias
 
-async function nowOnAddDeviceAlias(driver: ThenableWebDriver) {
-    await driver.wait(until.elementLocated(By.id('deviceAliasContinue')), 3_000);
+async function on_AddDeviceAlias(driver: ThenableWebDriver) {
+    await driver.wait(until.elementLocated(By.id('deviceAlias_Continue')), 3_000);
 }
 
-async function onAddDeviceAliasType(alias : string, driver: ThenableWebDriver) {
+async function on_AddDeviceAlias_Type(alias : string, driver: ThenableWebDriver) {
     await driver.findElement(By.id('deviceAlias')).sendKeys(alias);
 }
 
-async function onAddDeviceAliasContinue(driver: ThenableWebDriver) {
-    await driver.findElement(By.id('deviceAliasContinue')).click();
+async function on_AddDeviceAlias_Continue(driver: ThenableWebDriver) {
+    await driver.findElement(By.id('deviceAlias_Continue')).click();
 }
 
 // View: Add device success
 
-async function nowOnAddDeviceSuccess(driver: ThenableWebDriver) {
+async function on_AddDeviceSuccess(driver: ThenableWebDriver) {
     await driver.wait(until.elementLocated(By.id('manageDevicesButton')), 10_000);
 }
 
 // View: Authorize application
 
-async function nowOnAuthApp(driver: ThenableWebDriver) {
+async function on_AuthApp(driver: ThenableWebDriver) {
     await driver.wait(until.elementLocated(By.id('confirmRedirect')), 5_000);
 }
 
-async function onAuthAppConfirm(driver: ThenableWebDriver) {
+async function on_AuthApp_Confirm(driver: ThenableWebDriver) {
     await driver.findElement(By.id('confirmRedirect')).click();
 }
 
@@ -260,22 +260,22 @@ async function run_in_browser_with_virtual_authenticator(test) {
 */
 
 async function registerNewIdentity(driver: ThenableWebDriver): Promise<string> {
-    await nowOnWelcome(driver);
-    await onWelcomeRegister(driver);
-    await nowOnRegister(driver);
-    await onRegisterTypeAliasEnter(DEVICE_NAME1, driver);
-    await nowOnRegisterConfirm(driver);
-    await onRegisterConfirmConfirm(driver);
-    const userNumber = await nowOnRegisterShowNumber(driver);
-    await onRegisterShowNumberContinue(driver);
+    await on_Welcome(driver);
+    await on_Welcome_Register(driver);
+    await on_Register(driver);
+    await on_Register_TypeAliasEnter(DEVICE_NAME1, driver);
+    await on_RegisterConfirm(driver);
+    await on_RegisterConfirm_Confirm(driver);
+    const userNumber = await on_RegisterShowNumber(driver);
+    await on_RegisterShowNumber_Continue(driver);
     return userNumber;
 }
 
 async function login(userNumber: string, driver: ThenableWebDriver) {
-    await nowOnWelcome(driver);
-    await onWelcomeTypeUserNumber(userNumber, driver);
-    await onWelcomeLogin(driver);
-    await nowOnMain(DEVICE_NAME1, driver);
+    await on_Welcome(driver);
+    await on_Welcome_TypeUserNumber(userNumber, driver);
+    await on_Welcome_Login(driver);
+    await on_Main(DEVICE_NAME1, driver);
 }
 
 
@@ -283,12 +283,12 @@ async function login(userNumber: string, driver: ThenableWebDriver) {
 /*
 ## The actual tests
 */
-test('Register new identity and login with it', async () => {
+test('_Register new identity and login with it', async () => {
     await run_in_browser_with_virtual_authenticator(async (driver) => {
         await driver.get(IDP_SERVICE_URL);
         let userNumber = await registerNewIdentity(driver);
-        await nowOnMain(DEVICE_NAME1, driver);
-        await await onMainLogout(driver);
+        await on_Main(DEVICE_NAME1, driver);
+        await await on_Main_Logout(driver);
         await login(userNumber, driver);
     })
 }, 300_000);
@@ -302,8 +302,8 @@ test('Log into client application, after registration', async () => {
         await driver.findElement(By.id('signinBtn')).click();
 
         let userNumber = await registerNewIdentity(driver);
-        await nowOnAuthApp(driver);
-        await onAuthAppConfirm(driver);
+        await on_AuthApp(driver);
+        await on_AuthApp_Confirm(driver);
 
         // check that we are indeed being redirected back to demo app
         let principal = await driver.wait(until.elementLocated(By.id('principal')), 10_000).getText();
@@ -317,73 +317,73 @@ test('Screenshots', async () => {
         await driver.get(IDP_SERVICE_URL);
         await wait_for_fonts(driver);
 
-	await nowOnWelcome(driver);
+	await on_Welcome(driver);
         await screenshot('00-welcome', driver);
-	await onWelcomeRegister(driver);
-	await nowOnRegister(driver);
+	await on_Welcome_Register(driver);
+	await on_Register(driver);
         await screenshot('01-register', driver);
-        await onRegisterTypeAliasEnter(DEVICE_NAME1, driver);
-        await nowOnRegisterConfirm(driver);
+        await on_Register_TypeAliasEnter(DEVICE_NAME1, driver);
+        await on_RegisterConfirm(driver);
         await screenshot('02-register-confirm', driver);
-        await onRegisterConfirmConfirm(driver);
-        const userNumber = await nowOnRegisterShowNumber(driver);
-        await onRegisterShowNumberFixup(driver);
+        await on_RegisterConfirm_Confirm(driver);
+        const userNumber = await on_RegisterShowNumber(driver);
+        await on_RegisterShowNumber_Fixup(driver);
         await screenshot('03-register-user-number', driver);
-        await onRegisterShowNumberContinue(driver);
-        await nowOnMain(DEVICE_NAME1, driver);
-        await onMainFixup(driver);
+        await on_RegisterShowNumber_Continue(driver);
+        await on_Main(DEVICE_NAME1, driver);
+        await on_Main_Fixup(driver);
         await screenshot('04-main', driver);
-        await onMainLogout(driver);
-        await nowOnWelcome(driver); // no point taking screenshot
-        await onWelcomeTypeUserNumber(userNumber, driver);
-        await onWelcomeLogin(driver);
-        await nowOnMain(DEVICE_NAME1, driver);
+        await on_Main_Logout(driver);
+        await on_Welcome(driver); // no point taking screenshot
+        await on_Welcome_TypeUserNumber(userNumber, driver);
+        await on_Welcome_Login(driver);
+        await on_Main(DEVICE_NAME1, driver);
 
         await driver.get(IDP_SERVICE_URL);
-        const userNumber2 = await nowOnWelcomeBack(driver);
+        const userNumber2 = await on_WelcomeBack(driver);
         expect(userNumber2).toBe(userNumber);
-        await onWelcomeBackFixup(driver);
+        await on_WelcomeBack_Fixup(driver);
         await screenshot('05-welcome-back', driver);
-        await onWelcomeBackLogin(driver);
-        await nowOnMain(DEVICE_NAME1, driver);
+        await on_WelcomeBack_Login(driver);
+        await on_Main(DEVICE_NAME1, driver);
 
         // Now the link device flow, using a second browser
         await run_in_browser_with_virtual_authenticator (async (driver2) => {
             await driver2.get(IDP_SERVICE_URL);
-	    await nowOnWelcome(driver2);
-            await onWelcomeTypeUserNumber(userNumber, driver2);
-            await onWelcomeAddDevice(driver2);
-            const link = await nowOnAddDevice(driver2);
+	    await on_Welcome(driver2);
+            await on_Welcome_TypeUserNumber(userNumber, driver2);
+            await on_Welcome_AddDevice(driver2);
+            const link = await on_AddDevice(driver2);
             console.log('The add device link is', link);
-            await onAddDeviceFixup(driver2);
+            await on_AddDevice_Fixup(driver2);
             await screenshot('06-new-device', driver2);
 
             // Log in with previous browser again
             await driver.get('about:blank');
             await driver.get(link);
             await wait_for_fonts(driver);
-            await nowOnWelcomeBack(driver);
-            await onWelcomeBackFixup(driver);
+            await on_WelcomeBack(driver);
+            await on_WelcomeBack_Fixup(driver);
             await screenshot('07-new-device-login', driver);
-            await onWelcomeBackLogin(driver);
-            await nowOnAddDeviceConfirm(driver);
-            await onAddDeviceConfirmFixup(driver);
+            await on_WelcomeBack_Login(driver);
+            await on_AddDeviceConfirm(driver);
+            await on_AddDeviceConfirm_Fixup(driver);
             await screenshot('08-new-device-confirm', driver);
-            await onAddDeviceConfirmConfirm(driver);
-            await nowOnAddDeviceAlias(driver);
+            await on_AddDeviceConfirm_Confirm(driver);
+            await on_AddDeviceAlias(driver);
             await screenshot('09-new-device-alias', driver);
-            await onAddDeviceAliasType(DEVICE_NAME2, driver);
-            await onAddDeviceAliasContinue(driver);
-            await nowOnAddDeviceSuccess(driver);
+            await on_AddDeviceAlias_Type(DEVICE_NAME2, driver);
+            await on_AddDeviceAlias_Continue(driver);
+            await on_AddDeviceSuccess(driver);
             await screenshot('10-new-device-done', driver);
 
             // Back to other browser, should be a welcome view now
-            await nowOnWelcomeBack(driver2);
-            await onWelcomeBackFixup(driver2);
+            await on_WelcomeBack(driver2);
+            await on_WelcomeBack_Fixup(driver2);
             await screenshot('11-new-device-login', driver2);
-            await onWelcomeBackLogin(driver2);
-            await nowOnMain(DEVICE_NAME2, driver2);
-            await onMainFixup(driver2);
+            await on_WelcomeBack_Login(driver2);
+            await on_Main(DEVICE_NAME2, driver2);
+            await on_Main_Fixup(driver2);
             await screenshot('12-new-device-listed', driver2);
         })
     })
