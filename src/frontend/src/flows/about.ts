a432ec816da07d362b3a32e67d693c5af08941a1
Joachim Breitner <mail@joachim-breitner.de>
2021-04-30 10:00:38 +0200

    Apply suggestions from code review
    Co-authored-by: Christoph Hegemann <6189397+kritzcreek@users.noreply.github.com>

--- src/frontend/src/flows/about.ts
+++ src/frontend/src/flows/about.ts
@@ -10,13 +10,13 @@ const pageContent = html`
   <div class="container" id="about">
     <h1>About the Internet Identity</h1>
     <p>
-      The Internet Identity is the identity provider for the Internet Computer: A single service to log into many applications on the Internet Computer.
+      Internet Identity is the identity provider for the Internet Computer: A single service to log into many apps on the Internet Computer.
     </p>
     <p>
-      The Internet Identity works without passwords! Instead, you log in with your fingerprint, your face or with dedicated security tokens. You can add multiple devices and use the Internet Identity with any of them.
+      Internet Identity protects your privacy. It will create a different identity for each application that you log into.
     </p>
     <p>
-      The Internet Identity protects your privacy. You will be someone else to each application that you log into.
+      Internet Identity works without passwords! You log in with your fingerprint, your face, or with dedicated security tokens. You can add multiple devices and security tokens and use your identities with any of them.
     </p>
     <p>
       Learn more with the <a href="todo">Internet Identity introduction</a>, and if you want to use the Internet Identity in your application, head for the <a href="todo">Developer documentation</a>.
