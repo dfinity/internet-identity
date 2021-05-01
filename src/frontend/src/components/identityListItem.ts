1eca29d75dd298e218998463042f573428cdd83e
Christoph <christoph.hegemann1337@gmail.com>
2021-05-01 12:56:45 +0200

    styles the manage page according to Pim's designs

--- src/frontend/src/components/identityListItem.ts
+++
@@ -1,6 +0,0 @@
-export const identityListItem = (alias) => `<span>${alias}</span>
-<button title="Remove identity"><svg id="icon_close" data-name="icon/close" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
-<rect id="Rectangle_91" data-name="Rectangle 91" width="14" height="14" fill="none"></rect>
-<path id="Line" d="M0,0,9,9" transform="translate(2.5 2.5)" fill="none" stroke="gray" stroke-miterlimit="10" stroke-width="3"></path>
-<path id="Line-2" data-name="Line" d="M9,0,0,9" transform="translate(2.5 2.5)" fill="none" stroke="gray" stroke-miterlimit="10" stroke-width="3"></path>
-</svg></button>`;
