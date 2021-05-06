1a9529a77f533eb2a1b1c1dcc47688989849c52e
Kyle Peacock <kylpeacock@gmail.com>
2021-05-05 18:31:28 -0700

    displayUserNumber focus

--- src/frontend/src/flows/displayUserNumber.ts
+++ src/frontend/src/flows/displayUserNumber.ts
@@ -1,24 +1,34 @@
 import { html, render } from "lit-html";
+import { nextTick } from "process";
 
 const pageContent = (userNumber) => html`
   <div class="container">
     <h1>Congratulations!</h1>
-    <p>Please record your user number. You will need it later to use your Internet Identity or to register additional devices.</p>
+    <p>
+      Please record your user number. You will need it later to use your
+      Internet Identity or to register additional devices.
+    </p>
     <label>User Number:</label>
-    <div class="highlightBox">
-      ${userNumber}
-    </div>
+    <div class="highlightBox">${userNumber}</div>
     <button id="displayUserContinue" class="primary">Continue</button>
   </div>
-  `;
+`;
 
 export const displayUserNumber = async (userNumber: bigint): Promise<void> => {
   const container = document.getElementById("pageContent") as HTMLElement;
   render(pageContent(userNumber), container);
-  return init()
-}
+  await nextTick(() => {
+    (document.getElementById("displayUserContinue") as
+      | HTMLButtonElement
+      | undefined)?.focus();
+  });
+  return init();
+};
 
-const init = (): Promise<void> => new Promise(resolve => {
-  const displayUserContinue = document.getElementById("displayUserContinue") as HTMLButtonElement;
-  displayUserContinue.onclick = () => resolve()
-});
+const init = (): Promise<void> =>
+  new Promise((resolve) => {
+    const displayUserContinue = document.getElementById(
+      "displayUserContinue"
+    ) as HTMLButtonElement;
+    displayUserContinue.onclick = () => resolve();
+  });
