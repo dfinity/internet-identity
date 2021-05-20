ccc8480adfeb8d96496493acd9d40c247aaf806c
Islam El-Ashi <islam.elashi@dfinity.org>
2021-05-20 17:41:15 +0200

    Add English as explicit wordlist

--- src/frontend/src/crypto/mnemonic.ts
+++ src/frontend/src/crypto/mnemonic.ts
@@ -1,5 +1,5 @@
 import { blobFromUint32Array } from '@dfinity/agent';
-import { entropyToMnemonic } from 'bip39';
+import { entropyToMnemonic, wordlists } from 'bip39';
 
 /**
  * @returns A random BIP39 mnemonic with 256 bits of entropy.
@@ -7,5 +7,5 @@ import { entropyToMnemonic } from 'bip39';
 export function generateMnemonic(): string {
   const entropy = new Uint32Array(32);
   crypto.getRandomValues(entropy);
-  return entropyToMnemonic(blobFromUint32Array(entropy).toString('hex'));
+  return entropyToMnemonic(blobFromUint32Array(entropy).toString('hex'), wordlists.english);
 }
