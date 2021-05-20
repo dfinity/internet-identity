09410dc920712201cfa03ff6cc88ba9f6be2669a
Islam El-Ashi <islam.elashi@dfinity.org>
2021-05-20 14:18:17 +0200

    lint + package lock

--- src/frontend/test-setup.ts
+++ src/frontend/test-setup.ts
@@ -1,3 +1,5 @@
-export {}
-global.crypto = require('@trust/webcrypto');
-global.TextEncoder = require('text-encoding').TextEncoder;
+import crypto from "@trust/webcrypto";
+import textEncoding = require('text-encoding');
+
+global.crypto = crypto;
+global.TextEncoder = textEncoding.TextEncoder;
