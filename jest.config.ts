b44efd6387a4c1b86405933a15cff347a8ca7f75
Nicolas Mattia <nicolas@dfinity.org>
2021-08-13 11:45:23 +0200

    Add anchor tests

--- jest.config.ts
+++ jest.config.ts
@@ -8,5 +8,6 @@ export default {
   moduleNameMapper: {
     "dfx-generated/internet_identity": internet_identity,
   },
-  setupFiles: [`<rootDir>/src/frontend/test-setup.ts`]
+  setupFiles: [`<rootDir>/src/frontend/test-setup.ts`],
+
 };
