a7adc495cd3c360d9146cb58e37d871671e03af3
Nicolas Mattia <nicolas@dfinity.org>
2021-08-17 11:51:13 +0200

    Document code

--- src/internet_identity/build.rs
+++ src/internet_identity/build.rs
@@ -22,6 +22,8 @@ fn main() -> Result<(), String> {
     let assets_module_path = Path::new(&out_dir).join("assets.rs");
     let asset_rel_paths = [
         ("/", "../../dist/index.html", ContentEncoding::Identity),
+
+        // The FAQ page is the same webapp, but the webapp routes to the correct page
         ("/faq", "../../dist/index.html", ContentEncoding::Identity),
         (
             "/index.html",
