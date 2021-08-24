b4fe47452e040384eac38cdd048d0cd573d3b9f2
Nicolas Mattia <nicolas@dfinity.org>
2021-08-24 14:04:27 +0200

    clean up faq ui

--- src/frontend/src/flows/faq/index.ts
+++ src/frontend/src/flows/faq/index.ts
@@ -10,24 +10,24 @@ export { questions } from "./questions";
 function renderQuestion(faq: Question) {
   return html`<li
     id=${faq.anchor}
-    class="sm:mx-4 sm:mb-10 sm:border-b-0 border-b-2 border-blue-400 sm:ring-2 sm:rounded-md p-4"
+    class="p-4"
   >
-    <div class="font-bold font-sans border-b-2 w-full border-blue-400">
+    <div class="font-bold font-sans border-b-2 w-full border-green-400">
       ${faq.question}
     </div>
-    <div class="pl-4 pt-4">
-      <p>${faq.answer}</p>
-      <ul class="p-2">
+    <div class="p-6">
+      <p class="leading-8 font-extralight max-w-md">${faq.answer}</p>
+      <ul class="p-4">
         ${Object.values(faq.links).map(
           (link) =>
             html`<li>
               &middot;
               <a
-                class="text-blue-800"
+                class="text-gray-800"
                 rel="noopener noreferrer"
                 href="${link.link}"
                 target="_blank"
-                >${link.name}</a
+                >${link.name} &#8599;</a
               >
             </li>`
         )}
@@ -45,30 +45,25 @@ const pageContent = html`
         background-color: transparent;
       }
       50% {
-        background-color: grey;
+        background-color: #A7F3D0;
       }
       100% {
         background-color: transparent;
       }
     }
-
-    a {
-      color: var(--grey-800);
-    }
-
     ul {
       list-style-type: none;
     }
 
     :target {
       animation-name: flash-question;
-      animation-duration: 3s;
+      animation-duration: 600ms;
     }
   </style>
   <div
-    class="container rounded-md m-auto h-full sm:my-10 bg-blue-100 sm:shadow-md text-blue-500 max-w-2xl"
+    class="container rounded-md m-auto h-full sm:my-10 bg-gray-100 sm:shadow-md text-gray-500 max-w-2xl"
   >
-    <h1 class="text-2xl text-center sm:text-left font-bold tracking-wide p-4">
+    <h1 class="text-7xl text-center font-bold tracking-wide p-4 text-green-300">
       FAQ
     </h1>
     <ul>
