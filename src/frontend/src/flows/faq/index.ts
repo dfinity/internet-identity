ba15a363b8e420beba81b131261dbee3a94bfe8d
Nicolas Mattia <nicolas@dfinity.org>
2021-09-14 11:32:51 +0200

    Clean up CSS

--- src/frontend/src/flows/faq/index.ts
+++ src/frontend/src/flows/faq/index.ts
@@ -50,7 +50,7 @@ const pageContent = html`
     }
 
     body {
-        position: relative;
+      position: relative;
     }
     /* briefly flash the question when redirected to a particular question */
     @keyframes flash-question {
@@ -58,113 +58,26 @@ const pageContent = html`
         background-color: transparent;
       }
       50% {
-        background-color: rgba(237,186,152,1);
-        border-radius: 0.3em;;
+        background-color: var(--rainbow-orange);
+        border-radius: 0.3em;
       }
       100% {
         background-color: transparent;
       }
     }
-    ul {
-      list-style-type: none;
-      width: auto;
-    }
-
-    @media only screen and (max-width: 600px) {
-        .faq__container {
-            padding: 0.6rem;
-        }
-    }
-    @media only screen and (min-width: 600px) {
-        .faq__container {
-            padding: 4rem;
-        }
-    }
-
-    .faq__questions {
-        padding: 0;
-    }
-
-    .faq__question {
-        padding: 1.5rem 0;
-    }
-
-    .faq__question-underline {
-        background: -webkit-gradient(linear,left top,right top,from(#29abe2),color-stop(33%,#fbb03b),color-stop(66%,#f15a24),to(#ed1e79));
-        background: linear-gradient(90deg,#29abe2,#fbb03b 33%,#f15a24 66%,#ed1e79);
-        height: 1px;
-        /*position: absolute; */
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: auto;
-    }
-    .faq__question-summary {
-        font-weight: 700;
-        font-family: ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-
-    }
-
-    .faq__title {
-        color: rgba(55,65,81,1);
-        font-weight: 700;
-        font-size: 4.5rem;
-        line-height: 1;
-        text-align: center;
-        letter-spacing: 0.025em;
-        padding: 2rem;
-    }
-
-    .faq__title-underline {
-        background: -webkit-gradient(linear,left top,right top,from(#29abe2),color-stop(33%,#fbb03b),color-stop(66%,#f15a24),to(#ed1e79));
-        background: linear-gradient(90deg,#29abe2,#fbb03b 33%,#f15a24 66%,#ed1e79);
-        height: 2px;
-        /*position: absolute; */
-        bottom: 0;
-        left: 0;
-        right: 0;
-        margin: 2em 5em;
-    }
-
-    .faq__question-details-container {
-        height: max-content;
-    }
-
-    .faq__answer {
-        line-height: 2rem;
-        font-weight: 200;
-        max-width: 28rem;
-        padding: 1.2em;
-        margin: auto 0;
-    }
-
-    .faq__answer-links {
-        padding: 1rem;
-    }
-
-    .faq__answer-link {
-        color: rgba(31,41,55,1);
-    }
-
-    .faq__container {
-        background-color: rgba(243,244,246,1);
-        max-width: 42rem;
-    }
-
     :target {
       animation-name: flash-question;
       animation-duration: 600ms;
     }
   </style>
   <div class="faq__container">
-  <div>
-    <h1 class="faq__title">
-      FAQ
-    </h1>
-    <div class="faq__title-underline"></div>
-    <ul class="faq__questions">
-      ${Object.values(questions).map((faq) => renderQuestion(faq))}
-    </ul>
+    <div>
+      <h1 class="faq__title">FAQ</h1>
+      <div class="faq__title-underline"></div>
+      <ul class="faq__questions">
+        ${Object.values(questions).map((faq) => renderQuestion(faq))}
+      </ul>
+    </div>
   </div>
 `;
 
