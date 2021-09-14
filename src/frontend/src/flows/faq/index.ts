5d044874c9128c02ed2be7d04120231f1bb7d9f3
Formatting Committer <nobody@example.com>
2021-09-14 13:52:11 +0000

    Updating frontend formatting

--- src/frontend/src/flows/faq/index.ts
+++ src/frontend/src/flows/faq/index.ts
@@ -61,18 +61,19 @@ const pageContent = html`
     }
   </style>
   <div class="faq__container">
-      <h1 class="faq__title">FAQ</h1>
-      <ul class="faq__questions">
-        ${Object.values(questions).map((faq) => renderQuestion(faq))}
-      </ul>
+    <h1 class="faq__title">FAQ</h1>
+    <ul class="faq__questions">
+      ${Object.values(questions).map((faq) => renderQuestion(faq))}
+    </ul>
   </div>
 `;
 
 const openAnchor = (): void => {
   const hash = location.hash.substring(1);
 
-  if (hash) { // TODO: allowString false: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/strict-boolean-expressions.md#options
-      // and allowNumber
+  if (hash) {
+    // TODO: allowString false: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/strict-boolean-expressions.md#options
+    // and allowNumber
     const details = document.getElementById(hash) as HTMLDetailsElement;
     details.open = true; // FIXME
   }
