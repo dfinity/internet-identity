de64428c0c476125bef08cc6c8e52f106a02e198
Formatting Committer <nobody@example.com>
2021-09-16 14:08:59 +0000

    Updating frontend formatting

--- src/frontend/src/test-e2e/util.ts
+++ src/frontend/src/test-e2e/util.ts
@@ -58,7 +58,7 @@ export async function runInBrowserCommon(
     automationProtocol: "webdriver",
     path: "/wd/hub",
     logLevel: "info",
-    outputDir: "./"
+    outputDir: "./",
   });
 
   // setup test suite
