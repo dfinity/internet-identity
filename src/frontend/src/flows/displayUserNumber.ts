83142a098eaaed9350f0fa910b57e70264931964
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2021-11-30 15:18:41 +0100

    Stronger prompt for recording identity anchor

--- src/frontend/src/flows/displayUserNumber.ts
+++ src/frontend/src/flows/displayUserNumber.ts
@@ -1,12 +1,24 @@
 import { html, render } from "lit-html";
+import {warningIcon} from "../components/icons";
 
 const pageContent = (userNumber: bigint) => html`
   <div class="container">
     <h1>Congratulations!</h1>
     <p>
-      Please record your new Identity Anchor. You will need it later to use
-      Internet Identity or to add additional devices.
+     Your new Identity Anchor has been created. You will need it later to use
+     Internet Identity or to add additional devices.
     </p>
+    <div class="nagBox">
+      <div class="nagIcon">${warningIcon}</div>
+      <div class="nagContent">
+        <div class="nagTitle">Record Your Identity Anchor</div>
+        <div class="nagMessage">
+          Do <b>NOT</b> forget to save this Identity Anchor. Save a backup on a
+          storage medium and write it down.</br>
+          Note: It is advised to keep Identity Anchors secret in order to stay anonymous.
+        </div>
+      </div>
+    </div>
     <label>Identity Anchor:</label>
     <div class="highlightBox">${userNumber}</div>
     <button id="displayUserContinue" class="primary">Continue</button>
