521c4ac3590d3a311c7ee7c008f63f38c8f1abba
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-01-13 10:42:17 +0100

    1024 -> 256 bits of entropy

--- src/frontend/src/crypto/mnemonic.ts
+++ src/frontend/src/crypto/mnemonic.ts
@@ -5,7 +5,7 @@ import { toHexString } from "@dfinity/identity/lib/cjs/buffer";
  * @returns A random BIP39 mnemonic with 256 bits of entropy.
  */
 export function generate(): string {
-  const entropy = new Uint32Array(32);
+  const entropy = new Uint32Array(8);
   crypto.getRandomValues(entropy);
   return entropyToMnemonic(toHexString(entropy.buffer), wordlists.english);
 }
