17e17461c24d0561c0483b9de9882b4ddeef6553
Nicolas Mattia <nicolas@dfinity.org>
2022-02-01 16:51:29 +0100

    Compute asset hashes in post_upgrade
    This removes the build-time hash computation and make it happen when
    `for_each_asset` is called, effectively in `post_upgrade` (and `init`).
    
    The interface is still a bit cumbersome (e.g. `for_each_asset` is still
    callback based) but this makes sure that `main.rs` is not changed; the
    focus here is the asset hashes. This is the first step in removing
    `CANISTER_ID` from the build process; we want to inject it in the assets
    directly, and this will change `main.rs` and `for_each_asset`.
    
    I've tested this locally and assets (at least `index.js`) have the same
    ic-certificate header as they have on `main`.

--- src/internet_identity/src/assets.rs
+++ src/internet_identity/src/assets.rs
@@ -1,1 +1,81 @@
-include!(concat!(env!("OUT_DIR"), "/assets.rs"));
+use sha2::Digest;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ContentEncoding {
+    Identity,
+    GZip,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ContentType {
+    HTML,
+    JS,
+    ICO,
+    WEBP,
+    SVG
+}
+
+pub fn for_each_asset(mut f: impl FnMut(&'static str, ContentEncoding, ContentType, &'static [u8], &[u8; 32])) {
+    let assets: [ (&str, &[u8], ContentEncoding, ContentType); 8] = [
+         ("/",
+            include_bytes!("../../../dist/index.html"),
+            ContentEncoding::Identity,
+            ContentType::HTML,
+         ),
+         // The FAQ and about pages are the same webapp, but the webapp routes to the correct page
+         (
+             "/faq",
+             include_bytes!("../../../dist/index.html"),
+             ContentEncoding::Identity,
+             ContentType::HTML,
+         ),
+         (
+             "/about",
+             include_bytes!("../../../dist/index.html"),
+             ContentEncoding::Identity,
+             ContentType::HTML,
+         ),
+         (
+             "/index.html",
+             include_bytes!("../../../dist/index.html"),
+             ContentEncoding::Identity,
+             ContentType::HTML,
+         ),
+         (
+             "/index.js",
+             include_bytes!("../../../dist/index.js.gz"),
+             ContentEncoding::GZip,
+             ContentType::JS,
+         ),
+         (
+             "/loader.webp",
+             include_bytes!("../../../dist/loader.webp"),
+             ContentEncoding::Identity,
+             ContentType::WEBP,
+         ),
+         (
+             "/favicon.ico",
+             include_bytes!("../../../dist/favicon.ico"),
+             ContentEncoding::Identity,
+             ContentType::ICO,
+         ),
+         (
+             "/ic-badge.svg",
+             include_bytes!("../../../dist/ic-badge.svg"),
+             ContentEncoding::Identity,
+             ContentType::SVG,
+         ),
+    ];
+
+    for (name, content, encoding, content_type) in assets {
+        let hash = hash_content(content);
+        f(name, encoding, content_type, content, &hash);
+    }
+}
+
+
+fn hash_content(bytes: &[u8]) -> [u8; 32] {
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(&bytes);
+    hasher.finalize().into()
+}
