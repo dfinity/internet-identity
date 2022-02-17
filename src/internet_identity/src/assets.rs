71ee31877a493e24a28e8b8027b34f8d5119443f
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-02-17 16:09:30 +0100

    Move HTTP related code to own submodule

--- src/internet_identity/src/assets.rs
+++ src/internet_identity/src/assets.rs
@@ -2,9 +2,10 @@
 //
 // This file describes which assets are used and how (content, content type and content encoding).
 
-use sha2::Digest;
-use lazy_static::lazy_static;
+use crate::{ASSETS, STATE};
 use ic_cdk::api;
+use lazy_static::lazy_static;
+use sha2::Digest;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum ContentEncoding {
@@ -18,7 +19,7 @@ pub enum ContentType {
     JS,
     ICO,
     WEBP,
-    SVG
+    SVG,
 }
 
 lazy_static! {
@@ -49,59 +50,84 @@ lazy_static! {
     };
 }
 
+// used both in init and post_upgrade
+pub fn init_assets() {
+    STATE.with(|s| {
+        let mut asset_hashes = s.asset_hashes.borrow_mut();
+
+        ASSETS.with(|a| {
+            let mut assets = a.borrow_mut();
+            for (path, content, content_encoding, content_type) in get_assets() {
+                asset_hashes.insert(path, sha2::Sha256::digest(content).into());
+                let mut headers = match content_encoding {
+                    ContentEncoding::Identity => vec![],
+                    ContentEncoding::GZip => {
+                        vec![("Content-Encoding".to_string(), "gzip".to_string())]
+                    }
+                };
+                headers.push((
+                    "Content-Type".to_string(),
+                    content_type.to_mime_type_string(),
+                ));
+                assets.insert(path, (headers, content));
+            }
+        });
+    });
+}
+
 // Get all the assets. Duplicated assets like index.html are shared and generally all assets are
 // prepared only once (like injecting the canister ID).
-pub fn get_assets() -> [ (&'static str, &'static [u8], ContentEncoding, ContentType); 8]  {
+fn get_assets() -> [(&'static str, &'static [u8], ContentEncoding, ContentType); 8] {
     let index_html: &[u8] = INDEX_HTML_STR.as_bytes();
     [
-         ("/",
-             index_html,
-             ContentEncoding::Identity,
-             ContentType::HTML,
-         ),
-         // The FAQ and about pages are the same webapp, but the webapp routes to the correct page
-         (
-             "/faq",
-             index_html,
-             ContentEncoding::Identity,
-             ContentType::HTML,
-         ),
-         (
-             "/about",
-             index_html,
-             ContentEncoding::Identity,
-             ContentType::HTML,
-         ),
-         (
-             "/index.html",
-             index_html,
-             ContentEncoding::Identity,
-             ContentType::HTML,
-         ),
-         (
-             "/index.js",
-             include_bytes!("../../../dist/index.js.gz"),
-             ContentEncoding::GZip,
-             ContentType::JS,
-         ),
-         (
-             "/loader.webp",
-             include_bytes!("../../../dist/loader.webp"),
-             ContentEncoding::Identity,
-             ContentType::WEBP,
-         ),
-         (
-             "/favicon.ico",
-             include_bytes!("../../../dist/favicon.ico"),
-             ContentEncoding::Identity,
-             ContentType::ICO,
-         ),
-         (
-             "/ic-badge.svg",
-             include_bytes!("../../../dist/ic-badge.svg"),
-             ContentEncoding::Identity,
-             ContentType::SVG,
-         ),
-        ]
-
+        (
+            "/",
+            index_html,
+            ContentEncoding::Identity,
+            ContentType::HTML,
+        ),
+        // The FAQ and about pages are the same webapp, but the webapp routes to the correct page
+        (
+            "/faq",
+            index_html,
+            ContentEncoding::Identity,
+            ContentType::HTML,
+        ),
+        (
+            "/about",
+            index_html,
+            ContentEncoding::Identity,
+            ContentType::HTML,
+        ),
+        (
+            "/index.html",
+            index_html,
+            ContentEncoding::Identity,
+            ContentType::HTML,
+        ),
+        (
+            "/index.js",
+            include_bytes!("../../../dist/index.js.gz"),
+            ContentEncoding::GZip,
+            ContentType::JS,
+        ),
+        (
+            "/loader.webp",
+            include_bytes!("../../../dist/loader.webp"),
+            ContentEncoding::Identity,
+            ContentType::WEBP,
+        ),
+        (
+            "/favicon.ico",
+            include_bytes!("../../../dist/favicon.ico"),
+            ContentEncoding::Identity,
+            ContentType::ICO,
+        ),
+        (
+            "/ic-badge.svg",
+            include_bytes!("../../../dist/ic-badge.svg"),
+            ContentEncoding::Identity,
+            ContentType::SVG,
+        ),
+    ]
 }
