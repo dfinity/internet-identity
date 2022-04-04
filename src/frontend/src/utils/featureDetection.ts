f97a8ee48d974003941d8b23182e710bf8425a83
Nicolas Mattia <nicolas@dfinity.org>
2022-04-04 18:10:46 +0200

    Show reason if feature check fails
    This displays a reason for the feature check (compatibility check) in
    the compatibility notice, which should help users understand why their
    browser is not supported.

--- src/frontend/src/utils/featureDetection.ts
+++ src/frontend/src/utils/featureDetection.ts
@@ -1,11 +1,17 @@
-export const hasRequiredFeatures = async (url: URL): Promise<boolean> => {
-  if (window.PublicKeyCredential === undefined) return false;
-  if (url.hash === "#compatibilityNotice") return false;
+export const checkRequiredFeatures = async (
+  url: URL
+): Promise<true | string> => {
+  if (window.PublicKeyCredential === undefined)
+    return "window.PublicKeyCredential is not defined";
+  if (url.hash === "#compatibilityNotice")
+    return "Remove #compatibilityNotice from the URL and try again.";
   // For mobile devices we want to make sure we can use platform authenticators
   if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android)/)) return true;
   try {
-    return await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable();
+    return (await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable())
+      ? true
+      : "UserVerifyingPlatformAuthenticator is not available";
   } catch (error) {
-    return false;
+    return `An error occured when checking for compatibility: ${error.message}`;
   }
 };
