4d90ac3c32b587ebf922c0074c23f1e3d733cb73
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-05-20 12:02:23 +0200

    Add test for prefilled anchor number when authenticating for a dapp (#649)
    * Add test for prefilled anchor number when authenticating for a dapp
    * Add comment to command doc

--- src/frontend/src/test-e2e/selenium.test.ts
+++ src/frontend/src/test-e2e/selenium.test.ts
@@ -23,6 +23,9 @@ import {
 import { FLOWS } from "./flows";
 import {
   addVirtualAuthenticator,
+  addWebAuthnCredential,
+  getWebAuthnCredentials,
+  originToRpId,
   removeFeaturesWarning,
   removeVirtualAuthenticator,
   RunConfiguration,
@@ -595,3 +598,55 @@ test("Screenshots", async () => {
     }
   );
 }, 400_000);
+
+test("Register first then log into client application", async () => {
+  await runInBrowser(async (browser: WebdriverIO.Browser) => {
+    const authenticatorId1 = await addVirtualAuthenticator(browser);
+
+    await browser.url(II_URL);
+    const userNumber = await FLOWS.registerNewIdentityWelcomeView(
+      DEVICE_NAME1,
+      browser
+    );
+
+    const credentials = await getWebAuthnCredentials(browser, authenticatorId1);
+    expect(credentials).toHaveLength(1);
+
+    const demoAppView = new DemoAppView(browser);
+    await demoAppView.open(DEMO_APP_URL, II_URL);
+    await demoAppView.waitForDisplay();
+    expect(await demoAppView.getPrincipal()).toBe("2vxsx-fae");
+    await demoAppView.signin();
+
+    const authenticatorId2 = await switchToPopup(browser);
+    await addWebAuthnCredential(
+      browser,
+      authenticatorId2,
+      credentials[0],
+      originToRpId(II_ORIGIN)
+    );
+
+    const authenticateView = new AuthenticateView(browser);
+    await authenticateView.waitForDisplay();
+    await authenticateView.expectPrefilledAnchorToBe(userNumber);
+    await authenticateView.authenticate();
+    const recoveryMethodSelectorView = new RecoveryMethodSelectorView(browser);
+    await recoveryMethodSelectorView.waitForDisplay();
+    await recoveryMethodSelectorView.skipRecovery();
+    const singleDeviceWarningView = new SingleDeviceWarningView(browser);
+    await singleDeviceWarningView.waitForDisplay();
+    await singleDeviceWarningView.remindLater();
+    await waitToClose(browser);
+    await demoAppView.waitForDisplay();
+    const principal = await demoAppView.getPrincipal();
+    expect(principal).not.toBe("2vxsx-fae");
+
+    expect(await demoAppView.whoami(REPLICA_URL, WHOAMI_CANISTER)).toBe(
+      principal
+    );
+
+    // default value
+    const exp = await browser.$("#expiration").getText();
+    expect(Number(exp) / (24 * 60 * 60_000_000_000)).toBeCloseTo(1);
+  });
+}, 300_000);
