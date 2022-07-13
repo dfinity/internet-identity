db6df4984b053fb27a44f1cf7b8fa61e691fff89
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2022-07-13 18:18:37 +0200

    Apply review suggestion
    Co-authored-by: Max <bitdivine@users.noreply.github.com>

--- demos/test-app/build.sh
+++ demos/test-app/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Make sure we always run from the script location
+# Try to run from the script location
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
