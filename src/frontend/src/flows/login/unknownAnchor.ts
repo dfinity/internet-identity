7ca1cc1ec985c9a15a7ca015493dc3c7fb07f105
Sue Ann Kim <kimsueann@gmail.com>
2022-08-05 10:32:13 -0700

    Change inline text buttons to anchor tags on landing page.
    Inside each <li class="textLink>:
    - Create an Internet Identity Anchor
    - but using a new device?
    - and want to recover?

--- src/frontend/src/flows/login/unknownAnchor.ts
+++ src/frontend/src/flows/login/unknownAnchor.ts
@@ -67,21 +67,19 @@ const pageContent = () => html` <style>
       <ul class="list-reset">
         <li class="textLink" id="registerSection">
           New?
-          <button id="registerButton" class="linkStyle">
+          <a id="registerButton" class="linkStyle">
             Create an Internet Identity Anchor.
-          </button>
+          </a>
         </li>
         <li class="textLink">
           Already have an anchor
-          <button id="addNewDeviceButton" class="linkStyle">
+          <a id="addNewDeviceButton" class="linkStyle">
             but using a new device?
-          </button>
+          </a>
         </li>
         <li class="textLink">
           Lost access
-          <button id="recoverButton" class="linkStyle">
-            and want to recover?
-          </button>
+          <a id="recoverLink" class="linkStyle"> and want to recover? </a>
         </li>
       </ul>
     </aside>
@@ -121,10 +119,10 @@ const initRegister = (
 };
 
 const initRecovery = () => {
-  const recoverButton = document.getElementById(
-    "recoverButton"
-  ) as HTMLButtonElement;
-  recoverButton.onclick = () => useRecovery();
+  const recoverLink = document.getElementById(
+    "recoverLink"
+  ) as HTMLAnchorElement;
+  recoverLink.onclick = () => useRecovery();
 };
 
 const initLogin = (resolve: (res: LoginFlowResult) => void) => {
