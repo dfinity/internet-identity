8292a06f9e3e906c6bcdffd4f63bd06ff09ceb90
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-08-12 19:21:32 +0200

    Remove dfx generated faff

--- webpack.config.js
+++ webpack.config.js
@@ -44,129 +44,112 @@ function readCanisterId() {
   return canisterId;
 }
 
+const isProduction = process.env.NODE_ENV === "production";
+const devtool = isProduction ? undefined : "source-map";
+
 /**
  * Generate a webpack configuration for a canister.
  */
-function generateWebpackConfigForCanister(name, info) {
-  const isProduction = process.env.NODE_ENV === "production";
-  const devtool = isProduction ? undefined : "source-map";
+module.exports = {
+  mode: isProduction ? "production" : "development",
+  entry: {
+    index: path.join(__dirname, "src", "frontend", "src", "index"),
+  },
+  devtool,
+  optimization: {
+    minimize: isProduction,
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+    fallback: {
+      assert: require.resolve("assert/"),
+      buffer: require.resolve("buffer/"),
+      events: require.resolve("events/"),
+      stream: require.resolve("stream-browserify/"),
+      util: require.resolve("util/"),
+    },
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+  devServer: {
+    // Set up a proxy that redirects API calls and /index.html to the
+    // replica; the rest we serve from here.
+    onBeforeSetupMiddleware: (devServer) => {
+      // canisterId singleton, read lazily
+      let canisterId = null;
 
-  return {
-    mode: isProduction ? "production" : "development",
-    entry: {
-      index: path.join(__dirname, "src", "frontend", "src", "index"),
+      // basically everything _except_ for index.js, because we want live reload
+      devServer.app.get(
+        ["/", "/index.html", "/faq", "/about"],
+        HttpProxyMiddlware.createProxyMiddleware({
+          // Read the replica host from dfx.json. The replica does not need to be running, which is convenient
+          // in case we just load pages that don't talk to the canister (then we wouldn't want to have to start the replica or build the canister)
+          target: readReplicaHost(),
+
+          // For path rewriting we use a function that lazily (i.e. when first called, using a singleton) reads
+          // the canister ID from the local dfx state and returns a new request path that includes
+          // the ?canisterId=foo atrocity
+          pathRewrite: (pathAndParams, req) => {
+            let queryParamsString = `?`;
+
+            const [path, params] = pathAndParams.split("?");
+
+            if (params) {
+              queryParamsString += `${params}&`;
+            }
+
+            if (canisterId === null) {
+              canisterId = readCanisterId();
+            }
+
+            queryParamsString += `canisterId=${canisterId}`;
+
+            return path + queryParamsString;
+          },
+        })
+      );
     },
-    devtool,
-    optimization: {
-      minimize: isProduction,
+    port: 8080,
+    proxy: {
+      // Make sure /api calls land on the replica (and not on webpack)
+      "/api": "http://localhost:8000",
     },
-    resolve: {
-      extensions: [".js", ".ts", ".jsx", ".tsx"],
-      fallback: {
-        assert: require.resolve("assert/"),
-        buffer: require.resolve("buffer/"),
-        events: require.resolve("events/"),
-        stream: require.resolve("stream-browserify/"),
-        util: require.resolve("util/"),
+    allowedHosts: [".localhost", ".local", ".ngrok.io"],
+  },
+
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/, loader: "ts-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset/resource",
       },
-    },
-    output: {
-      filename: "[name].js",
-      path: path.join(__dirname, "dist"),
-    },
-    devServer: {
-      // Set up a proxy that redirects API calls and /index.html to the
-      // replica; the rest we serve from here.
-      onBeforeSetupMiddleware: (devServer) => {
-        // canisterId singleton, read lazily
-        let canisterId = null;
-
-        // basically everything _except_ for index.js, because we want live reload
-        devServer.app.get(
-          ["/", "/index.html", "/faq", "/about"],
-          HttpProxyMiddlware.createProxyMiddleware({
-            // Read the replica host from dfx.json. The replica does not need to be running, which is convenient
-            // in case we just load pages that don't talk to the canister (then we wouldn't want to have to start the replica or build the canister)
-            target: readReplicaHost(),
-
-            // For path rewriting we use a function that lazily (i.e. when first called, using a singleton) reads
-            // the canister ID from the local dfx state and returns a new request path that includes
-            // the ?canisterId=foo atrocity
-            pathRewrite: (pathAndParams, req) => {
-              let queryParamsString = `?`;
-
-              const [path, params] = pathAndParams.split("?");
-
-              if (params) {
-                queryParamsString += `${params}&`;
-              }
-
-              if (canisterId === null) {
-                canisterId = readCanisterId();
-              }
-
-              queryParamsString += `canisterId=${canisterId}`;
-
-              return path + queryParamsString;
-            },
-          })
-        );
-      },
-      port: 8080,
-      proxy: {
-        // Make sure /api calls land on the replica (and not on webpack)
-        "/api": "http://localhost:8000",
-      },
-      allowedHosts: [".localhost", ".local", ".ngrok.io"],
-    },
-
-    // Depending in the language or framework you are using for
-    // front-end development, add module loaders to the default
-    // webpack configuration. For example, if you are using React
-    // modules and CSS as described in the "Adding a stylesheet"
-    // tutorial, uncomment the following lines:
-    module: {
-      rules: [
-        { test: /\.(ts|tsx)$/, loader: "ts-loader" },
-        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
         {
-          test: /\.(png|jpg|gif)$/i,
-          type: "asset/resource",
+          from: path.join(__dirname, "src", "frontend", "assets"),
+          to: path.join(__dirname, "dist"),
         },
       ],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(__dirname, "src", "frontend", "assets"),
-            to: path.join(__dirname, "dist"),
-          },
-        ],
-      }),
-      new webpack.ProvidePlugin({
-        Buffer: [require.resolve("buffer/"), "Buffer"],
-        process: require.resolve("process/browser"),
-      }),
-      new webpack.EnvironmentPlugin({
-        II_FETCH_ROOT_KEY: "0",
-        II_DUMMY_AUTH: "0",
-        II_DUMMY_CAPTCHA: "0",
-      }),
-      new CompressionPlugin({
-        test: /\.js(\?.*)?$/i,
-      }),
-      new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/),
-    ],
-  };
-}
-
-// If you have additional webpack configurations you want to build
-//  as part of this configuration, add them to the section below.
-module.exports = [
-  ...Object.entries(dfxJson.canisters)
-    .map(([name, info]) => {
-      return generateWebpackConfigForCanister(name, info);
-    })
-    .filter((x) => !!x),
-];
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: [require.resolve("buffer/"), "Buffer"],
+      process: require.resolve("process/browser"),
+    }),
+    new webpack.EnvironmentPlugin({
+      II_FETCH_ROOT_KEY: "0",
+      II_DUMMY_AUTH: "0",
+      II_DUMMY_CAPTCHA: "0",
+    }),
+    new CompressionPlugin({
+      test: /\.js(\?.*)?$/i,
+    }),
+    new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/),
+  ],
+};
