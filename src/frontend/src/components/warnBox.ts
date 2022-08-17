e411e547b72e9f59ea6b7f421b4c0d0133ff38de
Sue Ann Kim <kimsueann@gmail.com>
2022-08-17 16:36:13 -0700

    warnBox component has custom properties: title, message, slot and htmlElement. - "slot" is for a custom element that gets appended at the end. -"htmlElement" is the type of wrapper element the component will be. By default the component will be an <aside>. The other option is a <div> wrapper.

--- src/frontend/src/components/warnBox.ts
+++ src/frontend/src/components/warnBox.ts
@@ -1,25 +1,34 @@
 import { html, TemplateResult } from "lit-html";
 import { warningIcon } from "./icons";
 
-// The Warning Component can be reused with following custom properties: title, message, and slot.
-// Slot is for a custom element if needed, such as a button, that gets appended at the end.
+// The Warning Component can be reused with following custom properties: title, message, slot and htmlElement.
+// "slot" is for a custom element, such as a button, that gets appended at the end.
+// "htmlElement" is the type of wrapper element the component will be.
+// By default the component will be an <aside>. The other option is a <div> wrapper.
 
 interface warnBoxProps {
   title: string;
   message: string | TemplateResult;
   slot?: TemplateResult;
+  htmlElement?: "div" | "aside";
 }
 
 export const warnBox = ({
   title,
   message,
   slot,
-}: warnBoxProps): TemplateResult =>
-  html`<aside class="warnBox">
+  htmlElement = "aside",
+}: warnBoxProps): TemplateResult => {
+  const contents: TemplateResult = html`
     <div class="warnIcon">${warningIcon}</div>
     <div class="warnContent">
       <div class="warnTitle">${title}</div>
       <div class="warnMessage">${message}</div>
       ${slot}
-    </aside>
-  </div>`;
+    </div>
+  `;
+
+  return html`${htmlElement === "aside"
+    ? html` <aside class="warnBox">${contents}</aside>`
+    : html` <div class="warnBox">${contents}</div> `}`;
+};
