763f897d823f1d54f3dc0d9fa0a22f2478f89e12
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-08-29 20:55:03 +0200

    Add loader to showcase
    It was missing.

--- src/frontend/src/showcase.ts
+++ src/frontend/src/showcase.ts
@@ -38,6 +38,7 @@ import {
 import { deviceRegistrationDisabledInfo } from "./flows/addDevice/welcomeView/deviceRegistrationModeDisabled";
 import { showVerificationCode } from "./flows/addDevice/welcomeView/showVerificationCode";
 import { verifyDevice } from "./flows/addDevice/manage/verifyTentativeDevice";
+import { withLoader } from "./components/loader";
 
 // A "dummy" connection which actually is just undefined, hoping pages won't call it
 const dummyConnection = undefined as unknown as AuthenticatedConnection;
@@ -142,6 +143,7 @@ const iiPages: Record<string, () => void> = {
     ),
   deviceSettings: () =>
     deviceSettings(userNumber, dummyConnection, simpleDevice, false),
+  loader: () => withLoader(() => new Promise(() => renderConstructing())),
 };
 
 // The showcase
