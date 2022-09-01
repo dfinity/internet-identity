369da009ff944dd727d5768b3b51e1463b3fffa4
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-09-01 13:38:52 +0200

    Fix warning borders on iOS and small devices
    The warning boxes were too big on small devices which caused the warning
    icon to leak out of the page. All warning messages were moved to
    `warnBox` and wrapped in a highligh-card.
    
    Additionally, the "details" caret was moved right a bit to prevent it
    overlapping with the left border.

--- src/frontend/src/flows/recovery/displaySingleDeviceWarning.ts
+++ src/frontend/src/flows/recovery/displaySingleDeviceWarning.ts
@@ -1,31 +1,30 @@
 import { html, render } from "lit-html";
-import { warningIcon } from "../../components/icons";
+import { warnBox } from "../../components/warnBox";
 import { setupRecovery } from "./setupRecovery";
 import { AuthenticatedConnection } from "../../utils/iiConnection";
 
 const pageContent = () => html`
-  <article
-    id="warningContainer"
-    class="l-container c-card c-card--highlight c-card--warning"
-  >
-    <hgroup>
-      <div aria-hidden class="c-card__icon">${warningIcon}</div>
-      <h1 id="warningHeading" class="t-title t-title--main c-card__title">
-        Warning
-      </h1>
-      <p class="t-lead">
-        If you lose all the devices assigned to your Internet Identity anchor,
-        then you will <em>lose access</em> to the anchor, and all associated
-        resources and tokens, unless you have a recovery mechanism setup. This
-        can be an external key fob or a secure seedphrase, which you must make
-        sure is not stolen.
-      </p>
-    </hgroup>
-    <p class="t-paragraph">
-      As a best practice, we recommend you assign multiple devices to an
-      Identity Anchor and add <em>at least</em> one recovery mechanism such as
-      an external key fob or a seedphrase.
-    </p>
+  <article id="warningContainer" class="l-container c-card c-card--highlight">
+    ${warnBox({
+      title: "Warning",
+      message: "Only one device registered.",
+      slot: html`
+        <p class="t-paragraph t-lead">
+          If you lose all the devices assigned to your Internet Identity anchor,
+          then you will <em>lose access</em> to the anchor, and all associated
+          resources and tokens, unless you have a recovery mechanism setup. This
+          can be an external key fob or a secure seedphrase, which you must make
+          sure is not stolen.
+        </p>
+
+        <p class="t-paragraph">
+          As a best practice, we recommend you assign multiple devices to an
+          Identity Anchor and add <em>at least</em> one recovery mechanism such
+          as an external key fob or a seedphrase.
+        </p>
+      `,
+      htmlElement: "div",
+    })}
     <div class="l-section">
       <button id="displayWarningAddRecovery" class="c-button">
         Add a recovery mechanism to an Identity Anchor
