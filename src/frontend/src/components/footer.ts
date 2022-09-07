f8ae73ccb3ff6139c9909756306b445ad1a93132
peterpeterparker <david.dalbusco@outlook.com>
2022-09-07 15:29:44 +0200

    feat: link footer with ic homepage

--- src/frontend/src/components/footer.ts
+++ src/frontend/src/components/footer.ts
@@ -3,8 +3,9 @@ import { html } from "lit-html";
 export const footer = html`<footer class="l-footer">
   <a
     class="page-signature"
-    aria-label="DFINITY homepage"
-    href="https://dfinity.org"
+    aria-label="Internet Computer homepage"
+    href="https://internetcomputer.org/"
+    rel="noopener noreferrer"
     target="_blank"
     ><img src="/ic-badge.svg" alt="Built on Crypto | Internet Computer"
   /></a>
