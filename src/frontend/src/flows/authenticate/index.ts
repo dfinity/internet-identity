4f329751f8ce9d33ed088b7391fecd87315cc507
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-09-09 16:48:09 +0200

    wip

--- src/frontend/src/flows/authenticate/index.ts
+++ src/frontend/src/flows/authenticate/index.ts
@@ -233,10 +233,12 @@ function initManagementBtn() {
   const manageButton = document.getElementById(
     "manageButton"
   ) as HTMLAnchorElement;
-  manageButton.onclick = () => {
-    window.location.hash = "";
-    window.location.reload();
-  };
+  if( manageButton !== null ) {
+      manageButton.onclick = () => {
+          window.location.hash = "";
+          window.location.reload();
+      };
+  }
 }
 
 const initRegistration = async (
