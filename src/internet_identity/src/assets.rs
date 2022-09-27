dc20949234ab5fc368094503437adddba6bfced9
David Dal Busco <david.dalbusco@dfinity.org>
2022-09-27 12:40:17 +0200

    Fix number of assets

--- src/internet_identity/src/assets.rs
+++ src/internet_identity/src/assets.rs
@@ -80,7 +80,7 @@ pub fn init_assets() {
 
 // Get all the assets. Duplicated assets like index.html are shared and generally all assets are
 // prepared only once (like injecting the canister ID).
-fn get_assets() -> [(&'static str, &'static [u8], ContentEncoding, ContentType); 9] {
+fn get_assets() -> [(&'static str, &'static [u8], ContentEncoding, ContentType); 21] {
     let index_html: &[u8] = INDEX_HTML_STR.as_bytes();
     [
         (
