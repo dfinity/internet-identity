727534ade366b2cda2da4a06c2b6f4056bf90463
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-11-21 16:21:25 +0100

    Refactor registration flow (#1033)
    * Refactor registration flow
    
    This cleans up the registration code and changes the module structure a bit.
    In particular the device alias picker is made to use idiomatic lit, most
    `.then()`s are replaced in favor of more consistent `async/await` and
    some tests are added.
    
    * Remove unused import

---
+++ src/frontend/src/flows/register/alias.ts
@@ -0,0 +1,90 @@
+import { createRef, ref, Ref } from "lit-html/directives/ref.js";
+import { withRef } from "../../utils/lit-html";
+import { html, render, TemplateResult } from "lit-html";
+import { icLogo } from "../../components/icons";
+import { validateAlias } from "../addDevice/validateAlias";
+
+/* Everything (template, component, page) related to picking a device alias */
+
+export const promptDeviceAliasTemplate = (props: {
+  continue: (alias: string) => void;
+  cancel: () => void;
+}): TemplateResult => {
+  const aliasInput: Ref<HTMLInputElement> = createRef();
+  return html`
+    <div class="l-container c-card c-card--highlight">
+      <hgroup>
+        <div class="c-logo">${icLogo}</div>
+        <p class="t-lead t-centered t-paragraph l-stack">
+          What device are you using?
+        </p>
+      </hgroup>
+      <form
+        id="registerForm"
+        @submit=${(e: SubmitEvent) => {
+          e.preventDefault();
+          e.stopPropagation();
+          withRef(aliasInput, (alias) => props.continue(alias.value));
+        }}
+      >
+        <input
+          id="registerAlias"
+          ${ref(aliasInput)}
+          @input=${(e: InputEvent) => {
+            if (!(e.currentTarget instanceof HTMLInputElement)) {
+              return;
+            }
+            e.currentTarget.setCustomValidity("");
+            e.currentTarget.reportValidity();
+          }}
+          @invalid=${(e: InputEvent) => {
+            if (!(e.currentTarget instanceof HTMLInputElement)) {
+              return;
+            }
+            const message = validateAlias(
+              e.currentTarget.validity,
+              e.currentTarget.value
+            );
+            e.currentTarget.setCustomValidity(message);
+          }}
+          placeholder="Device name"
+          aria-label="device name"
+          type="text"
+          required
+          maxlength="30"
+          pattern="^[A-Za-z0-9]+((-|\\s|_)*[A-Za-z0-9])*$"
+          spellcheck="false"
+          class="c-input"
+        />
+        <div class="c-button-group">
+          <button
+            id="registerCancel"
+            type="button"
+            class="c-button c-button--secondary"
+            @click="${() => props.cancel()}"
+          >
+            Cancel
+          </button>
+          <button id="registerButton" type="submit" class="c-button">
+            Next
+          </button>
+        </div>
+      </form>
+    </div>
+  `;
+};
+
+export const promptDeviceAliasPage = (props: {
+  cancel: () => void;
+  continue: (alias: string) => void;
+  container?: HTMLElement;
+}): void => {
+  const container =
+    props.container ?? (document.getElementById("pageContent") as HTMLElement);
+  render(promptDeviceAliasTemplate(props), container);
+};
+
+export const promptDeviceAlias = (): Promise<string | null> =>
+  new Promise((resolve) => {
+    promptDeviceAliasPage({ cancel: () => resolve(null), continue: resolve });
+  });
---
+++ src/frontend/src/flows/register/index.ts
@@ -0,0 +1,42 @@
+import { Connection } from "../../utils/iiConnection";
+import { unknownToString } from "../../utils/utils";
+import { confirmRegister } from "./captcha";
+import { makeCaptcha } from "./captcha";
+import { LoginFlowResult, cancel } from "../login/flowResult";
+import { constructIdentity } from "./construct";
+import { promptDeviceAlias } from "./alias";
+
+/** Registration (anchor creation) flow for new users */
+export const register = async ({
+  connection,
+}: {
+  connection: Connection;
+}): Promise<LoginFlowResult> => {
+  try {
+    const alias = await promptDeviceAlias();
+    if (alias === null) {
+      return cancel;
+    }
+
+    const [captcha, identity] = await Promise.all([
+      makeCaptcha(connection),
+      constructIdentity(),
+    ]);
+
+    const result = await confirmRegister(
+      connection,
+      Promise.resolve(captcha),
+      identity,
+      alias
+    );
+
+    return result;
+  } catch (e) {
+    return {
+      tag: "err",
+      title: "Failed to create anchor",
+      message: "An error occurred during anchor creation.",
+      detail: unknownToString(e, "unknown error"),
+    };
+  }
+};
