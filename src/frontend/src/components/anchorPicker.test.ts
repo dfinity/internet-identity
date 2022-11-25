b79d5777421a147b826fedc73f279aa612c6a311
Nicolas Mattia <nicolas.mattia@dfinity.org>
2022-11-25 13:24:19 +0100

    Fixup anchor picker tests

--- src/frontend/src/components/anchorPicker.test.ts
+++ src/frontend/src/components/anchorPicker.test.ts
@@ -12,6 +12,7 @@ test("first anchor is focused", async () => {
   const picker = mkAnchorPicker({
     savedAnchors: [BigInt(10000), BigInt(9990042)],
     pick: () => {},
+    moreOptions: () => {},
     ...commonProps,
   });
   render(picker.template, document.body);
@@ -28,6 +29,7 @@ test("pick saved anchor", async () => {
     pick: (anchor) => {
       picked = anchor;
     },
+    moreOptions: () => {},
     ...commonProps,
   });
   render(picker.template, document.body);
@@ -38,36 +40,6 @@ test("pick saved anchor", async () => {
   ) as HTMLElement;
   elem.click();
   expect(picked).toBe(BigInt(10000));
-});
-
-test("pick custom anchor", async () => {
-  let picked: bigint | undefined = undefined;
-  const picker = mkAnchorPicker({
-    savedAnchors: [BigInt(10000), BigInt(9990042)],
-    pick: (anchor) => {
-      picked = anchor;
-    },
-    ...commonProps,
-  });
-  render(picker.template, document.body);
-  // Tick once, otherwise element isn't focused yet
-  await tick();
-  const elem = document.querySelector(
-    '[data-role="more-options"]'
-  ) as HTMLElement;
-  elem.click();
-  await tick();
-  const input = document.querySelector(
-    '[data-role="anchor-input"]'
-  ) as HTMLInputElement;
-  input.value = "1234";
-
-  const create = document.querySelector(
-    '[data-role="anchor-create"]'
-  ) as HTMLInputElement;
-  create.click();
-
-  expect(picked).toBe(BigInt(1234));
 });
 
 const tick = (): Promise<void> => new Promise((resolve) => setTimeout(resolve));
