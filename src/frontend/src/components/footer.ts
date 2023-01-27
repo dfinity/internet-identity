f578251d86956b46e57018645a6fd7875e321bdd
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-01-27 13:12:20 +0100

    Add noopened noreferrer to FAQ links

--- src/frontend/src/components/footer.ts
+++ src/frontend/src/components/footer.ts
@@ -1,20 +1,24 @@
 import { html, TemplateResult } from "lit-html";
+import { ifDefined } from "lit-html/directives/if-defined.js";
 
 export const navigationLink = ({
   labelText,
   id,
   url,
   classes,
+  rel,
 }: {
   labelText: string;
   id: string;
   url: string;
   classes: string;
+  rel?: string;
 }): TemplateResult => html`<a
   id="${id}"
   class="${classes}"
   href="${url}"
   target="_blank"
+  rel=${ifDefined(rel)}
   >${labelText}</a
 >`;
 
@@ -36,5 +40,6 @@ export const footer = html`<footer class="l-footer">
     id: "faqLink",
     url: "https://support.dfinity.org/hc/en-us/sections/8730568843412-Internet-Identity",
     classes: "t-link--discreet l-footer__elem",
+    rel: "noopener noreferrer",
   })}
 </footer>`;
