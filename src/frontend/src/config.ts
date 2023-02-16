42dd1b30f2d10cbe3dcd1d21df09b589a9eb3c0e
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-02-16 16:33:24 +0100

    Enable II on identity.internetcomputer.org
    This updates the CSP rules to allow II to be served on
    `https://identity.internetcomputer.org` in addition to
    `https://identity.ic0.app`.
    
    Note that the code now refers to
    `https://identity.internetcomputer.org`. The test infrastructure is
    also updated.

--- src/frontend/src/config.ts
+++ src/frontend/src/config.ts
@@ -1,7 +1,7 @@
 /* Global configuration for II and constants */
 
 // The official URL, without protocol
-export const OFFICIAL_II_URL_NO_PROTOCOL = "identity.ic0.app";
+export const OFFICIAL_II_URL_NO_PROTOCOL = "identity.internetcomputer.org";
 
 // The URL where the official, production II is served
 export const OFFICIAL_II_URL = "https://" + OFFICIAL_II_URL_NO_PROTOCOL;
--- src/frontend/src/test-e2e/register.test.ts
+++ src/frontend/src/test-e2e/register.test.ts
@@ -35,7 +35,7 @@ const TEST_APP_CANISTER_ID = test_app_canister_ids.test_app.local;
 const TEST_APP_CANONICAL_URL = `https://${TEST_APP_CANISTER_ID}.ic0.app`;
 const TEST_APP_NICE_URL = "https://nice-name.com";
 const REPLICA_URL = "https://ic0.app";
-const II_URL = "https://identity.ic0.app";
+import { II_URL } from "./constants";
 const ABOUT_URL = `${II_URL}/about`;
 
 const DEVICE_NAME1 = "Virtual WebAuthn device";
