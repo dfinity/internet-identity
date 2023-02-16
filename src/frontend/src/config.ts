4c14e80a4851c7278c89402cae33aad2bbb0a034
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-02-16 13:54:11 +0100

    Fixup

--- src/frontend/src/config.ts
+++ src/frontend/src/config.ts
@@ -1,7 +1,7 @@
 /* Global configuration for II and constants */
 
-// The URL where the official, production II is served
-export const OFFICIAL_II_URL = "https://identity.ic0.app";
-
 // The official URL, without protocol
 export const OFFICIAL_II_URL_NO_PROTOCOL = "identity.ic0.app";
+
+// The URL where the official, production II is served
+export const OFFICIAL_II_URL = "https://" + OFFICIAL_II_URL_NO_PROTOCOL;
