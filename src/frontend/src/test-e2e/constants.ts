51ef394f3e5af6227b9f1de4b506225818d93de6
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-02-28 10:44:51 +0100

    Enable e2e-tests on identity.ic0.app (#1268)
    * Enable e2e-tests on identity.ic0.app
    
    This makes CI run all e2e tests on https://identity.ic0.app, in addition
    to those already running on https://identity.internetcomputer.org. We do
    this to ensure both domains are supported.
    
    * Clarify intent

--- src/frontend/src/test-e2e/constants.ts
+++ src/frontend/src/test-e2e/constants.ts
@@ -7,7 +7,8 @@ export const TEST_APP_CANONICAL_URL = `https://${TEST_APP_CANISTER_ID}.icp0.io`;
 export const TEST_APP_CANONICAL_URL_LEGACY = `https://${TEST_APP_CANISTER_ID}.ic0.app`;
 export const TEST_APP_NICE_URL = "https://nice-name.com";
 export const REPLICA_URL = "https://icp-api.io";
-export const II_URL = "https://identity.internetcomputer.org";
+export const II_URL =
+  process.env.II_URL ?? "https://identity.internetcomputer.org";
 export const ABOUT_URL = `${II_URL}/about`;
 
 export const DEVICE_NAME1 = "Virtual WebAuthn device";
