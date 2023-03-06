689a6d0e7907532dbb063ad088793946d287ea80
David Aerne <meodai@gmail.com>
2023-03-06 12:32:34 +0100

    add a comment block for the irregularities

--- src/frontend/src/components/irregularity.ts
+++ src/frontend/src/components/irregularity.ts
@@ -8,6 +8,20 @@ interface irregularityProps {
   closeFn?: () => void;
 }
 
+/**
+ * Irregularity component
+ *
+ * To use when something is out of the ordinary. For example, when a user has
+ * not yet completed a task, or when a user has not yet verified their email
+ * address. Or if there is an error with a form. It can also be used as a tost.
+ *
+ * It is a generic to use for all irregularities.
+ *
+ * @param {string} message - The message to display
+ * @param {string[]} additionalClasses - Additional classes to add to the component
+ * @param {string} errorType - The type of irregularity. Can be "warning", "info" or "error"
+ * @param {function} closeFn - When provided, the irregularity will be closable.
+ **/
 export const irregularity = ({
   message,
   additionalClasses = [],
