dc2544d831640abfff6494fb1ed0408eea471c7a
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-03-10 13:57:13 +0100

    Format ts

--- src/frontend/src/flows/recovery/displaySeedPhrase.ts
+++ src/frontend/src/flows/recovery/displaySeedPhrase.ts
@@ -37,64 +37,72 @@ const displaySeedPhraseTemplate = ({
 
   const pageContentSlot = html`
     <article>
-    <hgroup>
-      <h1 class="t-title t-title--main">Store Recovery Phrase</h1>
-      <p class="t-lead">
-         If you lose access to your devices, use your recovery phrase to access your Internet Identity.
-      </p>
-    </hgroup>
-    <h2 class="t-title l-stack">Your recovery phrase</h2>
-    <div>
-      <output
-        class="c-input c-input--textarea c-input--textarea-narrow c-input--readonly c-input--icon"
-        ><ol translate="no"
-        class="c-list c-list--recovery">
-          ${recoveryWords.map(
-            (word, i) =>
-              html`<li style="--i: ${i / recoveryWords.length}">${word}</li>`
-          )}
-        </ol>
-        <i
+      <hgroup>
+        <h1 class="t-title t-title--main">Store Recovery Phrase</h1>
+        <p class="t-lead">
+          If you lose access to your devices, use your recovery phrase to access
+          your Internet Identity.
+        </p>
+      </hgroup>
+      <h2 class="t-title l-stack">Your recovery phrase</h2>
+      <div>
+        <output
+          class="c-input c-input--textarea c-input--textarea-narrow c-input--readonly c-input--icon"
+          ><ol translate="no" class="c-list c-list--recovery">
+            ${recoveryWords.map(
+              (word, i) =>
+                html`<li style="--i: ${i / recoveryWords.length}">${word}</li>`
+            )}
+          </ol>
+          <i
             ${ref(phraseCopyElement)}
             @click=${() => copyPhrase()}
-          aria-label="Copy phrase to clipboard"
-          title="Copy phrase to clipboard"
-          tabindex="0"
-          id="seedCopy"
-          class="c-button__icon c-input__icon"
+            aria-label="Copy phrase to clipboard"
+            title="Copy phrase to clipboard"
+            tabindex="0"
+            id="seedCopy"
+            class="c-button__icon c-input__icon"
           >
             <span>Copy</span>
-            ${copyIcon}
-            ${checkmarkIcon}
-          </i
-        ></output
-      >
-    </div>
+            ${copyIcon} ${checkmarkIcon}
+          </i></output
+        >
+      </div>
 
-    <p class="t-paragraph">
-      Securely store your recovery phrase, and do not share it with anyone!
-    </p>
+      <p class="t-paragraph">
+        Securely store your recovery phrase, and do not share it with anyone!
+      </p>
 
-    <div class="l-stack">
-      <input ${ref(checkbox)} type="checkbox" id="ack-checkbox" name="scales"
-        @change=${() =>
-          withRef(continueButton, (continueButton) =>
-            withRef(checkbox, (checkbox) => {
-              continueButton.disabled = !checkbox.checked;
-            })
-          )}
-      />
-      <label for="ack-checkbox" class="t-strong">I have stored my recovery phrase.</label>
-    </div>
-    <div class="l-stack">
-      <button ${ref(continueButton)}
-      @click=${() => onContinue()}
-      id="displaySeedPhraseContinue" class="c-button" disabled>
-        Continue
-      </button>
-    </div>
-  </article>
-`;
+      <div class="l-stack">
+        <input
+          ${ref(checkbox)}
+          type="checkbox"
+          id="ack-checkbox"
+          name="scales"
+          @change=${() =>
+            withRef(continueButton, (continueButton) =>
+              withRef(checkbox, (checkbox) => {
+                continueButton.disabled = !checkbox.checked;
+              })
+            )}
+        />
+        <label for="ack-checkbox" class="t-strong"
+          >I have stored my recovery phrase.</label
+        >
+      </div>
+      <div class="l-stack">
+        <button
+          ${ref(continueButton)}
+          @click=${() => onContinue()}
+          id="displaySeedPhraseContinue"
+          class="c-button"
+          disabled
+        >
+          Continue
+        </button>
+      </div>
+    </article>
+  `;
 
   return mainWindow({
     isWideContainer: true,
--- src/frontend/src/test-e2e/views.ts
+++ src/frontend/src/test-e2e/views.ts
@@ -126,7 +126,6 @@ export class RecoveryMethodSelectorView extends View {
   }
 
   async getSeedPhrase(): Promise<string> {
-
     // This tries to read the recovery phrase by first copying it to the clipboard.
 
     await this.copySeedPhrase();
@@ -137,23 +136,25 @@ export class RecoveryMethodSelectorView extends View {
 
     // First, create a new textarea element where the phrase will be pasted
     await this.browser.execute(() => {
-        const elem = document.createElement("textarea");
-        elem.setAttribute("id", "my-paste-area");
-        document.body.prepend(elem);
+      const elem = document.createElement("textarea");
+      elem.setAttribute("id", "my-paste-area");
+      document.body.prepend(elem);
     });
 
     // Select the element and mock "Ctrl + V" for pasting the clipboard content into said element
     await this.browser.$("#my-paste-area").click();
-    await this.browser.keys(['Control', 'v'])
+    await this.browser.keys(["Control", "v"]);
 
     // Read the element's value and clean up
     const seedPhrase = await this.browser.execute(() => {
-        const elem = document.querySelector("#my-paste-area") as HTMLTextAreaElement;
-        // NOTE: we could also query the value with wdio's $(..).getValue(), but since we have
-        // the element here might as well.
-        const seedPhrase = elem.value!;
-        elem.remove();
-        return seedPhrase;
+      const elem = document.querySelector(
+        "#my-paste-area"
+      ) as HTMLTextAreaElement;
+      // NOTE: we could also query the value with wdio's $(..).getValue(), but since we have
+      // the element here might as well.
+      const seedPhrase = elem.value!;
+      elem.remove();
+      return seedPhrase;
     });
 
     return seedPhrase;
