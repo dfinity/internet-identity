97b89011b947a8ed828be10ade009e8333d220dd
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-03-10 15:05:22 +0100

    Improve documentation

--- src/internet_identity/src/anchor_management.rs
+++ src/internet_identity/src/anchor_management.rs
@@ -51,6 +51,24 @@ pub fn get_anchor_info(anchor_number: AnchorNumber) -> IdentityAnchorInfo {
     })
 }
 
+/// Handles all the bookkeeping required after a successful anchor operation:
+/// * Adds the operation to the archive buffer
+/// * Increments the anchor operation counter
+/// * Records anchor activity
+pub fn anchor_operation_bookkeeping(
+    anchor_number: AnchorNumber,
+    operation: Operation,
+    previous_activity: Option<Timestamp>,
+) {
+    archive_operation(anchor_number, caller(), operation);
+    state::usage_metrics_mut(|metrics| {
+        metrics.anchor_operation_counter += 1;
+    });
+    active_anchor_stats::update_active_anchors_stats(previous_activity);
+}
+
+/// Adds a device to the given anchor and returns the operation to be archived.
+/// Panics if this operation violates anchor constraints (see [Anchor]).
 pub fn add(anchor: &mut Anchor, device_data: DeviceData) -> Operation {
     let new_device = Device::from(device_data);
     anchor
@@ -62,6 +80,10 @@ pub fn add(anchor: &mut Anchor, device_data: DeviceData) -> Operation {
     }
 }
 
+/// Updates a device of the given anchor and returns the operation to be archived.
+/// Panics if
+/// * the device to be updated does not exist
+/// * the operation violates anchor constraints (see [Anchor])
 pub fn update(anchor: &mut Anchor, device_key: DeviceKey, device_data: DeviceData) -> Operation {
     let Some(existing_device) = anchor.device(&device_key) else {
         trap("Could not find device to update, check device key")
@@ -81,6 +103,10 @@ pub fn update(anchor: &mut Anchor, device_key: DeviceKey, device_data: DeviceDat
     }
 }
 
+/// Replaces a device of the given anchor with another and returns the operation to be archived.
+/// Panics if
+/// * the device to be replaced does not exist
+/// * the operation violates anchor constraints (see [Anchor])
 pub fn replace(anchor: &mut Anchor, old_device: DeviceKey, new_device: DeviceData) -> Operation {
     anchor
         .remove_device(&old_device)
@@ -96,28 +122,14 @@ pub fn replace(anchor: &mut Anchor, old_device: DeviceKey, new_device: DeviceDat
     }
 }
 
+/// Removes a device of the given anchor and returns the operation to be archived.
+/// Panics if the device to be removed does not exist
 pub fn remove(anchor: &mut Anchor, device_key: DeviceKey) -> Operation {
     anchor
         .remove_device(&device_key)
         .unwrap_or_else(|err| trap(&format!("failed to remove device: {err}")));
 
     Operation::RemoveDevice { device: device_key }
-}
-
-/// Handles all the bookkeeping required after a successful anchor operation:
-/// * Add the operation to the archive buffer
-/// * Increment the anchor operation counter
-/// * Record anchor activity
-pub fn anchor_operation_bookkeeping(
-    anchor_number: AnchorNumber,
-    operation: Operation,
-    previous_activity: Option<Timestamp>,
-) {
-    archive_operation(anchor_number, caller(), operation);
-    state::usage_metrics_mut(|metrics| {
-        metrics.anchor_operation_counter += 1;
-    });
-    active_anchor_stats::update_active_anchors_stats(previous_activity);
 }
 
 /// Updates the device on the anchor to reflect the current usage.
