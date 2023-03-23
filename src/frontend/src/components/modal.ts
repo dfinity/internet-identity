c42354aa7787beb6b40cadf179633d279c9dadc8
David Aerne <meodai@gmail.com>
2023-03-23 15:06:43 +0100

    use references instead of querySelectors

--- src/frontend/src/components/modal.ts
+++ src/frontend/src/components/modal.ts
@@ -1,4 +1,5 @@
 import { html, TemplateResult, render } from "lit-html";
+import { createRef, ref, Ref } from "lit-html/directives/ref.js";
 
 /**
  * creates a modal that contains arbitrary content
@@ -8,29 +9,47 @@ import { html, TemplateResult, render } from "lit-html";
 export const createModal = ({
   slot,
   id,
+  closeButtonRef,
+  submitButtonRef,
 }: {
   slot: TemplateResult;
   id: string;
+  closeButtonRef: Ref<HTMLButtonElement>;
+  submitButtonRef: Ref<HTMLButtonElement>;
 }): HTMLDialogElement => {
   const modalHtml = html`
     <form method="dialog" class="c-modal__content c-card c-card--modal">
       <div class="c-modal__inner">
-        <span class="c-modal__close" data-closemodal>&times;</span>
+        <button
+          class="c-modal__close"
+          data-closemodal
+          aria-label="Close Modal"
+          ${ref(closeButtonRef)}
+        >
+          &times;
+        </button>
         ${slot}
         <div class="c-modal__footer">
-          <button type="submit" class="c-button c-button--primary">OK</button>
+          <button
+            type="submit"
+            class="c-button c-button--primary"
+            ${ref(submitButtonRef)}
+          >
+            OK
+          </button>
         </div>
       </div>
     </form>
   `;
 
-  const $modal = document.createElement("dialog");
-  $modal.id = id;
-  $modal.classList.add("c-modal");
-  $modal.setAttribute("aria-modal", "true");
+  const modalElement = document.createElement("dialog");
+  modalElement.id = id;
+  modalElement.classList.add("c-modal");
+  modalElement.setAttribute("aria-modal", "true");
 
-  render(modalHtml, $modal);
-  return $modal;
+  render(modalHtml, modalElement);
+
+  return modalElement;
 };
 
 export const modal = ({
@@ -40,26 +59,31 @@ export const modal = ({
   slot: TemplateResult;
   id: string;
 }): Promise<void> => {
-  const $modal = createModal({ slot, id });
-  document.body.appendChild($modal);
+  const closeButtonRef: Ref<HTMLButtonElement> = createRef();
+  const submitButtonRef: Ref<HTMLButtonElement> = createRef();
+
+  const modalElement = createModal({
+    slot,
+    id,
+    closeButtonRef,
+    submitButtonRef,
+  });
+  document.body.appendChild(modalElement);
 
   return new Promise((resolve) => {
     // open modal
-    $modal.showModal();
-    const $modalClose = $modal.querySelector(
-      "[data-closemodal]"
-    ) as HTMLElement;
-    const $modalSubmit = $modal.querySelector(
-      "button[type=submit]"
-    ) as HTMLButtonElement;
+    modalElement.showModal();
+
+    const closeButton = closeButtonRef.value!;
+    const submitButton = submitButtonRef.value!;
 
     const close = () => {
       resolve();
-      $modal.close();
-      $modal.remove();
+      modalElement.close();
+      modalElement.remove();
     };
 
-    $modalClose.onclick = close;
-    $modalSubmit.onclick = close;
+    closeButton.onclick = close;
+    submitButton.onclick = close;
   });
 };
