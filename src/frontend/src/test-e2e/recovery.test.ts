6e840e3eda3a93a1bb73ebef295aefcf7a2a36e0
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-03-23 12:53:29 +0100

    Add test for recovering after reset

--- src/frontend/src/test-e2e/recovery.test.ts
+++ src/frontend/src/test-e2e/recovery.test.ts
@@ -53,6 +53,47 @@ test("Reset unprotected recovery phrase", async () => {
   });
 }, 300_000);
 
+test("Recover access, after reset", async () => {
+  await runInBrowser(async (browser: WebdriverIO.Browser) => {
+    await addVirtualAuthenticator(browser);
+    await browser.url(II_URL);
+    const userNumber = await FLOWS.registerNewIdentityWelcomeView(
+      DEVICE_NAME1,
+      browser
+    );
+    const mainView = new MainView(browser);
+    await mainView.waitForDeviceDisplay(DEVICE_NAME1);
+
+    // Create a phrase
+    const _seedPhrase = await FLOWS.addRecoveryMechanismSeedPhrase(browser);
+    await mainView.waitForDisplay();
+
+    // Reset the phrase
+    // Ensure the settings dropdown is in view
+    await browser.execute("window.scrollTo(0, document.body.scrollHeight)");
+    await mainView.reset(RECOVERY_PHRASE_NAME);
+    await browser.acceptAlert();
+    const seedPhrase = await FLOWS.readSeedPhrase(browser);
+    await mainView.waitForDisplay();
+
+    // Logout
+    await mainView.logout();
+
+    // Recover with new phrase
+    const welcomeView = new WelcomeView(browser);
+    await welcomeView.recover();
+    const recoveryView = new RecoverView(browser);
+    await recoveryView.waitForDisplay();
+    await recoveryView.enterIdentityAnchor(userNumber);
+    await recoveryView.continue();
+    await recoveryView.waitForSeedInputDisplay();
+    await recoveryView.enterSeedPhrase(seedPhrase);
+    await recoveryView.enterSeedPhraseContinue();
+    await recoveryView.skipDeviceEnrollment();
+    await mainView.waitForDeviceDisplay(DEVICE_NAME1);
+  });
+}, 300_000);
+
 test("Reset unprotected recovery phrase, when authenticated with phrase", async () => {
   await runInBrowser(async (browser: WebdriverIO.Browser) => {
     await addVirtualAuthenticator(browser);
