cc849ff4a8f42d66c7f0daf38a5000493dc531b5
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-03-30 09:11:46 +0200

    Replace __get_candid_interface_tmp_hack with metadata section (#1373)
    This replaces the candid interface hack with the proper public
    metadata section added with `ic-wasm`.

--- demos/test-app/build.sh
+++ demos/test-app/build.sh
@@ -9,3 +9,4 @@ npm ci
 npm run build
 cargo build --release --target wasm32-unknown-unknown
 ic-wasm "target/wasm32-unknown-unknown/release/test_app.wasm" -o "./test_app.wasm" shrink
+ic-wasm test_app.wasm -o test_app.wasm metadata candid:service -f test_app.did -v public
