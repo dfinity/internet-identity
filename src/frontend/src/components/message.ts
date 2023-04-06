9e48f04487b576d29ac78ce53f2f8685bb89d278
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-04-06 19:22:21 +0200

    Extract index message and copy (#1406)
    This extracts a `showMessage` component/function from the index and
    extracts the copy too.
    
    The new `showMessage` is also reused in other places that need to
    display a centered message, and the `data-role` is fixed (in index.ts,
    the attribute had been copy/pasted but was incorrectly suggesting
    some authorization was successful).

---
+++ src/frontend/src/components/message.ts
@@ -0,0 +1,33 @@
+import { html, TemplateResult } from "lit-html";
+import { ifDefined } from "lit-html/directives/if-defined.js";
+import { renderPage } from "../utils/lit-html";
+
+const showMessageTemplate = ({
+  message,
+  role,
+}: {
+  message: string;
+  role?: string;
+}): TemplateResult => {
+  return html`<h1
+    style="position: absolute; max-width: 100%; top: 50%; transform: translate(0, -50%);"
+    data-role=${ifDefined(role)}
+  >
+    ${message}
+  </h1>`;
+};
+
+export const showMessagePage = renderPage(showMessageTemplate);
+
+// Show a simple, centered message (with optional "data-role" attribute)
+export const showMessage = ({
+  message,
+  role,
+}: {
+  message: string;
+  role?: string;
+}): void =>
+  showMessagePage({
+    message,
+    role,
+  });
