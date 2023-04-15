040b4742d176ce122fd3e80c66e6be2109edd413
David Dal Busco <david.dalbusco@outlook.com>
2023-04-15 09:25:03 +0200

    fix: css.gz support

--- src/internet_identity/src/assets.rs
+++ src/internet_identity/src/assets.rs
@@ -171,8 +171,8 @@ fn file_to_asset_path(asset: &File) -> String {
             .chars()
             .take(file_path.len() - ".html".len())
             .collect()
-    } else if file_path.ends_with(".js.gz") {
-        // drop .gz for .js.gz files (i.e. maps **/<foo>.js.gz to **/<foo>.js)
+    } else if file_path.ends_with(".gz") {
+        // drop .gz for .gz files (i.e. maps **/<foo>.js.gz to **/<foo>.js or **/<foo>.css.gz to **/<foo>.css)
         file_path = file_path
             .chars()
             .take(file_path.len() - ".gz".len())
