f23095793f7a0bc3dad36179075d2507d84449b6
David Dal Busco <david.dalbusco@outlook.com>
2023-04-20 16:57:53 +0200

    test: use body instead

--- src/frontend/src/flows/addDevice/manage/addDeviceSuccess.test.ts
+++ src/frontend/src/flows/addDevice/manage/addDeviceSuccess.test.ts
@@ -2,20 +2,17 @@ import { I18n } from "../../../i18n";
 import { addDeviceSuccessPage } from "./addDeviceSuccess";
 
 describe("addDeviceSuccess", () => {
-  beforeEach(() => {
-    const pageContent = document.createElement("div");
-    pageContent.setAttribute("id", "pageContent");
-    document.body.appendChild(pageContent);
-  });
-
   it("should resolve promise on click to continue", () => {
     const onContinue = jest.fn();
 
-    addDeviceSuccessPage({
-      i18n: new I18n(),
-      deviceAlias: "Test device alias",
-      onContinue,
-    });
+    addDeviceSuccessPage(
+      {
+        i18n: new I18n(),
+        deviceAlias: "Test device alias",
+        onContinue,
+      },
+      document.body
+    );
 
     document.querySelector<HTMLButtonElement>('[data-action="next"]')?.click();
 
