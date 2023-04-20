c793b32fc8bafaeb04ac19ae483d9ab9b6c25089
David Dal Busco <david.dalbusco@outlook.com>
2023-04-20 16:13:40 +0200

    test: addDeviceSuccessPage

--- src/frontend/src/flows/addDevice/manage/addDeviceSuccess.test.ts
+++ src/frontend/src/flows/addDevice/manage/addDeviceSuccess.test.ts
@@ -1,23 +1,18 @@
-import { renderAddDeviceSuccess } from "./addDeviceSuccess";
+import { I18n } from "../../../i18n";
+import { addDeviceSuccessPage } from "./addDeviceSuccess";
 
 describe("addDeviceSuccess", () => {
-  const deviceAlias = "Test device alias";
+  it("should resolve promise on click to continue", () => {
+    const onContinue = jest.fn();
 
-  beforeAll(() => {
-    const pageContent = document.createElement("div");
-    pageContent.setAttribute("id", "pageContent");
-    document.body.appendChild(pageContent);
-  });
-
-  it("should resolve promise on click to continue", async () => {
-    const result = renderAddDeviceSuccess({
-      deviceAlias,
+    addDeviceSuccessPage({
+      i18n: new I18n(),
+      deviceAlias: "Test device alias",
+      onContinue,
     });
 
-    document
-      .querySelector<HTMLButtonElement>('[data-action="next"]')
-      ?.click();
+    document.querySelector<HTMLButtonElement>('[data-action="next"]')?.click();
 
-    await result;
+    expect(onContinue).toHaveBeenCalledTimes(1);
   });
 });
