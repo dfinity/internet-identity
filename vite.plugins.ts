34d20ce82108b8e9a7f1d49d82ad26d1b3958742
David Dal Busco <david.dalbusco@outlook.com>
2023-04-24 11:19:40 +0200

    Replace webpack with vitejs (#1461)
    * build: vitejs
    
    * chore: redo as in main
    
    * Update vite.config.ts
    
    Co-authored-by: Nicolas Mattia <nicolas.mattia@dfinity.org>
    
    * chore: remove envDir
    
    * Update vite.config.ts
    
    Co-authored-by: Nicolas Mattia <nicolas.mattia@dfinity.org>
    
    * Update vite.plugins.ts
    
    Co-authored-by: Nicolas Mattia <nicolas.mattia@dfinity.org>
    
    ---------
    
    Co-authored-by: Nicolas Mattia <nicolas.mattia@dfinity.org>

---
+++ vite.plugins.ts
@@ -0,0 +1,60 @@
+import { assertNonNullish } from "@dfinity/utils";
+import { readFileSync } from "fs";
+
+/**
+ * Read the II canister ID from dfx's local state
+ */
+const readCanisterId = (): string => {
+  const canisterIdsJsonFile = "./.dfx/local/canister_ids.json";
+
+  try {
+    const {
+      internet_identity: { local: canisterId },
+    } = JSON.parse(readFileSync(canisterIdsJsonFile, "utf-8"));
+
+    assertNonNullish(
+      canisterId,
+      `Could not get canister ID from ${canisterIdsJsonFile}`
+    );
+
+    console.log("Read canister ID:", canisterId);
+
+    return canisterId;
+  } catch (e) {
+    throw Error(`Could not get canister ID from ${canisterIdsJsonFile}: ${e}`);
+  }
+};
+
+/**
+ * Inject the II canister ID as a <script /> tag in index.html for local development.
+ */
+export const injectCanisterIdPlugin = (): {
+  name: "html-transform";
+  transformIndexHtml(html: string): string;
+} => ({
+  name: "html-transform",
+  transformIndexHtml(html): string {
+    return html.replace(
+      `<script id="setupJs"></script>`,
+      `<script data-canister-id="${readCanisterId()}" id="setupJs"></script>`
+    );
+  },
+});
+
+/**
+ * Remove the <script /> that loads the index.js for production build.
+ * A script loader is injected by the backend.
+ */
+export const stripInjectJsScript = (): {
+  name: "html-transform";
+  transformIndexHtml(html: string): string;
+} => ({
+  name: "html-transform",
+  transformIndexHtml(html): string {
+    const match = `<script type="module" crossorigin src="/index.js"></script>`;
+    if (!html.includes(match)) {
+      throw new Error("Expecting script tag to replace, found none");
+    }
+    return html.replace(match, ``);
+  },
+});
