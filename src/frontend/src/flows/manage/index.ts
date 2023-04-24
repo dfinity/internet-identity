557f5bc19cd1eebbd5997c571fb4654ef1df0c09
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-04-24 10:22:32 +0200

    Clean up

--- src/frontend/src/flows/manage/index.ts
+++ src/frontend/src/flows/manage/index.ts
@@ -455,11 +455,10 @@ const unknownError = (): Error => {
   return new Error("Unknown error");
 };
 
-// Return a shuffled version of the array. Adapted from
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-// to avoid shuffling in place.
+// Return a shuffled version of the array. Adapted from https://stackoverflow.com/a/12646864 to
+// avoid shuffling in place.
 const shuffleArray = <T>(array_: T[]): T[] => {
-  const [...array] = array_;
+  const array = [...array_];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
