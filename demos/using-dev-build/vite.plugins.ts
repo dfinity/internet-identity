7bd646160d3d2177c5a7ee95b801f6e6d5cc4bca
David Dal Busco <david.dalbusco@outlook.com>
2023-04-25 11:10:08 +0200

    build: network

--- demos/using-dev-build/vite.plugins.ts
+++ demos/using-dev-build/vite.plugins.ts
@@ -1,17 +1,22 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
+// npm run dev = local
+// npm run build = local
+// dfx deploy = local
+// dfx deploy --network ic = ic
+const network = process.env.DFX_NETWORK ?? ("local" as const);
+
 export const readCanisterIds = (): Record<string, string> => {
-  const canisterIdsJsonFile = join(
-    process.cwd(),
-    ".dfx",
-    "local",
-    "canister_ids.json"
-  );
+  const canisterIdsJsonFile =
+    network === "ic"
+      ? join(process.cwd(), "canister_ids.json")
+      : join(process.cwd(), ".dfx", "local", "canister_ids.json");
 
   try {
     type Details = {
-      local: string;
+      ic?: string;
+      local?: string;
     };
 
     const config: Record<string, Details> = JSON.parse(
@@ -23,7 +28,8 @@ export const readCanisterIds = (): Record<string, string> => {
 
       return {
         ...acc,
-        [`${canisterName.toUpperCase()}_CANISTER_ID`]: canisterDetails.local,
+        [`${canisterName.toUpperCase()}_CANISTER_ID`]:
+          canisterDetails[network as keyof Details],
       };
     }, {});
   } catch (e) {
