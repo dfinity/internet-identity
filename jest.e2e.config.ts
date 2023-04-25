806f6725626b9635a7e8423b037bc37fb3109c4a
David Dal Busco <david.dalbusco@outlook.com>
2023-04-25 09:11:56 +0200

    build: aliases for e2e test as well

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -1,5 +1,9 @@
 export default {
   extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^\\$generated/(.*)$": "<rootDir>/src/frontend/generated/$1",
+    "^\\$root/(.*)$": "<rootDir>/src/frontend/src/$1",
+  },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
