783248d71bc1903f6c3e8114f07503130cb15290
David Dal Busco <david.dalbusco@outlook.com>
2023-05-09 14:53:48 +0200

    test: vitest for e2e too

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,31 @@
-import { configDefaults, defineConfig } from 'vitest/config'
-import {resolve} from "path";
+import { resolve } from "path";
+import { UserConfig } from "vite";
+import { configDefaults, defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }: UserConfig): UserConfig => {
+  return {
     resolve: {
-        alias: {
-            // Polyfill stream for the browser. e.g. needed in "Recovery Phrase" features.
-            stream: "stream-browserify",
-            // Custom alias we are using to shorten and make absolute the imports
-            $generated: resolve(__dirname, "src/frontend/generated"),
-            $src: resolve(__dirname, "src/frontend/src"),
-        },
+      alias: {
+        // Polyfill stream for the browser. e.g. needed in "Recovery Phrase" features.
+        stream: "stream-browserify",
+        // Custom alias we are using to shorten and make absolute the imports
+        $generated: resolve(__dirname, "src/frontend/generated"),
+        $src: resolve(__dirname, "src/frontend/src"),
+      },
     },
     test: {
-        environment: 'jsdom',
-        exclude: [...configDefaults.exclude, 'src/frontend/src/test-e2e/**'],
-        globals: true,
-        watch: false,
-        setupFiles: './src/frontend/test-setup.ts'
-    }
-})
+      environment: "jsdom",
+      exclude: [
+        ...configDefaults.exclude,
+        ...(mode === "test" ? ["src/frontend/src/test-e2e/**"] : []),
+      ],
+      include: [
+        ...configDefaults.include,
+        ...(mode === "e2e" ? ["src/frontend/src/test-e2e/**/*.test.ts"] : []),
+      ],
+      globals: true,
+      watch: false,
+      setupFiles: "./src/frontend/test-setup.ts",
+    },
+  };
+});
