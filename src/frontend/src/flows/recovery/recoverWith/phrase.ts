8346352bc5493738aa389fdf645a3b9e4c0fc15d
David Dal Busco <david.dalbusco@outlook.com>
2023-05-09 10:28:11 +0200

    chore: redo

--- src/frontend/src/flows/recovery/recoverWith/phrase.ts
+++ src/frontend/src/flows/recovery/recoverWith/phrase.ts
@@ -199,14 +199,14 @@ export const wordTemplate = ({
     ${asyncReplace(icon)}
     <input
       @paste=${(e: ClipboardEvent) =>
-        withElement(e, ($event, element) => {
+        withElement(e, (event, element) => {
           e.preventDefault();
 
           // Get the text pasted
-          if ($event.clipboardData === null) {
+          if (event.clipboardData === null) {
             return;
           }
-          const text = $event.clipboardData.getData("text");
+          const text = event.clipboardData.getData("text");
 
           // Split the text into words, dropping (leading) white spaces, empty strings (from e.g. double spaces), etc
           const [word = undefined, ...rest] = text
