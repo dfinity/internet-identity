b2ac901384093c990052e89f1f5a479adaf0204e
David Dal Busco <david.dalbusco@outlook.com>
2023-05-10 08:03:24 +0200

    feat: open dropdown on click

--- src/frontend/src/flows/manage/settingsDropdown.ts
+++ src/frontend/src/flows/manage/settingsDropdown.ts
@@ -1,22 +1,42 @@
 import { dropdownIcon } from "$src/components/icons";
+import { handleKeyPress } from "$src/utils/keyboard.utils";
+import { Chan } from "$src/utils/utils";
 import { html, TemplateResult } from "lit-html";
+import { asyncReplace } from "lit-html/directives/async-replace.js";
 
-export const settingsDropdown = ({
-  alias,
-  id,
-  settings,
-}: {
+interface SettingsDropdown {
   alias: string;
   id: string;
   settings: { fn: () => void; caption: string; action: string }[];
-}): TemplateResult => {
+}
+
+const mkChasm = ({ alias, id, settings }: SettingsDropdown): TemplateResult => {
+  /* Toggle the chasm open/closed */
+  const ariaExpanded = new Chan(false);
+  const chasmToggle = () => ariaExpanded.send(!ariaExpanded.latest);
+
+  const backdrop = ariaExpanded.map((expanded) =>
+    expanded
+      ? html`<div
+          class="c-dropdown__backdrop"
+          role="button"
+          tabindex="-1"
+          @click=${() => chasmToggle()}
+          @keypress=${($event: KeyboardEvent) =>
+            handleKeyPress({ $event, callback: chasmToggle })}
+        />`
+      : undefined
+  );
+
   return html` <div class="c-action-list__action c-dropdown">
+    ${asyncReplace(backdrop)}
     <button
       class="c-dropdown__trigger c-action-list__action"
-      aria-expanded="false"
+      aria-expanded="${asyncReplace(ariaExpanded)}"
       aria-controls="dropdown-${id}"
       aria-label="Open settings"
       data-device=${alias}
+      @click=${() => chasmToggle()}
     >
       ${dropdownIcon}
     </button>
@@ -38,3 +58,6 @@ export const settingsDropdown = ({
     </ul>
   </div>`;
 };
+
+export const settingsDropdown = (params: SettingsDropdown): TemplateResult =>
+  mkChasm(params);
