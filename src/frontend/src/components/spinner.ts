750bd1538e2b6eb23bb091e64e0e9d3f8cb39e13
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-05-15 17:15:28 +0200

    Extract loading page from authz

---
+++ src/frontend/src/components/spinner.ts
@@ -0,0 +1,26 @@
+import { spinner } from "$src/components/icons";
+import { renderPage, TemplateElement } from "$src/utils/lit-html";
+import { html, TemplateResult } from "lit-html";
+
+const showSpinnerTemplate = ({
+  message,
+}: {
+  message: TemplateElement;
+}): TemplateResult => {
+  return html`
+    <div class="l-container c-card c-card--highlight t-centered">
+      <div class="c-spinner-wrapper">
+        <div class="c-spinner">${spinner}</div>
+      </div>
+      <p class="t-lead t-paragraph l-stack">${message}</p>
+    </div>
+  `;
+};
+
+export const showSpinnerPage = renderPage(showSpinnerTemplate);
+
+// Show a simple, centered message (with optional "data-role" attribute)
+export const showSpinner = ({ message }: { message: TemplateElement }): void =>
+  showSpinnerPage({
+    message,
+  });
