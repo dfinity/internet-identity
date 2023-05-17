6af8ccf871289e9b7e040a79f14da7c873cbcbcb
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-05-17 14:52:46 +0200

    Remove test dependency on framework testdata

--- src/internet_identity/tests/integration/v2_api/identity_info.rs
+++ src/internet_identity/tests/integration/v2_api/identity_info.rs
@@ -5,15 +5,14 @@ use canister_tests::api::internet_identity as api;
 use canister_tests::api::internet_identity::api_v2;
 use canister_tests::flows;
 use canister_tests::framework::{
-    device_data_1, device_data_2, env, expect_user_error_with_message, install_ii_canister,
-    principal_1, recovery_device_data_1, recovery_device_data_2, time, II_WASM,
+    env, expect_user_error_with_message, install_ii_canister, time, II_WASM,
 };
 use ic_cdk::api::management_canister::main::CanisterId;
 use ic_test_state_machine_client::ErrorCode::CanisterCalledTrap;
 use ic_test_state_machine_client::{CallError, StateMachine};
 use internet_identity_interface::internet_identity::types::{
     AuthnMethodData, AuthnMethodRegistration, DeviceData, IdentityInfoResponse, IdentityNumber,
-    KeyType, MetadataEntry,
+    KeyType, MetadataEntry, Purpose,
 };
 use regex::Regex;
 use serde_bytes::ByteBuf;
@@ -83,26 +82,28 @@ fn should_provide_authn_registration() -> Result<(), CallError> {
     const AUTHN_METHOD_REGISTRATION_TIMEOUT: u64 = Duration::from_secs(900).as_nanos() as u64; // 15 minutes
     let env = env();
     let canister_id = install_ii_canister(&env, II_WASM.clone());
-    let identity_number = flows::register_anchor(&env, canister_id);
-    let device = DeviceData {
+    let device1 = sample_devices().remove(0);
+    let identity_number = flows::register_anchor_with_device(&env, canister_id, &device1);
+    let device2 = DeviceData {
+        pubkey: ByteBuf::from([55; 32]),
         metadata: Some(HashMap::from([(
             "recovery_metadata_1".to_string(),
             MetadataEntry::String("recovery data 1".to_string()),
         )])),
-        ..recovery_device_data_1()
+        ..device1.clone()
     };
 
-    api::enter_device_registration_mode(&env, canister_id, principal_1(), identity_number)?;
+    api::enter_device_registration_mode(&env, canister_id, device1.principal(), identity_number)?;
     api::add_tentative_device(
         &env,
         canister_id,
         Principal::anonymous(),
         identity_number,
-        &device,
+        &device2,
     )?;
 
     let Some(IdentityInfoResponse::Ok(identity_info)) =
-        api_v2::get_identity_info(&env, canister_id, principal_1(), identity_number)? else {
+        api_v2::get_identity_info(&env, canister_id, device1.principal(), identity_number)? else {
         panic!("Expected identity info to be returned");
     };
 
@@ -110,7 +111,7 @@ fn should_provide_authn_registration() -> Result<(), CallError> {
         identity_info.authn_method_registration,
         Some(AuthnMethodRegistration {
             expiration: time(&env) + AUTHN_METHOD_REGISTRATION_TIMEOUT,
-            authn_method: Some(AuthnMethodData::from(device))
+            authn_method: Some(AuthnMethodData::from(device2))
         })
     );
     Ok(())
@@ -164,16 +165,18 @@ fn sample_devices() -> Vec<DeviceData> {
             "some_key".to_string(),
             MetadataEntry::String("some data".to_string()),
         )])),
-        ..device_data_1()
+        origin: Some("https://some.origin".to_string()),
+        ..DeviceData::auth_test_device()
     };
 
     let device2 = DeviceData {
+        pubkey: ByteBuf::from([1; 32]),
+        credential_id: Some(ByteBuf::from([12; 32])),
         metadata: Some(HashMap::from([(
             "different_key".to_string(),
             MetadataEntry::String("other data".to_string()),
         )])),
-        credential_id: None,
-        ..device_data_2()
+        ..DeviceData::auth_test_device()
     };
 
     vec![device1, device2]
@@ -181,17 +184,22 @@ fn sample_devices() -> Vec<DeviceData> {
 
 fn sample_recovery_devices() -> Vec<DeviceData> {
     let device1 = DeviceData {
+        pubkey: ByteBuf::from([2; 32]),
+        purpose: Purpose::Recovery,
+        key_type: KeyType::SeedPhrase,
         metadata: Some(HashMap::from([(
             "recovery_metadata_1".to_string(),
             MetadataEntry::String("recovery data 1".to_string()),
         )])),
-        ..recovery_device_data_1()
+        ..DeviceData::auth_test_device()
     };
 
     let device2 = DeviceData {
+        pubkey: ByteBuf::from([3; 32]),
+        purpose: Purpose::Recovery,
         credential_id: Some(ByteBuf::from("credential id 1")),
         key_type: KeyType::CrossPlatform,
-        ..recovery_device_data_2()
+        ..DeviceData::auth_test_device()
     };
 
     vec![device1, device2]
