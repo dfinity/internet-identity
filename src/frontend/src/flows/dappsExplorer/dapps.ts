bb26e8cbc995f552d012901ab802eea2836023e1
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-05-23 16:53:35 +0200

    Add autoupdate workflow for dapps
    This adds a daily workflow run that checks
    https://github.com/dfinity/portal and updates our local list of dapps &
    logos.
    
    This involves a few changes:
    
    * A new script, `scripts/update-dapps`, is added and used by the workflow.
    * This script was run and the dapps are up-to-date.
    * The explorer & other components making use of logos are updated to
      only prefix the logo with BASE_URL (& URL path prefix) where the logos
      are used (the dapps list only gives the logo name now, for clarity &
      brevity)
    * The vite build is updated to avoid compressing SVG assets (the
      canister chokes on those, and due to the updated dapps data, we now
      have SVG assets)
    
    The vite build is updated to _not_ compress svgs, since

--- src/frontend/src/flows/dappsExplorer/dapps.ts
+++ src/frontend/src/flows/dappsExplorer/dapps.ts
@@ -1,4 +1,3 @@
-import { BASE_URL } from "$src/environment";
 import { features } from "$src/features";
 
 // The list of dapps. This is derived from https://github.com/dfinity/portal:
@@ -16,20 +15,17 @@ export type DappDescription = ElementOf<typeof dappsJson>;
 
 // The list of dapps we showcase
 export const getDapps = (): DappDescription[] => {
-  const dapps = dappsJson.map((dapp) => ({
-    ...dapp,
-    /* fix up logo path (inherited from dfinity/portal) to match our assets */
-    logo: dapp.logo.replace("/img/showcase/", BASE_URL + "icons/"),
-  }));
+  const dapps = [...dappsJson];
 
   // XXX: Piggy back on the DUMMY_CAPTCHA feature to assume this is a test build
   // and add test data
   if (features.DUMMY_CAPTCHA) {
     // The dapp used in tests
+    const nnsDappLogo = dapps.find((dapp) => dapp.name === "NNS Dapp");
     dapps.push({
       name: "Test Dapp",
-      link: "https://nice-name.com",
-      logo: BASE_URL + "icons/nnsfront-enddapp_logo.webp",
+      website: "https://nice-name.com",
+      logo: nnsDappLogo?.logo ?? "no-such-logo",
     });
   }
 
