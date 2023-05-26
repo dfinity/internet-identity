9456aba3564992d8f2c915e7ad83590d55fa209b
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-05-26 12:17:35 +0200

    Remove trailing whitespace (#1641)
    It was probably left there by mistake, and no one reclaimed it.

--- src/internet_identity/tests/integration/active_anchor_stats/mod.rs
+++ src/internet_identity/tests/integration/active_anchor_stats/mod.rs
@@ -1,6 +1,6 @@
 //! Module for active anchor statistics
 
-/// Tests for the general (domain independent) active anchor statistics  
+/// Tests for the general (domain independent) active anchor statistics
 mod all_domains;
 
 /// Tests for the active anchor statistics that are specific to the II domains.
