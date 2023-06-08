e7fb1ccfc5b2228c28b1db8031c8efdd85eda244
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-06-08 16:51:24 +0200

    Wrap dapp descriptions in KnownDapp (#1678)
    This adds a type wrapper (class), `KnownDapp`, that ensures that the
    logo field of `DappDescription` is used correctly.
    
    This ensures the path to actual logo files (`"<BASE_URL>/icons/<logo>"`)
    has to be set only once.

--- src/frontend/src/flows/dappsExplorer/dapps.ts
+++ src/frontend/src/flows/dappsExplorer/dapps.ts
@@ -1,3 +1,4 @@
+import { BASE_URL } from "$src/environment";
 import { features } from "$src/features";
 
 // The list of dapps. This is derived from https://github.com/dfinity/portal:
@@ -13,20 +14,56 @@ type ElementOf<Arr> = Arr extends readonly (infer ElementOf)[]
 
 export type DappDescription = ElementOf<typeof dappsJson>;
 
+// A 'known dapp", i.e. a dapp description with the logo fixed up and convenience methods
+export class KnownDapp {
+  constructor(public descr: DappDescription) {}
+
+  // It's a known dapp if the origin matches
+  // * the website
+  // * any (some) of the authOrigins
+  hasOrigin(orig: string): boolean {
+    return (
+      orig === new URL(this.descr.website).origin ||
+      (this.descr.authOrigins ?? []).some(
+        (authOrigin) => orig === new URL(authOrigin).origin
+      )
+    );
+  }
+
+  // Path to use for logo files
+  public get logoSrc(): string {
+    return BASE_URL + "icons/" + this.descr.logo;
+  }
+
+  public get name(): string {
+    return this.descr.name;
+  }
+
+  public get oneLiner(): string | undefined {
+    return this.descr.oneLiner;
+  }
+
+  public get website(): string | undefined {
+    return this.descr.oneLiner;
+  }
+}
+
 // The list of dapps we showcase
-export const getDapps = (): DappDescription[] => {
-  const dapps = [...dappsJson];
+export const getDapps = (): KnownDapp[] => {
+  const dapps = [...dappsJson].map((dapp) => new KnownDapp(dapp));
 
   // XXX: Piggy back on the DUMMY_CAPTCHA feature to assume this is a test build
   // and add test data
   if (features.DUMMY_CAPTCHA) {
     // The dapp used in tests
-    const nnsDappLogo = dapps.find((dapp) => dapp.name === "NNS Dapp");
-    dapps.push({
-      name: "Test Dapp",
-      website: "https://nice-name.com",
-      logo: nnsDappLogo?.logo ?? "no-such-logo",
-    });
+    const nnsDappLogo = dapps.find((dapp) => dapp.descr.name === "NNS Dapp");
+    dapps.push(
+      new KnownDapp({
+        name: "Test Dapp",
+        website: "https://nice-name.com",
+        logo: nnsDappLogo?.descr.logo ?? "no-such-logo",
+      })
+    );
   }
 
   return dapps;
