93b5b3fa1344e21c37cea996b20f530e8f519780
David Aerne <meodai@gmail.com>
2023-06-27 14:41:43 +0200

    make the compatiblity notice use the mainWindow component

--- src/frontend/src/flows/compatibilityNotice.ts
+++ src/frontend/src/flows/compatibilityNotice.ts
@@ -1,23 +1,28 @@
 import { compatibilityChart } from "$src/components/compatibilityChart";
+import { mainWindow } from "$src/components/mainWindow";
 import { warnBox } from "$src/components/warnBox";
 import { html, render } from "lit-html";
 
 // Taken from: https://caniuse.com/?search=PublicKeyCredential
 const pageContent = (reason: string) => html`
-  <div class="l-container c-card c-card--highlight">
-    <h1 class="t-title t-title--main" id="compatibilityNotice">
-      Internet Identity does not support your browser
-    </h1>
-    ${warnBox({
-      title: "Reason",
-      message: reason,
-      additionalClasses: ["l-stack"],
-    })}
-    <div class="l-stack">${compatibilityChart}</div>
-  </div>
+  <h1 class="t-title t-title--main" id="compatibilityNotice">
+    Internet Identity does not support your browser
+  </h1>
+  ${warnBox({
+    title: "Reason",
+    message: reason,
+    additionalClasses: ["l-stack"],
+  })}
+  <div class="l-stack">${compatibilityChart}</div>
 `;
 
 export const compatibilityNotice = (reason: string): void => {
   const container = document.getElementById("pageContent") as HTMLElement;
-  render(pageContent(reason), container);
+  render(
+    mainWindow({
+      slot: pageContent(reason),
+      showLogo: false,
+    }),
+    container
+  );
 };
