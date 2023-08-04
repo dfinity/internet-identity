c5a7d63c7f94530630d7b0ff4282dee2ce8e806e
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-08-04 16:09:37 +0200

    Remove unnecessary npm commands from build script (#1770)

--- demos/vc_issuer/build.sh
+++ demos/vc_issuer/build.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 
-npm ci
-npm run build
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./Cargo.toml -j1
 ic-wasm "target/wasm32-unknown-unknown/release/vc_issuer.wasm" -o "./vc_issuer.wasm" shrink
 ic-wasm vc_issuer.wasm -o vc_issuer.wasm metadata candid:service -f vc_issuer.did -v public
