fbb018a71b80d13966af9a79b3a8d1b78c39a4d2
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-08-17 17:16:15 +0200

    Refactor release script / action
    This PR refactors how release notes are generated.
    
    The release action script no longer interacts with the GitHub API directly.
    Rather we fetch the information beforehand using the `github-script` action
    and have the script assemble the full release-notes.
    
    This removes complexity from the release action script, solves paging issues
    and reduces the number of API calls being made.

--- .github/actions/release/run.sh
+++ .github/actions/release/run.sh
@@ -5,11 +5,8 @@
 
 set -euo pipefail
 
-GITHUB_TOKEN=${INPUT_TOKEN:-${GITHUB_TOKEN:?No token given}}
-
 PRODUCTION_ASSET=${INPUT_PRODUCTION_ASSET:?No production asset specified}
 RELEASE_TAG=${RELEASE_TAG:-${GITHUB_REF_NAME:?No value for tag}}
-RELEASE_TAG_PREVIOUS=${RELEASE_TAG_PREVIOUS:-}
 
 # Starting the "intro" section where we display a short intro
 section_intro=$(mktemp)
@@ -61,32 +58,29 @@ do
     # This works because our jobs contain the filename without extension
     # (either added manually or by build matrix which takes in the filename as argument and adds it to the job name).
     # https://github.community/t/get-action-job-id/17365/7
-    if [ -z "${GITHUB_RUN_ID:-}" ]
+    if [ -z "${INPUT_WORKFLOW_JOBS:-}" ]
     then
         # if not running on GitHub (e.g. locally for debugging), skip
         html_url="https://example.com"
         step="step"
     else
-        job_id=$(curl --silent "https://api.github.com/repos/dfinity/internet-identity/actions/runs/$GITHUB_RUN_ID/jobs" \
-            | jq -cMr \
-            --arg search_string "${filename%%.*}" \
-            '.jobs[] | select(.name | contains($search_string)) | .id')
-                    >&2 echo "Found job id: $job_id"
+        job_id=$(jq -cMr --arg search_string "${filename%%.*}" \
+                '.[] | select(.name | contains($search_string)) | .id'\
+                <<< "$INPUT_WORKFLOW_JOBS")
+                        >&2 echo "Found job id: $job_id"
 
         # Now get the URL that we'll link to for verification of the sha
-        html_url=$(curl --silent "https://api.github.com/repos/dfinity/internet-identity/actions/runs/$GITHUB_RUN_ID/jobs" \
-            | jq -cMr \
-            --argjson job_id "$job_id" \
-            '.jobs[] | select(.id == $job_id) | .html_url')
-                >&2 echo "Found html_url: $html_url"
+        html_url=$(jq -cMr --argjson job_id "$job_id" \
+                  '.[] | select(.id == $job_id) | .html_url'\
+                  <<< "$INPUT_WORKFLOW_JOBS")
+                      >&2 echo "Found html_url: $html_url"
 
         # Additionally grab the step number of the 'sha256sum' step
-        step=$(curl --silent "https://api.github.com/repos/dfinity/internet-identity/actions/runs/$GITHUB_RUN_ID/jobs" \
-            | jq -cMr \
-            --argjson job_id "$job_id" \
-            --arg filename "$filename" \
-            '.jobs[] | select(.id == $job_id) | .steps[] | select(.name | endswith("sha256sum " + $filename)) | .number')
-                >&2 echo "Found step: $step"
+        step=$(jq -cMr --argjson job_id "$job_id" \
+              --arg filename "$filename" \
+              '.[] | select(.id == $job_id) | .steps[] | select(.name | endswith("sha256sum " + $filename)) | .number'\
+              <<< "$INPUT_WORKFLOW_JOBS")
+                  >&2 echo "Found step: $step"
     fi
 
     # Prepare the cells:
@@ -110,40 +104,10 @@ done <<< "$INPUT_ASSETS"
 
 >&2 echo "Creating release notes"
 
-# NOTE: we create the release notes ourselves, instead of letting GitHub do it with
-# 'generate_release_notes: true', here we can actually specify the release range. When doing
-# it on its own, GitHub is really bad at figuring which tag to use as the previous tag (for
-# listing contributions since).
-# https://github.com/github/feedback/discussions/5975
-section_whats_changed=$(mktemp)
-if [ -z "$RELEASE_TAG_PREVIOUS" ]
-then
-    RELEASE_TAG_PREVIOUS=$(curl --silent \
-        -H "Accept: application/vnd.github.v3+json" \
-        https://api.github.com/repos/dfinity/internet-identity/releases \
-        | jq -cMr 'sort_by(.published_at) | reverse | .[0] | .tag_name')
-fi
-
->&2 echo "Using following tag as previous: $RELEASE_TAG_PREVIOUS"
-
-jq_body=$(jq -cM -n \
-    --arg previous_tag_name "$RELEASE_TAG_PREVIOUS" \
-    --arg tag_name "$RELEASE_TAG" \
-    '{ tag_name: $tag_name, previous_tag_name: $previous_tag_name }')
-
-curl --silent \
-    -X POST \
-    -H "authorization: token $GITHUB_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/dfinity/internet-identity/releases/generate-notes \
-    --data "$jq_body" \
-    | jq -cMr '.body' >> "$section_whats_changed"
-
-
 body=$(mktemp)
 >&2 echo "Using '$body' for body"
 cat "$section_intro" >> "$body" && echo >> "$body" && rm "$section_intro"
-cat "$section_whats_changed" >> "$body" && echo >> "$body" && rm "$section_whats_changed"
+echo "$INPUT_CHANGELOG" >> "$body"
 cat "$section_build_flavors" >> "$body" && echo >> "$body" && rm "$section_build_flavors"
 cat "$section_wasm_verification" >> "$body" && echo >> "$body" && rm "$section_wasm_verification"
 
