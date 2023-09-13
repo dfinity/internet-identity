8a418f19c4eeea51fc6ed0173235c8d05bcf8bd4
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-09-13 14:46:02 +0200

    Provide verify function externally to usePin flow
    This PR expands the usePin function to take a PIN verification
    function as an argument. This will later be used to immediately check
    whether the identity can be decrypted using the provided PIN.

--- src/frontend/src/flows/pin/usePin.ts
+++ src/frontend/src/flows/pin/usePin.ts
@@ -1,23 +1,25 @@
 import { mainWindow } from "$src/components/mainWindow";
-import { pinInput } from "$src/components/pinInput";
+import { pinInput, PinResult } from "$src/components/pinInput";
 import { I18n } from "$src/i18n";
 import { mount, renderPage } from "$src/utils/lit-html";
-import { TemplateResult, html } from "lit-html";
+import { html, TemplateResult } from "lit-html";
 
 import copyJson from "./usePin.json";
 
 /* Prompt the user to input their PIN for use */
 
-const usePinTemplate = ({
+const usePinTemplate = <T>({
   i18n,
   cancel,
+  verify,
   onContinue,
   onUsePasskey,
   scrollToTop = false,
 }: {
   i18n: I18n;
   cancel: () => void;
-  onContinue: (pin: string) => void;
+  verify: (pin: string) => Promise<PinResult<T>>;
+  onContinue: (result: T) => void;
   onUsePasskey: () => void;
   /* put the page into view */
   scrollToTop?: boolean;
@@ -25,9 +27,7 @@ const usePinTemplate = ({
   const copy = i18n.i18n(copyJson);
   const pinInput_ = pinInput({
     onSubmit: onContinue,
-    verify: (pin) =>
-      // XXX: here we don't do any verification, but ideally the identity reconstruction should happen here
-      ({ ok: true, value: pin }),
+    verify: verify,
     secret: true,
   });
   const slot = html`
@@ -57,14 +57,19 @@ const usePinTemplate = ({
 };
 
 export const usePinPage = renderPage(usePinTemplate);
-export const usePin = (): Promise<
-  { kind: "pin"; pin: string } | { kind: "canceled" } | { kind: "passkey" }
+export const usePin = <T>({
+  verifyPin,
+}: {
+  verifyPin: (pin: string) => Promise<PinResult<T>>;
+}): Promise<
+  { kind: "pin"; result: T } | { kind: "canceled" } | { kind: "passkey" }
 > => {
   return new Promise((resolve) =>
-    usePinPage({
+    renderPage(usePinTemplate<T>)({
       i18n: new I18n(),
       onUsePasskey: () => resolve({ kind: "passkey" }),
-      onContinue: (pin: string) => resolve({ kind: "pin", pin }),
+      verify: verifyPin,
+      onContinue: (result: T) => resolve({ kind: "pin", result }),
       cancel: () => resolve({ kind: "canceled" }),
       scrollToTop: true,
     })
