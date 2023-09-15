35fa1e64e3d107eb6462eab5e71ccf9724bb3c0d
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-09-15 16:34:03 +0200

    Highlight recovery box on no recoveries (#1888)
    * Highlight recovery box on no recoveries
    
    If there are no recovery methods, the recovery section is now highlighted
    with a warning.
    
    * Implement review feedback

--- src/frontend/src/flows/manage/recoveryMethodsSection.ts
+++ src/frontend/src/flows/manage/recoveryMethodsSection.ts
@@ -26,8 +26,31 @@ export const recoveryMethodsSection = ({
   addRecoveryPhrase: () => void;
   addRecoveryKey: () => void;
 }): TemplateResult => {
+  const i18n = new I18n();
+
+  const copy = i18n.i18n(copyJson);
+  const warnNoRecovery = isNullish(recoveryPhrase) && isNullish(recoveryKey);
+  const wrapClasses = [
+    "l-stack",
+    ...(warnNoRecovery ? ["c-card", "c-card--narrow", "c-card--warning"] : []),
+  ];
+
   return html`
-    <aside class="l-stack">
+    <aside class=${wrapClasses.join(" ")}>
+      ${warnNoRecovery
+        ? html`
+            <span
+              class="c-card__label c-card__label--hasIcon"
+              aria-hidden="true"
+            >
+              <i
+                class="c-card__icon c-icon c-icon--error__flipped c-icon--inline"
+                >${warningIcon}</i
+              >
+              <h2>${copy.security_warning}</h2>
+            </span>
+          `
+        : ""}
       <div class="t-title">
         <h2>Recovery Methods</h2>
       </div>
@@ -35,10 +58,10 @@ export const recoveryMethodsSection = ({
         <ul>
           ${isNullish(recoveryPhrase)
             ? missingRecovery({ recovery: "phrase", addRecoveryPhrase })
-            : recoveryPhraseItem({ recoveryPhrase, i18n: new I18n() })}
+            : recoveryPhraseItem({ recoveryPhrase, i18n })}
           ${isNullish(recoveryKey)
             ? missingRecovery({ recovery: "key", addRecoveryKey })
-            : recoveryKeyItem({ recoveryKey, i18n: new I18n() })}
+            : recoveryKeyItem({ recoveryKey, i18n })}
         </ul>
       </div>
     </aside>
