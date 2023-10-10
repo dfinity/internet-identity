61e2413ffe043a931872f4a6908bbcf8d4b84bd9
Elias Datler <elias.datler@dfinity.org>
2023-10-10 17:33:54 +0000

    remove compression

--- src/canister_tests/src/framework.rs
+++ src/canister_tests/src/framework.rs
@@ -1,7 +1,6 @@
 use crate::api;
 use crate::api::http_request;
 use candid::Principal;
-use flate2::read::GzDecoder;
 use flate2::{Compression, GzBuilder};
 use ic_cdk::api::management_canister::main::CanisterId;
 use ic_representation_independent_hash::Value;
@@ -235,13 +234,11 @@ pub fn save_compressed_stable_memory(
 }
 
 pub fn restore_compressed_stable_memory(env: &PocketIc, canister_id: CanisterId, path: &str) {
-    let file = File::open(path).expect("Failed to open stable memory file");
-    let mut decoder = GzDecoder::new(file);
+    let mut file = File::open(path).expect("Failed to open stable memory file");
     let mut buffer = vec![];
-    decoder
-        .read_to_end(&mut buffer)
+    file.read_to_end(&mut buffer)
         .expect("error while decoding stable memory file");
-    env.set_stable_memory(canister_id, buffer, BlobCompression::NoCompression);
+    env.set_stable_memory(canister_id, buffer, BlobCompression::Gzip);
 }
 
 pub const PUBKEY_1: &str = "test";
