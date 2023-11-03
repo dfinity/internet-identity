1c9513f87fc2cf91e35ff00bc0ff002adb2ae742
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-11-03 11:12:56 +0100

    test

--- src/frontend/screenshots.ts
+++ src/frontend/screenshots.ts
@@ -111,7 +111,7 @@ async function withChrome<T>(
       browserVersion: "119.0.6045.105",
       "goog:chromeOptions": chromeOptions,
     },
-    logLevel: "info",
+    logLevel: "trace",
   });
 
   if (isNullish(mobileEmulation)) {
