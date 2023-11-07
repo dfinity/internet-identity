89ccd1a0a3c991ab0681fc937b460b51a51c2777
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-11-07 10:30:29 +0100

    Don't apply forwarding rule on nullish host

--- vite.plugins.ts
+++ vite.plugins.ts
@@ -117,10 +117,17 @@ export const replicaForwardPlugin = ({
         return;
       }
 
-      const matchingRule = forwardRules.find((rule) => {
-        let host = req.headers["host"];
-        return isNullish(host) || rule.hosts.includes(host);
-      });
+      let host = req.headers["host"];
+      if (isNullish(host)) {
+        // default handling
+        return next();
+      }
+
+      const matchingRule = forwardRules.find(
+        // the TS compiler doesn't get that due to the isNullish check above, host can't be undefined
+        // (which is why the explicit cast is there)
+        (rule) => rule.hosts.includes(host as string)
+      );
       if (isNullish(matchingRule)) {
         // default handling
         return next();
