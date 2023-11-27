c6c75fb56eda2b6716054063daa43909649c3cf9
Nicolas Mattia <nicolas.mattia@dfinity.org>
2023-11-27 12:00:08 +0100

    wip

--- demos/vc_issuer/build.sh
+++ demos/vc_issuer/build.sh
@@ -6,6 +6,11 @@ set -euo pipefail
 VC_ISSUER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$VC_ISSUER_DIR"
 
+# Build the app
+pushd ../../
+npm run -w vc_issuer build
+popd
+
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./Cargo.toml -j1
 ic-wasm "target/wasm32-unknown-unknown/release/vc_issuer.wasm" -o "./vc_issuer.wasm" shrink
 ic-wasm vc_issuer.wasm -o vc_issuer.wasm metadata candid:service -f vc_issuer.did -v public
