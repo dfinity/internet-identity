a26714fe871f30eba430a5ee7da2351bf9412f48
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-12-08 12:11:11 +0100

    Add certification version metadata to demo canisters (#2122)
    The metadata is required for certification v2 to work properly.

--- demos/vc_issuer/build.sh
+++ demos/vc_issuer/build.sh
@@ -12,5 +12,7 @@ npm run build
 cargo build --release --target wasm32-unknown-unknown --manifest-path ./Cargo.toml -j1
 ic-wasm "target/wasm32-unknown-unknown/release/vc_issuer.wasm" -o "./vc_demo_issuer.wasm" shrink
 ic-wasm vc_demo_issuer.wasm -o vc_demo_issuer.wasm metadata candid:service -f vc_demo_issuer.did -v public
+# indicate support for certificate version 1 and 2 in the canister metadata
+ic-wasm vc_demo_issuer.wasm -o vc_demo_issuer.wasm metadata supported_certificate_versions -d "1,2" -v public
 gzip --no-name --force "vc_demo_issuer.wasm"
 
