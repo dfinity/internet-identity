b9fce85f9b0bf74098e7c25cb9b098b8975fbe80
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-12-20 16:41:28 +0100

    Add API v2 test helpers (#2152)
    This PR adds more useful API v2 test helpers in preparation for more APIv2 methods.
    
    In addition, the `authn_method_add` test is refactored to make use of those new helpers.

--- src/internet_identity/tests/integration/v2_api/authn_method_test_helpers.rs
+++ src/internet_identity/tests/integration/v2_api/authn_method_test_helpers.rs
@@ -3,9 +3,10 @@ use ic_cdk::api::management_canister::main::CanisterId;
 use ic_test_state_machine_client::StateMachine;
 use internet_identity_interface::internet_identity::types::{
     AuthnMethod, AuthnMethodData, AuthnMethodProtection, ChallengeAttempt, IdentityNumber,
-    PublicKeyAuthn, Purpose,
+    MetadataEntry, PublicKeyAuthn, Purpose, WebAuthn,
 };
 use serde_bytes::ByteBuf;
+use std::collections::HashMap;
 
 pub fn eq_ignoring_last_authentication(a: &AuthnMethodData, b: &AuthnMethodData) -> bool {
     let a = AuthnMethodData {
@@ -17,6 +18,23 @@ pub fn eq_ignoring_last_authentication(a: &AuthnMethodData, b: &AuthnMethodData)
         ..b.clone()
     };
     a == b
+}
+
+pub fn assert_eq_ignoring_last_authentication(
+    authn_methods1: &[AuthnMethodData],
+    authn_methods2: &[AuthnMethodData],
+) {
+    authn_methods1
+        .iter()
+        .zip(authn_methods2.iter())
+        .for_each(|(a, b)| {
+            assert!(
+                eq_ignoring_last_authentication(a, b),
+                "authn methods are not equal: {:?} != {:?}",
+                a,
+                b
+            )
+        });
 }
 
 pub fn test_authn_method() -> AuthnMethodData {
@@ -56,11 +74,122 @@ pub fn create_identity_with_authn_method(
     .expect("identity_register failed")
 }
 
-pub fn sample_authn_method(i: u8) -> AuthnMethodData {
+pub fn create_identity_with_authn_methods(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    authn_methods: &[AuthnMethodData],
+) -> IdentityNumber {
+    let first_authn_method = authn_methods.first().expect("authn_methods is empty");
+    let identity_number = create_identity_with_authn_method(env, canister_id, first_authn_method);
+
+    for authn_method in authn_methods.iter().skip(1) {
+        api_v2::authn_method_add(
+            env,
+            canister_id,
+            first_authn_method.principal(),
+            identity_number,
+            authn_method,
+        )
+        .expect("API call failed")
+        .expect("authn_method_add failed");
+    }
+    identity_number
+}
+
+pub fn sample_pubkey_authn_method(i: u8) -> AuthnMethodData {
     AuthnMethodData {
         authn_method: AuthnMethod::PubKey(PublicKeyAuthn {
             pubkey: ByteBuf::from(vec![i; 32]),
         }),
         ..test_authn_method()
     }
+}
+
+pub fn sample_webauthn_authn_method(i: u8) -> AuthnMethodData {
+    AuthnMethodData {
+        authn_method: AuthnMethod::WebAuthn(WebAuthn {
+            pubkey: ByteBuf::from(vec![i; 32]),
+            credential_id: ByteBuf::from(vec![i * 2; 32]),
+        }),
+        ..test_authn_method()
+    }
+}
+
+pub fn sample_authn_methods() -> Vec<AuthnMethodData> {
+    let authn_method1 = AuthnMethodData {
+        metadata: HashMap::from([
+            (
+                "some_key".to_string(),
+                MetadataEntry::String("some data".to_string()),
+            ),
+            (
+                "origin".to_string(),
+                MetadataEntry::String("https://some.origin".to_string()),
+            ),
+            (
+                "alias".to_string(),
+                MetadataEntry::String("Test Authn Method 1".to_string()),
+            ),
+        ]),
+        ..sample_pubkey_authn_method(0)
+    };
+
+    let authn_method2 = AuthnMethodData {
+        metadata: HashMap::from([(
+            "different_key".to_string(),
+            MetadataEntry::String("other data".to_string()),
+        )]),
+        ..sample_webauthn_authn_method(1)
+    };
+
+    let authn_method3 = AuthnMethodData {
+        metadata: HashMap::from([
+            (
+                "recovery_metadata_1".to_string(),
+                MetadataEntry::String("recovery data 1".to_string()),
+            ),
+            (
+                "origin".to_string(),
+                MetadataEntry::String("https://identity.ic0.app".to_string()),
+            ),
+            (
+                "usage".to_string(),
+                MetadataEntry::String("recovery_phrase".to_string()),
+            ),
+        ]),
+        purpose: Purpose::Recovery,
+        ..sample_pubkey_authn_method(2)
+    };
+
+    let authn_method4 = AuthnMethodData {
+        metadata: HashMap::default(),
+        purpose: Purpose::Recovery,
+        ..sample_webauthn_authn_method(3)
+    };
+
+    let authn_method5 = AuthnMethodData {
+        metadata: HashMap::from([
+            (
+                "origin".to_string(),
+                MetadataEntry::String("https://identity.internetcomputer.org".to_string()),
+            ),
+            (
+                "alias".to_string(),
+                MetadataEntry::String("Test Authn Method 5".to_string()),
+            ),
+            (
+                "usage".to_string(),
+                MetadataEntry::String("browser_storage_key".to_string()),
+            ),
+        ]),
+        ..sample_webauthn_authn_method(4)
+    };
+
+    vec![
+        authn_method1,
+        authn_method2,
+        authn_method3,
+        authn_method4,
+        authn_method5,
+    ]
 }
