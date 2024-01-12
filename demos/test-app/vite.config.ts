06a3e4bdfc4f77d7d5ffc47dd75e6894781cdfae
Nicolas Mattia <nicolas.mattia@dfinity.org>
2024-01-12 15:55:57 +0100

    Create workspace for vite plugins (#2200)
    This adds a new npm workspace for the plugins & build utils used
    throughout the frontend codebase. This allows deduplicating some helpers
    (reading replica host, canister id) and move some concerns (building)
    out of the main workspace.

--- demos/test-app/vite.config.ts
+++ demos/test-app/vite.config.ts
@@ -1,37 +1,8 @@
-import { execSync } from "child_process";
+import {
+  getReplicaHost,
+  readCanisterId,
+} from "@dfinity/internet-identity-vite-plugins";
 import { defineConfig } from "vite";
-
-/**
- * Read a canister ID from dfx's local state
- */
-export const readCanisterId = ({
-  canisterName,
-}: {
-  canisterName: string;
-}): string => {
-  const command = `dfx canister id ${canisterName}`;
-  try {
-    const stdout = execSync(command);
-    return stdout.toString().trim();
-  } catch (e) {
-    throw Error(
-      `Could not get canister ID for '${canisterName}' with command '${command}', was the canister deployed? ${e}`
-    );
-  }
-};
-
-export const getReplicaHost = (): string => {
-  const command = `dfx info webserver-port`;
-  try {
-    const stdout = execSync(command);
-    const port = stdout.toString().trim();
-    return `http://127.0.0.1:${port}`;
-  } catch (e) {
-    throw Error(
-      `Could not get replica port '${command}', is the replica running? ${e}`
-    );
-  }
-};
 
 const rewriteRoute = (pathAndParams: string): string => {
   let queryParamsString = `?`;
