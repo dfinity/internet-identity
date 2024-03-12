935faa595cf7d14d8c41f170d2bb6bb1aa233738
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-12 15:49:03 +0100

    Import current file instead of new one

--- src/showcase/src/backgrounds.ts
+++ src/showcase/src/backgrounds.ts
@@ -1,6 +1,6 @@
 import { IdentityBackground } from "$src/components/identityCard";
+import backgroundImage from "$src/components/identityCard.png";
 import { ImageMetadata } from "astro";
-import backgroundImage from "./assets/identityCard.png";
 
 export const identityBackground = IdentityBackground.getSingleton(
   // When running the build, this is also build but not in an Astro envioronment
