320973d085c9e3931ffbe3ee40e6bed6dcfa9448
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-12 17:12:26 +0100

    Fix typos

--- src/showcase/src/backgrounds.ts
+++ src/showcase/src/backgrounds.ts
@@ -3,6 +3,6 @@ import backgroundImage from "$src/components/identityCard.png";
 import { ImageMetadata } from "astro";
 
 export const identityBackground = IdentityBackground.getSingleton(
-  // When running the build, this is also build but not in an Astro envioronment
+  // When running the build, this is also built but not in an Astro environment
   (backgroundImage as unknown as ImageMetadata).src
 );
