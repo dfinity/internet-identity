9e68dbc177da7f6852ecc42708e35bb18d147b44
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2024-03-12 17:30:14 +0100

    Remove duplicated IdentityBackground (#2361)
    * Import current file instead of new one
    
    * Fix typos

--- src/showcase/src/backgrounds.ts
+++ src/showcase/src/backgrounds.ts
@@ -1,8 +1,8 @@
 import { IdentityBackground } from "$src/components/identityCard";
+import backgroundImage from "$src/components/identityCard.png";
 import { ImageMetadata } from "astro";
-import backgroundImage from "./assets/identityCard.png";
 
 export const identityBackground = IdentityBackground.getSingleton(
-  // When running the build, this is also build but not in an Astro envioronment
+  // When running the build, this is also built but not in an Astro environment
   (backgroundImage as unknown as ImageMetadata).src
 );
