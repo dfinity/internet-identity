5ce37acef8a1103e3a25a20251df6f5aff46585a
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-12 15:45:23 +0100

    Fix identity background in Showcase

--- src/showcase/src/backgrounds.ts
+++ src/showcase/src/backgrounds.ts
@@ -1,3 +1,8 @@
-import { loadIdentityBackground } from "$src/components/identityCard";
+import { IdentityBackground } from "$src/components/identityCard";
+import { ImageMetadata } from "astro";
+import backgroundImage from "./assets/identityCard.png";
 
-export const identityBackground = loadIdentityBackground();
+export const identityBackground = IdentityBackground.getSingleton(
+  // When running the build, this is also build but not in an Astro envioronment
+  (backgroundImage as unknown as ImageMetadata).src
+);
