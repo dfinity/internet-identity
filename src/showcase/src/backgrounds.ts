20465bc7356c9ca72a1d3c32ead70b70d871e767
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-16 16:05:50 +0200

    Remove backgrounds helper

--- src/showcase/src/backgrounds.ts
+++
@@ -1,4 +0,0 @@
-import identityCardBackground from "$src/assets/identityCardBackground.png";
-import { PreLoadImage } from "$src/utils/preLoadImage";
-
-export const identityBackground = new PreLoadImage(identityCardBackground.src);
