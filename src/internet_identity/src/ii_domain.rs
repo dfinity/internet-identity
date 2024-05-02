fe74132b374dcce38d4c80933d3c5b959381cb46
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2024-05-02 13:45:33 +0200

    Refactor IIDomain enum for conciseness
    Refactors IIDomain to be more concise because the type will be
    used more often after #2449.

--- src/internet_identity/src/ii_domain.rs
+++ src/internet_identity/src/ii_domain.rs
@@ -2,15 +2,15 @@ use crate::storage::anchor::DomainActivity;
 use crate::{IC0_APP_ORIGIN, INTERNETCOMPUTER_ORG_ORIGIN};
 #[derive(Eq, PartialEq)]
 pub enum IIDomain {
-    Ic0AppDomain,
-    InternetComputerOrgDomain,
+    Ic0App,
+    InternetComputerOrg,
 }
 
 impl IIDomain {
     pub fn is_same_domain(&self, activity: &DomainActivity) -> bool {
         match self {
-            IIDomain::Ic0AppDomain => matches!(activity, DomainActivity::Ic0App),
-            IIDomain::InternetComputerOrgDomain => {
+            IIDomain::Ic0App => matches!(activity, DomainActivity::Ic0App),
+            IIDomain::InternetComputerOrg => {
                 matches!(activity, DomainActivity::InternetComputerOrg)
             }
         }
@@ -18,8 +18,8 @@ impl IIDomain {
 
     pub fn other_ii_domain(&self) -> IIDomain {
         match self {
-            IIDomain::Ic0AppDomain => IIDomain::InternetComputerOrgDomain,
-            IIDomain::InternetComputerOrgDomain => IIDomain::Ic0AppDomain,
+            IIDomain::Ic0App => IIDomain::InternetComputerOrg,
+            IIDomain::InternetComputerOrg => IIDomain::Ic0App,
         }
     }
 }
@@ -29,8 +29,8 @@ impl TryFrom<&str> for IIDomain {
 
     fn try_from(origin: &str) -> Result<Self, Self::Error> {
         match origin {
-            IC0_APP_ORIGIN => Ok(IIDomain::Ic0AppDomain),
-            INTERNETCOMPUTER_ORG_ORIGIN => Ok(IIDomain::InternetComputerOrgDomain),
+            IC0_APP_ORIGIN => Ok(IIDomain::Ic0App),
+            INTERNETCOMPUTER_ORG_ORIGIN => Ok(IIDomain::InternetComputerOrg),
             _ => Err(()),
         }
     }
