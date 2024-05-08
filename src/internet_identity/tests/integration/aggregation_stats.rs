dfinity/internet-identity

commit 71113f3329c6659bac51cd33e0a634459b3811fb

Implement automatic event pruning (#2460)
This PR adds automatic event pruning using timers and self-calls (to
enable retries on failure).

In normal operation automatic event pruning should not be needed.
However, if II stops processing `prepare_delegation` canister calls for
some reason for an extended period of time then the backlog of events
that need pruning will grow.

To make sure that II can process this backlog, `PruneEvents` are injected
if the latest event is older than 5 minutes. If the backlog is too large
to be processed in a single canister call, then II will fall back to
smaller time windows until it becomes processable.

src/internet_identity/src/main.rs
@@ -13,6 +13,7 @@ use canister_sig_util::signature_map::LABEL_SIG;
 use ic_cdk::api::{caller, set_certified_data, trap};
 use ic_cdk::call;
 use ic_cdk_macros::{init, post_upgrade, pre_upgrade, query, update};
+use ic_cdk_timers::{set_timer, set_timer_interval};
 use internet_identity_interface::archive::types::BufferedEntry;
 use internet_identity_interface::http_gateway::{HttpRequest, HttpResponse};
 use internet_identity_interface::internet_identity::types::vc_mvp::{
@@ -22,6 +23,7 @@ use internet_identity_interface::internet_identity::types::vc_mvp::{
 use internet_identity_interface::internet_identity::types::*;
 use serde_bytes::ByteBuf;
 use std::collections::HashMap;
+use std::time::Duration;
 use storage::{Salt, Storage};
 
 mod activity_stats;
@@ -372,6 +374,22 @@ fn acknowledge_entries(sequence_number: u64) {
     archive::acknowledge_entries(sequence_number)
 }
 
+/// Check for backlogged event data and prune if necessary.
+/// Only callable by this canister.
+#[update]
+#[candid_method]
+async fn prune_events_if_necessary() {
+    activity_stats::event_stats::prune_events_if_necessary().await
+}
+
+/// Inject pruning event at the given timestamp.
+/// Only callable by this canister.
+#[update]
+#[candid_method]
+fn inject_prune_event(timestamp: Timestamp) {
+    activity_stats::event_stats::inject_prune_event(timestamp);
+}
+
 #[init]
 #[candid_method(init)]
 fn init(maybe_arg: Option<InternetIdentityInit>) {
@@ -383,6 +401,15 @@ fn init(maybe_arg: Option<InternetIdentityInit>) {
     // make sure the fully initialized storage configuration is written to stable memory
     state::storage_borrow_mut(|storage| storage.flush());
     update_root_hash();
+
+    // Immediately prune events if necessary and also set a timer to do so every 5 minutes.
+    // We need to use a timer here, because we cannot call canisters directly in init.
+    set_timer(Duration::from_nanos(0), || {
+        ic_cdk::spawn(prune_events_if_necessary());
+    });
+    set_timer_interval(Duration::from_nanos(5 * MINUTE_NS), || {
+        ic_cdk::spawn(prune_events_if_necessary());
+    });
 }
 
 #[post_upgrade]

src/internet_identity/tests/integration/aggregation_stats.rs
@@ -15,6 +15,7 @@ use std::time::Duration;
 const SESSION_LENGTH: u64 = 900; // 900 seconds
 const PD_COUNT: &str = "prepare_delegation_count";
 const PD_SESS_SEC: &str = "prepare_delegation_session_seconds";
+const PRUNE_EVENT_COUNT: &str = "prune_event_count";
 
 #[test]
 fn should_report_expected_aggregations() -> Result<(), CallError> {
@@ -56,7 +57,6 @@ fn should_not_report_empty_aggregations() -> Result<(), CallError> {
     let env = env();
     let canister_id = install_ii_canister(&env, II_WASM.clone());
     let ii_origin = "ic0.app";
-    let ii_origin2 = "internetcomputer.org";
     let identity_nr = create_identity(&env, canister_id, ii_origin);
 
     delegation_for_origin(&env, canister_id, identity_nr, "https://some-dapp.com")?;
@@ -75,18 +75,14 @@ fn should_not_report_empty_aggregations() -> Result<(), CallError> {
     assert_eq!(keys, expected_keys);
 
     env.advance_time(Duration::from_secs(60 * 60 * 24 * 30)); // 30 days
-
-    let identity_nr2 = create_identity(&env, canister_id, ii_origin2);
-    delegation_for_origin(&env, canister_id, identity_nr2, "https://some-dapp.com")?;
+    env.tick(); // execute timers
 
     let aggregations = api::stats(&env, canister_id)?.event_aggregations;
 
-    // set of keys is now entirely different, because the other origin has been pruned after 30 days
+    // set of keys is now entirely different due to automatic pruning
     let mut expected_keys = vec![
-        aggregation_key(PD_COUNT, "24h", ii_origin2),
-        aggregation_key(PD_COUNT, "30d", ii_origin2),
-        aggregation_key(PD_SESS_SEC, "24h", ii_origin2),
-        aggregation_key(PD_SESS_SEC, "30d", ii_origin2),
+        aggregation_key(PRUNE_EVENT_COUNT, "24h", "other"),
+        aggregation_key(PRUNE_EVENT_COUNT, "30d", "other"),
     ];
     let mut keys = aggregations.into_keys().collect::<Vec<_>>();
     // sort for stable comparison

Cargo.toml
@@ -31,6 +31,7 @@ candid = "0.10"
 candid_parser = "0.1.2"
 ic-cdk = "0.12"
 ic-cdk-macros = "0.8"
+ic-cdk-timers = "0.6"
 ic-certification = "2.2"
 ic-http-certification = "2.2"
 ic-metrics-encoder = "1"

src/archive/Cargo.toml
@@ -13,7 +13,7 @@ internet_identity_interface.workspace = true
 candid.workspace = true
 ic-cdk.workspace = true
 ic-cdk-macros.workspace = true
-ic-cdk-timers = "0.6.0"
+ic-cdk-timers.workspace = true
 ic-metrics-encoder.workspace = true
 ic-stable-structures.workspace = true
 # other

src/frontend/generated/internet_identity_idl.js
@@ -456,6 +456,7 @@ export const idlFactory = ({ IDL }) => {
         [],
       ),
     'init_salt' : IDL.Func([], [], []),
+    'inject_prune_event' : IDL.Func([Timestamp], [], []),
     'lookup' : IDL.Func([UserNumber], [IDL.Vec(DeviceData)], ['query']),
     'prepare_delegation' : IDL.Func(
         [UserNumber, FrontendHostname, SessionKey, IDL.Opt(IDL.Nat64)],
@@ -467,6 +468,7 @@ export const idlFactory = ({ IDL }) => {
         [IDL.Variant({ 'Ok' : PreparedIdAlias, 'Err' : PrepareIdAliasError })],
         [],
       ),
+    'prune_events_if_necessary' : IDL.Func([], [], []),
     'register' : IDL.Func(
         [DeviceData, ChallengeResult, IDL.Opt(IDL.Principal)],
         [RegisterResponse],

src/frontend/generated/internet_identity_types.d.ts
@@ -363,6 +363,7 @@ export interface _SERVICE {
       { 'Err' : IdentityRegisterError }
   >,
   'init_salt' : ActorMethod<[], undefined>,
+  'inject_prune_event' : ActorMethod<[Timestamp], undefined>,
   'lookup' : ActorMethod<[UserNumber], Array<DeviceData>>,
   'prepare_delegation' : ActorMethod<
     [UserNumber, FrontendHostname, SessionKey, [] | [bigint]],
@@ -373,6 +374,7 @@ export interface _SERVICE {
     { 'Ok' : PreparedIdAlias } |
       { 'Err' : PrepareIdAliasError }
   >,
+  'prune_events_if_necessary' : ActorMethod<[], undefined>,
   'register' : ActorMethod<
     [DeviceData, ChallengeResult, [] | [Principal]],
     RegisterResponse

src/internet_identity/Cargo.toml
@@ -29,6 +29,7 @@ captcha = { git = "https://github.com/dfinity/captcha", rev = "9c0d2dd9bf519e255
 # All IC deps
 candid.workspace = true
 ic-cdk.workspace = true
+ic-cdk-timers.workspace = true
 ic-cdk-macros.workspace = true
 ic-certification.workspace = true
 ic-metrics-encoder.workspace = true

src/internet_identity/internet_identity.did
@@ -536,11 +536,16 @@ service : (opt InternetIdentityInit) -> {
     http_request_update: (request: HttpRequest) -> (HttpResponse);
 
     deploy_archive: (wasm: blob) -> (DeployArchiveResult);
-    /// Returns a batch of entries _sorted by sequence number_ to be archived.
-    /// This is an update call because the archive information _must_ be certified.
-    /// Only callable by this IIs archive canister.
+    // Returns a batch of entries _sorted by sequence number_ to be archived.
+    // This is an update call because the archive information _must_ be certified.
+    // Only callable by this IIs archive canister.
     fetch_entries: () -> (vec BufferedArchiveEntry);
     acknowledge_entries: (sequence_number: nat64) -> ();
+
+    // Calls used for event stats housekeeping.
+    // Only callable by the canister itself.
+    prune_events_if_necessary: () -> ();
+    inject_prune_event: (timestamp: Timestamp) -> ();
 
     // V2 API
     // WARNING: The following methods are experimental and may change in the future.

commit b7f2a5bfd490c16077beada7e9bdcdd4ce4ff9bf

Simplify II init
A bug was introduced in #2460 that did not correctly restart timers
after an upgrade.
This PR simplifies the initialization to share more code and adds
a test to catch this bug in the future.

src/internet_identity/src/main.rs
@@ -393,13 +393,22 @@ fn inject_prune_event(timestamp: Timestamp) {
 #[init]
 #[candid_method(init)]
 fn init(maybe_arg: Option<InternetIdentityInit>) {
-    init_assets();
     state::init_new();
+    initialize(maybe_arg);
+}
 
+#[post_upgrade]
+fn post_upgrade(maybe_arg: Option<InternetIdentityInit>) {
+    state::init_from_stable_memory();
+    // load the persistent state after initializing storage as it manages the respective stable cell
+    state::load_persistent_state();
+
+    initialize(maybe_arg);
+}
+
+fn initialize(maybe_arg: Option<InternetIdentityInit>) {
+    init_assets();
     apply_install_arg(maybe_arg);
-
-    // make sure the fully initialized storage configuration is written to stable memory
-    state::storage_borrow_mut(|storage| storage.flush());
     update_root_hash();
 
     // Immediately prune events if necessary and also set a timer to do so every 5 minutes.
@@ -410,19 +419,6 @@ fn init(maybe_arg: Option<InternetIdentityInit>) {
     set_timer_interval(Duration::from_nanos(5 * MINUTE_NS), || {
         ic_cdk::spawn(prune_events_if_necessary());
     });
-}
-
-#[post_upgrade]
-fn post_upgrade(maybe_arg: Option<InternetIdentityInit>) {
-    init_assets();
-    state::init_from_stable_memory();
-    // load the persistent state after initializing storage, otherwise the memory address to load it from cannot be calculated
-    state::load_persistent_state();
-
-    // We drop all the signatures on upgrade, users will
-    // re-request them if needed.
-    update_root_hash();
-    apply_install_arg(maybe_arg);
 }
 
 fn apply_install_arg(maybe_arg: Option<InternetIdentityInit>) {

src/internet_identity/tests/integration/aggregation_stats.rs
@@ -122,19 +122,6 @@ fn should_report_at_most_100_entries() -> Result<(), CallError> {
     Ok(())
 }
 
-fn assert_expected_aggregation(
-    aggregations: &HashMap<String, Vec<(String, u64)>>,
-    key: &str,
-    data: Vec<(String, u64)>,
-) {
-    assert_eq!(
-        aggregations.get(key).unwrap(),
-        &data,
-        "Aggregation key \"{}\" does not match",
-        key
-    );
-}
-
 #[test]
 fn should_keep_aggregations_across_upgrades() -> Result<(), CallError> {
     const II_ORIGIN: &str = "ic0.app";
@@ -186,6 +173,48 @@ fn should_keep_aggregations_across_upgrades() -> Result<(), CallError> {
 
     assert_expected_state(&env, canister_id)?;
     Ok(())
+}
+
+#[test]
+fn should_prune_automatically_after_upgrade() -> Result<(), CallError> {
+    let env = env();
+    let canister_id = install_ii_canister(&env, II_WASM.clone());
+    let identity_nr = create_identity(&env, canister_id, "ic0.app");
+
+    delegation_for_origin(&env, canister_id, identity_nr, "https://some-dapp.com")?;
+
+    // upgrade then advance time to trigger pruning
+    upgrade_ii_canister(&env, canister_id, II_WASM.clone());
+    env.advance_time(Duration::from_secs(60 * 60 * 24 * 30)); // 30 days
+    env.tick(); // execute timers
+
+    let aggregations = api::stats(&env, canister_id)?.event_aggregations;
+
+    // set of keys only reflects pruning now
+    let mut expected_keys = vec![
+        aggregation_key(PRUNE_EVENT_COUNT, "24h", "other"),
+        aggregation_key(PRUNE_EVENT_COUNT, "30d", "other"),
+    ];
+    let mut keys = aggregations.into_keys().collect::<Vec<_>>();
+    // sort for stable comparison
+    expected_keys.sort();
+    keys.sort();
+    assert_eq!(keys, expected_keys);
+
+    Ok(())
+}
+
+fn assert_expected_aggregation(
+    aggregations: &HashMap<String, Vec<(String, u64)>>,
+    key: &str,
+    data: Vec<(String, u64)>,
+) {
+    assert_eq!(
+        aggregations.get(key).unwrap(),
+        &data,
+        "Aggregation key \"{}\" does not match",
+        key
+    );
 }
 
 fn create_identity(env: &StateMachine, canister_id: CanisterId, ii_origin: &str) -> IdentityNumber {