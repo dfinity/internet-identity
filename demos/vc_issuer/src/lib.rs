8e5b86aee4fe542b43e169b0abb6ff7b5c0ca8a2
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2024-07-09 18:40:37 +0200

    wip

--- demos/vc_issuer/src/lib.rs
+++ demos/vc_issuer/src/lib.rs
@@ -1,5 +1,4 @@
 /// A small module that makes get_random work on wasm32-unknown-unknown.
-/// The dependency on get_random comes from the captcha library.
 #[cfg(all(
     target_arch = "wasm32",
     target_vendor = "unknown",
