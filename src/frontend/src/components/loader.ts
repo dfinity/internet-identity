253513e78414bcc915c7eadea4f2f5c549b13890
sea-snake <sea-snake@outlook.com>
2024-11-21 14:43:00 +0100

    Show support links on loading and error screens.

--- src/frontend/src/components/loader.ts
+++ src/frontend/src/components/loader.ts
@@ -1,22 +1,40 @@
+import { ERROR_SUPPORT_URL } from "$src/config";
 import { html, render } from "lit-html";
 import loaderUrl from "./loader.svg";
 
-const loader = () => html` <div id="loader" class="c-loader">
-  <img class="c-loader__image" src=${loaderUrl} alt="loading" />
-</div>`;
+// Duration in milliseconds a user considers as taking forever
+const TAKING_FOREVER = 10000;
+
+const loader = (takingForever = false) =>
+  html` <div id="loader" class="c-loader">
+    <img class="c-loader__image" src=${loaderUrl} alt="loading" />
+    ${takingForever &&
+    html`<a
+      href="${ERROR_SUPPORT_URL}"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="c-loader__link"
+      >Ongoing issues</a
+    >`}
+  </div>`;
 
 const startLoader = () => {
   const container = document.getElementById("loaderContainer") as HTMLElement;
   render(loader(), container);
-};
 
-const endLoader = () => {
-  const container = document.getElementById("loaderContainer") as HTMLElement;
-  render(html``, container);
+  const takingForeverTimeout = setTimeout(
+    () => render(loader(true), container),
+    TAKING_FOREVER
+  );
+
+  return () => {
+    clearTimeout(takingForeverTimeout);
+    render(html``, container);
+  };
 };
 
 export const withLoader = async <A>(action: () => Promise<A>): Promise<A> => {
-  startLoader();
+  const endLoader = startLoader();
   try {
     return await action();
   } finally {
