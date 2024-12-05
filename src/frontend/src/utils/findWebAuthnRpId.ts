aa5a226e2ec5bf81101ef32b21452276eb89d048
Llorenç <llorenc.muntaner@dfinity.org>
2024-12-05 13:58:41 +0100

    Improve comments

--- src/frontend/src/utils/findWebAuthnRpId.ts
+++ src/frontend/src/utils/findWebAuthnRpId.ts
@@ -2,6 +2,17 @@ import { DeviceData } from "$generated/internet_identity_types";
 
 const defaultDomain = "https://identity.ic0.app";
 
+/**
+ * Helper to extract the top and secondary level domain from a URL.
+ *
+ * Example: "https://identity.ic0.app" -> "ic0.app"
+ *
+ * @param url {string} The URL to extract the domain from.
+ * @returns {string} The top and secondary level domain.
+ *
+ * @throws {Error} If the URL is invalid.
+ * @throws {Error} If the URL does not contain a top and secondary level domain.
+ */
 const getTopAndSecondaryLevelDomain = (url: string): string => {
   const parts = new URL(url).hostname.split(".").reverse();
 
@@ -12,8 +23,14 @@ const getTopAndSecondaryLevelDomain = (url: string): string => {
   return `${parts[1]}.${parts[0]}`;
 };
 
-// Helper to count devices for a domain, defaulting to "ic0.app" if origin is empty
-// The domain must be a top and secondary level domain.
+/**
+ * Helper to count devices for a domain, defaulting to "ic0.app" if origin is empty
+ *
+ * @param devices - The list of devices registered for the user.
+ * @param domain - The domain to check for devices. It must be a top and secondary level domain e.g. "ic0.app"
+ * We need this to support the beta domains with the same functions: beta.identity.ic0.app, beta.identity.internetcomputer.org
+ * @returns {DeviceData[]} The list of devices registered for the domain.
+ */
 const getDevicesForDomain = (
   devices: DeviceData[],
   domain: string
@@ -34,12 +51,15 @@ const getDevicesForDomain = (
  * 3. If there is no device registered for the current domain and none of the preferred domains.
  *    Raise an error because the devices should be registered in one of the preferred domains.
  *
- * @param currentUrl {string} The current URL of the page.
- * @param devices {DeviceData[]} The list of devices registered for the user.
- * @param preferredDomains {string[]} The list of domains in order or preference to use as the RP ID.
+ * @param currentUrl - The current URL of the page.
+ * @param devices - The list of devices registered for the user.
+ * @param preferredDomains - Optional list of domains in order or preference to use as the RP ID.
  * @returns {string | undefined} The RP ID to use for WebAuthn registration.
  * `undefined` when the RP ID is the same as the current domain and is not needed.
- * @throws {Error} If devices are not registered for any of the preferred domains, no devices exist or the current domain is invalid.
+ *
+ * @throws {Error} If devices are not registered for any of the preferred domains.
+ * @throws {Error} If no devices exist or the current domain is invalid.
+ * @throws {Error} If the current domain is invalid.
  */
 export const findWebAuthnRpId = (
   currentUrl: string,
