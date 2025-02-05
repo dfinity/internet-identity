f11a09f47537507d50ffab629d3575fc7dcdebe8
Andri Schatz <andri.schatz@dfinity.org>
2025-02-05 15:50:34 +0100

    minor adjustment

--- src/internet_identity/src/authz_utils.rs
+++ src/internet_identity/src/authz_utils.rs
@@ -97,11 +97,8 @@ where
 pub fn check_authorization(
     anchor_number: AnchorNumber,
 ) -> Result<(Anchor, DeviceKey), AuthorizationError> {
-    ic_cdk::println!("check_authorization");
     let anchor = state::anchor(anchor_number);
-    ic_cdk::println!("anchor: {:?}", anchor);
     let caller = caller();
-    ic_cdk::println!("caller: {:?}", caller);
 
     for device in anchor.devices() {
         if caller == Principal::self_authenticating(&device.pubkey)
@@ -114,14 +111,10 @@ pub fn check_authorization(
             return Ok((anchor.clone(), device.pubkey.clone()));
         }
     }
-    ic_cdk::println!("check_authorization: no device found");
     // check openid authorization
     for credential in anchor.openid_credentials() {
         //TODO: handle temp keys
-        ic_cdk::println!("credential: {:?}", credential);
-        ic_cdk::println!("credential.principal(): {:?}", credential.principal());
         if caller == credential.principal() {
-            ic_cdk::println!("check_authorization: credential found");
             return Ok((anchor.clone(), credential.public_key()));
         }
     }
