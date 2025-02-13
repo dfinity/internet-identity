823be50d3c287e15cf0b4854c161b8e41ce7608a
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2025-02-13 11:58:05 +0100

    Install Plausible Tracking WebAuthn Flow (#2860)
    * Install Plausible with auth funnel
    
    * Setup Plausible from config

--- src/frontend/src/vc-flow.ts
+++ src/frontend/src/vc-flow.ts
@@ -1,4 +1,9 @@
 import { vcFlow } from "./flows/verifiableCredentials";
 import { createSpa } from "./spa";
+import { analytics, initAnalytics } from "./utils/analytics";
 
-void createSpa((connection) => vcFlow({ connection }));
+void createSpa((connection) => {
+  initAnalytics(connection.canisterConfig.analytics_config[0]?.[0]);
+  analytics.pageView();
+  return vcFlow({ connection });
+});
