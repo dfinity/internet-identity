0047cce31331db923a5c55d449f6b7ade823fab3
sea-snake <sea-snake@outlook.com>
2025-02-14 13:56:43 +0100

    Update to latest chrome

--- demos/using-dev-build/wdio.conf.ts
+++ demos/using-dev-build/wdio.conf.ts
@@ -15,7 +15,7 @@ export const config: WebdriverIO.Config = {
   capabilities: [
     {
       browserName: "chrome",
-      browserVersion: "122.0.6261.111", // More information about available versions can be found here: https://github.com/GoogleChromeLabs/chrome-for-testing
+      browserVersion: "133.0.6943.53", // More information about available versions can be found here: https://github.com/GoogleChromeLabs/chrome-for-testing
       "goog:chromeOptions": {
         args: ["headless", "disable-gpu", "disable-dev-shm-usage"],
       },
