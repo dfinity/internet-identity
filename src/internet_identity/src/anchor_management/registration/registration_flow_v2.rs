65cb8e3239d57424ddca1138ad749f21b1f034e0
Andri Schatz <andri.schatz@dfinity.org>
2025-03-21 14:41:42 +0100

    fix being able to create multiple identities per-credential

--- src/internet_identity/src/anchor_management/registration/registration_flow_v2.rs
+++ src/internet_identity/src/anchor_management/registration/registration_flow_v2.rs
@@ -3,9 +3,9 @@ use crate::anchor_management::registration::captcha::{
 };
 use crate::anchor_management::registration::rate_limit::process_rate_limit;
 use crate::anchor_management::registration::Base64;
-use crate::anchor_management::{activity_bookkeeping, post_operation_bookkeeping};
+use crate::anchor_management::{self, activity_bookkeeping, post_operation_bookkeeping};
 use crate::state::flow_states::RegistrationFlowState;
-use crate::storage::anchor::Device;
+use crate::storage::anchor::{self, Device};
 use crate::{openid, state};
 use candid::Principal;
 use ic_cdk::api::time;
@@ -214,14 +214,16 @@ fn create_identity(arg: &CreateIdentityData) -> Result<IdentityNumber, IdRegFini
             let open_id_credential =
                 openid::verify(jwt, salt).map_err(IdRegFinishError::InvalidAuthnMethod)?;
 
-            identity
-                .add_openid_credential(open_id_credential.clone())
-                .map_err(|err| IdRegFinishError::InvalidAuthnMethod(err.to_string()))?;
-            //TODO: add bookkeeping
+            let add_credential_operation = anchor_management::add_openid_credential(&mut identity, open_id_credential.clone())
+                    .map_err(|err| IdRegFinishError::InvalidAuthnMethod(err.to_string()))?;
+            
+            //TODO: add activity bookkeeping
 
-            Operation::RegisterAnchorWithOpenIdCredential {
-                iss: open_id_credential.iss.clone(),
-            }
+            let Operation::AddOpenIdCredential { iss } = add_credential_operation else {
+                unreachable!()
+            };
+
+            Operation::RegisterAnchorWithOpenIdCredential { iss }
         }
     };
 
