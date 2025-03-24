8ff0dd017b370981a1aff4206ab3ff9c63f60ec1
Llorenç <llorenc.muntaner@dfinity.org>
2025-03-24 10:36:18 +0100

    CR changes

--- src/frontend/src/utils/Funnel.ts
+++ src/frontend/src/utils/Funnel.ts
@@ -2,33 +2,31 @@ import { analytics } from "./analytics";
 import { trackWindowSession } from "./trackWindowSession";
 
 export class Funnel<T extends { [K in keyof T]: string }> {
-  private readonly name: string;
-  private readonly events: T;
-  private cleanupSession?: () => void;
+  #name: string;
+  #cleanupSession?: () => void;
 
-  constructor(name: string, events: T) {
-    this.name = name;
-    this.events = events;
+  constructor(name: string) {
+    this.#name = name;
   }
 
   init(): void {
-    analytics.event("start-" + this.name);
+    analytics.event("start-" + this.#name);
 
     // Start window session tracking
-    this.cleanupSession = trackWindowSession({
+    this.#cleanupSession = trackWindowSession({
       onEnterSession: () => {
-        analytics.event(`start-${this.name}-window-session-enter`);
+        analytics.event(`start-${this.#name}-window-session-enter`);
       },
       onLeaveSession: () => {
-        analytics.event(`start-${this.name}-window-session-leave`);
+        analytics.event(`start-${this.#name}-window-session-leave`);
       },
     });
   }
 
   close(): void {
-    if (this.cleanupSession) {
-      this.cleanupSession();
-      this.cleanupSession = undefined;
+    if (this.#cleanupSession) {
+      this.#cleanupSession();
+      this.#cleanupSession = undefined;
     }
   }
 
