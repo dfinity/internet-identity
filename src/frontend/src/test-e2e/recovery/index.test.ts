127d6f63e82f77c6b4ef965806f0a21aea0948a8
sea-snake <sea-snake@outlook.com>
2025-03-27 18:43:34 +0100

    Maybe this works

--- src/frontend/src/test-e2e/recovery/index.test.ts
+++ src/frontend/src/test-e2e/recovery/index.test.ts
@@ -26,24 +26,24 @@ test("Recover with phrase", async () => {
 }, 300_000);
 
 test("Recover with device", async () => {
-  await runInBrowser(async (browser: WebdriverIO.Browser) => {
-    // const loginAuthenticator = await addVirtualAuthenticator(browser);
-    // await browser.url(II_URL);
-    // const userNumber = await FLOWS.registerNewIdentityWelcomeView(browser);
-    // const mainView = new MainView(browser);
-    // await mainView.waitForDeviceDisplay(DEVICE_NAME1);
-    // await removeVirtualAuthenticator(browser, loginAuthenticator);
-    // await browser.pause(10000);
-    //
-    // await addVirtualAuthenticator(browser);
-    // await FLOWS.addRecoveryMechanismDevice(browser);
-    // await mainView.waitForDisplay();
-    // await mainView.logout();
-    //
-    // await FLOWS.recoverUsingDevice(browser, userNumber);
-    // await mainView.waitForDisplay();
-    // await mainView.waitForDeviceDisplay(DEVICE_NAME1);
-  });
+  // await runInBrowser(async (browser: WebdriverIO.Browser) => {
+  // const loginAuthenticator = await addVirtualAuthenticator(browser);
+  // await browser.url(II_URL);
+  // const userNumber = await FLOWS.registerNewIdentityWelcomeView(browser);
+  // const mainView = new MainView(browser);
+  // await mainView.waitForDeviceDisplay(DEVICE_NAME1);
+  // await removeVirtualAuthenticator(browser, loginAuthenticator);
+  // await browser.pause(10000);
+  //
+  // await addVirtualAuthenticator(browser);
+  // await FLOWS.addRecoveryMechanismDevice(browser);
+  // await mainView.waitForDisplay();
+  // await mainView.logout();
+  //
+  // await FLOWS.recoverUsingDevice(browser, userNumber);
+  // await mainView.waitForDisplay();
+  // await mainView.waitForDeviceDisplay(DEVICE_NAME1);
+  // });
 }, 300_000);
 
 test("Recover with both phrase and device", async () => {
