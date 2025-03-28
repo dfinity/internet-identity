da17c787ce91187f45e0b776a70467849107e7e4
sea-snake <sea-snake@outlook.com>
2025-03-28 17:24:35 +0100

    Bump webdriverio version

--- vitest.config.ts
+++ vitest.config.ts
@@ -26,8 +26,8 @@ export default defineConfig(
           // sequentially, and maxThreads must never be lower than minThreads.
           // Therefore, we adjust it here to have the flexibility to set the max
           // to one for the e2e tests only.
-          // minThreads: 1,
-          singleThread: true,
+          minThreads: 1,
+          useAtomics: true,
         },
       },
     },
