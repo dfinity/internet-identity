26e087bbc4f7eca26e6d3876658c8bce2756f2d0
sea-snake <sea-snake@outlook.com>
2025-03-31 16:37:08 +0200

    Cleanup PR

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,6 +20,13 @@ export default defineConfig(
       globals: true,
       watch: false,
       setupFiles: "./src/frontend/test-setup.ts",
+      // Make sure that our browser e2e tests run one by one:
+      // - `sequence.concurrent: false` within each test file
+      // - `fileParallelism: false` across test files
+      //
+      // This makes sure that WebdriverIO won't start more than one browser
+      // instance at the same time, which would result in the following error:
+      // `Could not start browser after 5 retries: Error: spawn ETXTBSY`
       sequence: {
         concurrent: false,
       },
