bd606094a857f0a05c38e33230826dd5e28c5424
Andri Schatz <andri.schatz@dfinity.org>
2025-04-09 15:30:05 +0200

    cleanup

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { readCanisterConfig } from "$lib/utils/init";
   import { analytics, initAnalytics } from "$lib/utils/analytics/analytics";
-  // TODO: Enable new styles only in the new layout pages.
-  // import "./app.css";
 
-  const { children, data } = $props();
+  const { children } = $props();
 
   const canisterConfig = readCanisterConfig();
 
