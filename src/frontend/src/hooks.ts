05f9a0202e334298e9b521c5e0c165233372248b
Andri Schatz <andri.schatz@dfinity.org>
2025-04-10 14:54:55 +0200

    try updating skeleton import

--- src/frontend/src/hooks.ts
+++ src/frontend/src/hooks.ts
@@ -1,11 +1,13 @@
 import { Reroute } from "@sveltejs/kit";
 import { WEBAUTHN_IFRAME_PATH } from "$lib/flows/iframeWebAuthn";
 import { getAddDeviceAnchor } from "$lib/utils/addDeviceLink";
-import { nonNullish } from "@dfinity/utils";
+import { isNullish, nonNullish } from "@dfinity/utils";
 import { DISCOVERABLE_PASSKEY_FLOW } from "$lib/state/featureFlags";
 import { get } from "svelte/store";
 
 export const reroute: Reroute = ({ url }) => {
+  if (isNullish(globalThis.window)) return;
+
   if (nonNullish(getAddDeviceAnchor(url))) {
     return "/register/device";
   }
