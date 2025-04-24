9f96a9551549135a12314b1f9b6707b7cfff950b
Llorenç <llorenc.muntaner@dfinity.org>
2025-04-24 10:53:04 +0200

    CR changes

--- src/frontend/src/routes/(new-styling)/new-authorize/components/ConnectOrCreatePasskey.svelte
+++ src/frontend/src/routes/(new-styling)/new-authorize/components/ConnectOrCreatePasskey.svelte
@@ -10,17 +10,15 @@
 
 <div class="mb-8 flex flex-col gap-4" in:fly={{ duration: 200, x: -10 }}>
   <p>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus alias
-    amet quas, ducimus iste iure et.
+    Passkeys offer a secure, password-free method for logging into your
+    accounts, eliminating the need to remember complex passwords.
   </p>
   <p>
-    Boriosam aliquid rerum dolore porro optio, explicabo corrupti temporibus
-    consequuntur quae accusamus eligendi eius.
+    A unique cryptographic key is stored on your device, enabling a faster, more
+    secure, and reliabe way to authenticate your identity.
   </p>
 </div>
 <div class="mt-auto flex flex-col items-stretch gap-4">
-  <Button onclick={create} variant="secondary"
-    >Don't have a passkey? Create one</Button
-  >
-  <Button onclick={connect} variant="secondary">Connect my Passkey</Button>
+  <Button onclick={create} variant="secondary">Create new passkey</Button>
+  <Button onclick={connect} variant="secondary">Already have a passkey?</Button>
 </div>
