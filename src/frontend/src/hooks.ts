d44270dd19ef481f4ad86d62dcd64595a8c539ae
sea-snake <104725312+sea-snake@users.noreply.github.com>
2025-04-29 11:20:57 +0200

    Fix VC flow on local (#3031)
    * Fix VC flow on local
    
    * Fix VC flow on local
    
    * Remove new-authenticate page for now, so that pre-render passes.

--- src/frontend/src/hooks.ts
+++ src/frontend/src/hooks.ts
@@ -1,13 +1,11 @@
 import { Reroute } from "@sveltejs/kit";
 import { WEBAUTHN_IFRAME_PATH } from "$lib/flows/iframeWebAuthn";
 import { getAddDeviceAnchor } from "$lib/utils/addDeviceLink";
-import { isNullish, nonNullish } from "@dfinity/utils";
+import { nonNullish } from "@dfinity/utils";
 import { DISCOVERABLE_PASSKEY_FLOW } from "$lib/state/featureFlags";
 import { get } from "svelte/store";
 
 export const reroute: Reroute = ({ url }) => {
-  if (isNullish(globalThis.window)) return;
-
   if (nonNullish(getAddDeviceAnchor(url))) {
     return "/register/device";
   }
@@ -19,13 +17,5 @@ export const reroute: Reroute = ({ url }) => {
   }
   if (url.hash === "#authorize") {
     return get(DISCOVERABLE_PASSKEY_FLOW) ? "/new-authorize" : "/authorize";
-  }
-  // TODO: this is out of scope for this release
-  // if (url.pathname === "/" && get(DISCOVERABLE_PASSKEY_FLOW)) {
-  //   return "/new-authenticate";
-  // }
-  // for now, reroute to index if someone tries to get in there
-  if (url.pathname === "/new-authenticate") {
-    return "/";
   }
 };
