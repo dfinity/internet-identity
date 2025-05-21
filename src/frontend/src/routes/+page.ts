1e93493e3ac9d5d98ba7a80bd490e43ee8a9a7b5
Llorenç <llorenc.muntaner@dfinity.org>
2025-05-21 15:23:41 +0200

    Fix typo

--- src/frontend/src/routes/+page.ts
+++ src/frontend/src/routes/+page.ts
@@ -1,7 +1,7 @@
 import { redirect, type Load, type LoadEvent } from "@sveltejs/kit";
 
 export const load: Load = (event: LoadEvent) => {
-  // TODO: Remove when we launch lanidng page in id.ai
+  // TODO: Remove when we launch landing page in id.ai
   const hostname = event.url.hostname;
   if (hostname === "id.ai") {
     throw redirect(307, "https://identity.internetcomputer.org");
