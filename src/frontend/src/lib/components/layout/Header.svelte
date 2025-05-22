3090353f7b2a93afad2a2d15b59a06b09bc59987
Llorenç <llorenc.muntaner@dfinity.org>
2025-05-22 09:23:24 +0200

    Fix typo

--- src/frontend/src/lib/components/layout/Header.svelte
+++ src/frontend/src/lib/components/layout/Header.svelte
@@ -11,7 +11,7 @@
   {...props}
   class={["flex h-14 items-center px-4 py-3 md:px-6 lg:px-8", className]}
 >
-  <!-- TODO: Revert to anchor tab when we create the landing page -->
+  <!-- TODO: Revert to anchor tag when we create the landing page -->
   <div class="flex h-14 items-center gap-4">
     <Logo class="text-fg-primary h-5.5" />
     <h1 class="text-md text-text-primary hidden font-semibold md:block">
