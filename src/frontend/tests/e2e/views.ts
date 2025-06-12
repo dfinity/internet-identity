bcfc030854ba9ab7e92f9074bc1c025b2c9b0ef5
Andri Schatz <andri.schatz@dfinity.org>
2025-06-12 14:44:39 +0200

    updated test to scroll button into view

--- src/frontend/tests/e2e/views.ts
+++ src/frontend/tests/e2e/views.ts
@@ -338,7 +338,14 @@ export class MainView extends View {
 
   async remove(deviceName: string): Promise<void> {
     await this.openDeviceActions({ deviceName });
-    await this.deviceAction({ deviceName, action: "remove" }).click();
+    const removeButton = await this.deviceAction({
+      deviceName,
+      action: "remove",
+    });
+    await removeButton.waitForDisplayed();
+    await removeButton.scrollIntoView();
+    await removeButton.click();
+
     const confirmRemoveDeviceView = new ConfirmRemoveDeviceView(this.browser);
     await confirmRemoveDeviceView.waitForDisplay();
     await confirmRemoveDeviceView.enterAlias(deviceName);
