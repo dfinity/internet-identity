6cc2e3acd098535066133036cf20dff568ffe41f
Andri Schatz <andri.schatz@dfinity.org>
2025-06-19 15:58:33 +0200

    spacing

--- src/frontend/src/lib/components/ui/Tabs.svelte
+++ src/frontend/src/lib/components/ui/Tabs.svelte
@@ -4,7 +4,7 @@
   const { children } = $props();
 </script>
 
-<nav class="px-4 py-3">
+<nav class="mb-4 px-4 py-3">
   <div class="mb-6 flex gap-2">
     <Logo class="text-fg-primary h-5.5" />
     <h1 class="text-text-primary font-semibold sm:block">
