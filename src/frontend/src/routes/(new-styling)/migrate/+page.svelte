1918420062394b24bdabc2655cf2d06e9ed045de
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2025-06-24 20:26:51 +0200

    Migration flow for existing users base functionality (#3137)
    * Authenticate and create new passkey
    
    * Go to manage on success
    
    * Fix continue as migrated identity
    
    * Fix typo
    
    * Add origin and authentication state
    
    * CR changes

---
+++ src/frontend/src/routes/(new-styling)/migrate/+page.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import AuthPanel from "$lib/components/layout/AuthPanel.svelte";
+  import Header from "$lib/components/layout/Header.svelte";
+  import Footer from "$lib/components/layout/Footer.svelte";
+  import Input from "$lib/components/ui/Input.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
+  import { isNullish, nonNullish } from "@dfinity/utils";
+  import { MigrationFlow } from "$lib/flows/migrationFlow.svelte";
+  import CreatePasskey from "$lib/components/views/CreatePasskey.svelte";
+  import { goto } from "$app/navigation";
+  import { lastUsedIdentitiesStore } from "$lib/stores/last-used-identities.store";
+  import ProgressRing from "$lib/components/ui/ProgressRing.svelte";
+  import { handleError } from "$lib/components/utils/error";
+
+  let identityNumber: number | undefined = $state(undefined);
+  const migrationFlow = new MigrationFlow();
+
+  const handleSubmit = async () => {
+    if (nonNullish(identityNumber)) {
+      await migrationFlow
+        .authenticateWithIdentityNumber(BigInt(identityNumber))
+        .catch(handleError);
+    }
+  };
+
+  const handleCreate = async (name: string) => {
+    if (isNullish(migrationFlow.identityNumber)) {
+      throw new Error("Identity number is null");
+    }
+    await migrationFlow.createPasskey(name);
+    lastUsedIdentitiesStore.selectIdentity(migrationFlow.identityNumber);
+  };
+</script>
+
+<div class="flex min-h-[100dvh] flex-col" data-page="migration-view">
+  <div class="h-[env(safe-area-inset-top)]"></div>
+  <Header />
+  <div class="flex flex-1 flex-col items-center justify-center">
+    <div class="col-start-1 row-start-1 flex flex-col">
+      <AuthPanel>
+        {#if migrationFlow.view === "enterNumber"}
+          {@render enterIdentityNumber()}
+        {:else if migrationFlow.view === "enterName"}
+          <CreatePasskey create={handleCreate} />
+        {:else}
+          {@render successfulMigration()}
+        {/if}
+      </AuthPanel>
+    </div>
+  </div>
+  <Footer />
+  <div class="h-[env(safe-area-inset-bottom)]"></div>
+</div>
+
+{#snippet enterIdentityNumber()}
+  <form class="flex flex-1 flex-col">
+    <div class="mb-8 flex flex-col">
+      <h1 class="text-text-primary mb-3 text-2xl font-medium">
+        Enter the identity number
+      </h1>
+      <p class="text-md text-text-tertiary mb-6 font-medium">
+        This process will allow you to use the new authentication flow.
+      </p>
+      <Input
+        bind:value={identityNumber}
+        inputmode="numeric"
+        placeholder="Identity number"
+        type="number"
+        size="md"
+        autocomplete="off"
+        autocorrect="off"
+        spellcheck="false"
+        aria-label="Identity number"
+      />
+    </div>
+    <div class="mt-auto flex flex-col items-stretch gap-3">
+      <Button
+        onclick={handleSubmit}
+        variant="primary"
+        size="lg"
+        type="submit"
+        disabled={isNullish(identityNumber) || migrationFlow.authenticating}
+      >
+        {#if migrationFlow.authenticating}
+          <ProgressRing />
+          <span>Authenticating...</span>
+        {:else}
+          <span>Migrate Identity</span>
+        {/if}
+      </Button>
+    </div>
+  </form>
+{/snippet}
+
+{#snippet successfulMigration()}
+  <div class="mb-8 flex flex-col">
+    <h1 class="text-text-primary mb-3 text-2xl font-medium">
+      Migration completed
+    </h1>
+    <p class="text-md text-text-tertiary mb-6 font-medium">
+      You can now authenticate with your identity in the new flow.
+    </p>
+    <Button
+      onclick={() => goto("/manage")}
+      variant="primary"
+      size="lg"
+      type="submit"
+    >
+      <span>Go to manage</span>
+    </Button>
+  </div>
+{/snippet}
