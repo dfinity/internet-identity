b30a0a78342a747a8a779dffb63191637d6f1b30
Llorenç <llorenc.muntaner@dfinity.org>
2025-07-03 10:54:48 +0200

    Add check for name when user visits dashboard in e2e test

--- src/frontend/tests/e2e-playwright/index.spec.ts
+++ src/frontend/tests/e2e-playwright/index.spec.ts
@@ -1,61 +1,105 @@
 import { test } from "@playwright/test";
 import { clearStorage, createIdentity, dummyAuth, II_URL } from "./utils";
 
+// Test user names
+const DEFAULT_USER_NAME = "John Doe";
+const SECONDARY_USER_NAME = "Jane Doe";
+
 test.describe("First visit", () => {
   test("Sign up with a new passkey", async ({ page }) => {
     const auth = dummyAuth();
     await page.goto(II_URL);
     await page.getByRole("button", { name: "Continue with Passkey" }).click();
     await page.getByRole("button", { name: "Set up a new Passkey" }).click();
-    await page.getByLabel("Identity name").fill("John Doe");
+    await page.getByLabel("Identity name").fill(DEFAULT_USER_NAME);
     auth(page);
     await page.getByRole("button", { name: "Create Passkey" }).click();
     await page.waitForURL(II_URL + "/manage");
+    await page
+      .getByRole("heading", {
+        name: new RegExp(`Welcome, ${DEFAULT_USER_NAME}!`),
+      })
+      .isVisible();
+    await page
+      .getByRole("heading", { level: 5, name: DEFAULT_USER_NAME })
+      .isVisible();
   });
 
   test("Sign in with an existing passkey", async ({ page }) => {
     const auth = dummyAuth();
-    await createIdentity(page, "John Doe", auth);
+    await createIdentity(page, DEFAULT_USER_NAME, auth);
     await clearStorage(page);
     await page.goto(II_URL);
     await page.getByRole("button", { name: "Continue with Passkey" }).click();
     auth(page);
     await page.getByRole("button", { name: "Use an existing Passkey" }).click();
     await page.waitForURL(II_URL + "/manage");
+    await page
+      .getByRole("heading", {
+        name: new RegExp(`Welcome, ${DEFAULT_USER_NAME}!`),
+      })
+      .isVisible();
+    await page
+      .getByRole("heading", { level: 5, name: DEFAULT_USER_NAME })
+      .isVisible();
   });
 });
 
 test.describe("Last used identities listed", () => {
   test("Sign in with last used identity", async ({ page }) => {
     const auth = dummyAuth();
-    await createIdentity(page, "John Doe", auth);
+    await createIdentity(page, DEFAULT_USER_NAME, auth);
     await page.goto(II_URL);
     auth(page);
-    await page.getByRole("button", { name: "John Doe" }).click();
+    await page.getByRole("button", { name: DEFAULT_USER_NAME }).click();
     await page.waitForURL(II_URL + "/manage");
+    await page
+      .getByRole("heading", {
+        name: new RegExp(`Welcome, ${DEFAULT_USER_NAME}!`),
+      })
+      .isVisible();
+    await page
+      .getByRole("heading", { level: 5, name: DEFAULT_USER_NAME })
+      .isVisible();
   });
 
   test("Sign in with another identity", async ({ page }) => {
     const auth = dummyAuth();
-    await createIdentity(page, "John Doe", auth);
+    await createIdentity(page, DEFAULT_USER_NAME, auth);
     await page.goto(II_URL);
     await page.getByRole("button", { name: "Use another identity" }).click();
     await page.getByRole("button", { name: "Continue with Passkey" }).click();
     auth(page);
     await page.getByRole("button", { name: "Use an existing Passkey" }).click();
     await page.waitForURL(II_URL + "/manage");
+    await page
+      .getByRole("heading", {
+        name: new RegExp(`Welcome, ${DEFAULT_USER_NAME}!`),
+      })
+      .isVisible();
+    await page
+      .getByRole("heading", { level: 5, name: DEFAULT_USER_NAME })
+      .isVisible();
   });
 
   test("Sign up with a new passkey", async ({ page }) => {
     const auth = dummyAuth();
-    await createIdentity(page, "John Doe", auth);
+    await createIdentity(page, DEFAULT_USER_NAME, auth);
     await page.goto(II_URL);
     await page.getByRole("button", { name: "Use another identity" }).click();
     await page.getByRole("button", { name: "Continue with Passkey" }).click();
     await page.getByRole("button", { name: "Set up a new Passkey" }).click();
-    await page.getByLabel("Identity name").fill("Jane Doe");
+    await page.getByLabel("Identity name").fill(SECONDARY_USER_NAME);
     auth(page);
     await page.getByRole("button", { name: "Create Passkey" }).click();
     await page.waitForURL(II_URL + "/manage");
+    await page
+      .getByRole("heading", {
+        name: new RegExp(`Welcome, ${SECONDARY_USER_NAME}!`),
+      })
+      .isVisible();
+    await page
+      .getByRole("heading", { level: 5, name: SECONDARY_USER_NAME })
+      .isVisible();
   });
 });
