fba11662bfc553e6b22ff098862b1bd4fb74e24b
Andri Schatz <andri.schatz@dfinity.org>
2025-07-11 16:28:04 -0700

    start implementing e2e

--- src/frontend/tests/e2e-playwright/utils.ts
+++ src/frontend/tests/e2e-playwright/utils.ts
@@ -113,3 +113,112 @@ export const clearStorage = async (page: Page): Promise<void> => {
   await page.goto(II_URL);
   await page.evaluate(() => localStorage.clear());
 };
+
+export const visibleAndClick = async (
+  page: Page,
+  role:
+    | "alert"
+    | "alertdialog"
+    | "application"
+    | "article"
+    | "banner"
+    | "blockquote"
+    | "button"
+    | "caption"
+    | "cell"
+    | "checkbox"
+    | "code"
+    | "columnheader"
+    | "combobox"
+    | "complementary"
+    | "contentinfo"
+    | "definition"
+    | "deletion"
+    | "dialog"
+    | "directory"
+    | "document"
+    | "emphasis"
+    | "feed"
+    | "figure"
+    | "form"
+    | "generic"
+    | "grid"
+    | "gridcell"
+    | "group"
+    | "heading"
+    | "img"
+    | "insertion"
+    | "link"
+    | "list"
+    | "listbox"
+    | "listitem"
+    | "log"
+    | "main"
+    | "marquee"
+    | "math"
+    | "meter"
+    | "menu"
+    | "menubar"
+    | "menuitem"
+    | "menuitemcheckbox"
+    | "menuitemradio"
+    | "navigation"
+    | "none"
+    | "note"
+    | "option"
+    | "paragraph"
+    | "presentation"
+    | "progressbar"
+    | "radio"
+    | "radiogroup"
+    | "region"
+    | "row"
+    | "rowgroup"
+    | "rowheader"
+    | "scrollbar"
+    | "search"
+    | "searchbox"
+    | "separator"
+    | "slider"
+    | "spinbutton"
+    | "status"
+    | "strong"
+    | "subscript"
+    | "superscript"
+    | "switch"
+    | "tab"
+    | "table"
+    | "tablist"
+    | "tabpanel"
+    | "term"
+    | "textbox"
+    | "time"
+    | "timer"
+    | "toolbar"
+    | "tooltip"
+    | "tree"
+    | "treegrid"
+    | "treeitem",
+  options?: {
+    checked?: boolean;
+    disabled?: boolean;
+    exact?: boolean;
+    expanded?: boolean;
+    includeHidden?: boolean;
+    level?: number;
+    name?: string | RegExp;
+    pressed?: boolean;
+    selected?: boolean;
+  },
+) => {
+  await expect(
+    page.getByRole(role, {
+      ...options,
+    }),
+  ).toBeVisible();
+  await page
+    .getByRole(role, {
+      ...options,
+    })
+    .click();
+};
