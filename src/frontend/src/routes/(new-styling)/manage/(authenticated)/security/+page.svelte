9dbfc6dc0306010a5af6d5ceef7bb1d5ef78ffae
Llorenç <llorenc.muntaner@dfinity.org>
2025-07-14 10:43:42 +0200

    Remove unused path

--- src/frontend/src/routes/(new-styling)/manage/(authenticated)/security/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1 class="text-text-primary mb-4 text-3xl font-semibold">Security</h1>
-<p class="text-text-tertiary text-md mb-12">
-  Settings and recommendations to keep your identity secure
-</p>
-<AccessMethods />
