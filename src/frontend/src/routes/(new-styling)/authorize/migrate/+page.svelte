2896f6695e14df74ae23303e497f821cf03cbad4
Andri Schatz <andri.schatz@dfinity.org>
2025-07-17 18:32:36 -0400

    adjuts

--- src/frontend/src/routes/(new-styling)/authorize/migrate/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import MigrationWizard from "$lib/components/wizards/MigrationWizard.svelte";
-  import { authorizationStore } from "$lib/stores/authorization.store";
-</script>
-
-<MigrationWizard
-  onSuccess={() => authorizationStore.authorize(undefined, 4000)}
-/>
