8209a7e230cc658ff5920f0495fb4787b664eb4f
sea-snake <sea-snake@outlook.com>
2025-07-23 13:05:06 +0200

    Redirect activate links to dashboard

--- src/frontend/src/routes/(new-styling)/activate/+page.svelte
+++ src/frontend/src/routes/(new-styling)/activate/+page.svelte
@@ -2,7 +2,8 @@
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
 
-  // Confirming a passkey from another device is handled in the dashboard
+  // Confirming a passkey from another device is handled in the dashboard,
+  // this can't be a load function since URL hash can't be read in there.
   onMount(() => {
     goto(`/manage?activate=${window.location.hash.slice(1)}`, {
       replaceState: true,
