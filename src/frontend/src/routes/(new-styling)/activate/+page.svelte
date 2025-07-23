18e7d0885d44f26bdba897455c8f1556c3c951f7
sea-snake <sea-snake@outlook.com>
2025-07-23 13:19:58 +0200

    Add error toast if code is invalid.

--- src/frontend/src/routes/(new-styling)/activate/+page.svelte
+++ src/frontend/src/routes/(new-styling)/activate/+page.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
+  import { toaster } from "$lib/components/utils/toaster";
 
   // Confirming a passkey from another device is handled in the dashboard,
   // this can't be a load function since URL hash can't be read in there.
-  onMount(() => {
-    goto(`/manage?activate=${window.location.hash.slice(1)}`, {
-      replaceState: true,
-    });
+  onMount(async () => {
+    const code = window.location.hash.slice(1);
+    const isValid = code.length === 5;
+    await goto(
+      isValid ? `/manage?activate=${window.location.hash.slice(1)}` : "/manage",
+      {
+        replaceState: true,
+      },
+    );
+    if (!isValid) {
+      toaster.error({
+        title: "Invalid code link",
+        description: "We didn't recognize that activation code.",
+      });
+    }
   });
 </script>
