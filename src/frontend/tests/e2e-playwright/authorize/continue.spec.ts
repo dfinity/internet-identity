462b4ccf250f68e645ebde5eaf619d03b9ec2b23
Andri Schatz <andri.schatz@dfinity.org>
2025-07-25 17:38:58 +0200

    revert button names

--- src/frontend/tests/e2e-playwright/authorize/continue.spec.ts
+++ src/frontend/tests/e2e-playwright/authorize/continue.spec.ts
@@ -25,7 +25,7 @@ test("Authorize by switching to another identity", async ({ page }) => {
   const otherPrincipal = await createIdentity(page, "Jane Doe", auth2);
   const principal = await authorize(page, async (authPage) => {
     await authPage.getByRole("button", { name: "Jane Doe" }).click();
-    await authPage.getByRole("button", { name: "John Doe" }).click();
+    await authPage.getByRole("button", { name: "Switch identity" }).click();
     auth1(authPage);
     await authPage.getByRole("button", { name: "Primary account" }).click();
   });
@@ -39,7 +39,7 @@ test("Authorize by signing in with a different passkey", async ({ page }) => {
   const expectedPrincipal = await createIdentity(page, "John Doe", auth1);
   const otherPrincipal = await createIdentity(page, "Jane Doe", auth2);
   const principal = await authorize(page, async (authPage) => {
-    await authPage.getByRole("button", { name: "Jane Doe" }).click();
+    await authPage.getByRole("button", { name: "Switch identity" }).click();
     await authPage
       .getByRole("button", { name: "Use another identity" })
       .click();
