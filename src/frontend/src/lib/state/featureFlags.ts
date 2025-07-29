cbc46bafac04080c3ce8e277e48f96ce7961fb34
sea-snake <sea-snake@outlook.com>
2025-07-29 15:48:01 +0200

    Move feature flag to init config

--- src/frontend/src/lib/state/featureFlags.ts
+++ src/frontend/src/lib/state/featureFlags.ts
@@ -1,6 +1,7 @@
 import { writable, type Writable } from "svelte/store";
 import { FeatureFlag } from "$lib/utils/featureFlags";
 import { isNullish } from "@dfinity/utils";
+import { canisterConfig } from "$lib/globals";
 
 declare global {
   interface Window {
@@ -10,15 +11,16 @@ declare global {
 
 type FeatureFlagStore = Writable<boolean> & {
   getFeatureFlag: () => FeatureFlag | undefined;
+  initialize: () => void;
 };
 
 const LOCALSTORAGE_FEATURE_FLAGS_PREFIX = "ii-localstorage-feature-flags__";
 
 const createFeatureFlagStore = (
   name: string,
-  defaultValue: boolean,
+  getDefaultValue: () => boolean,
 ): FeatureFlagStore => {
-  const { subscribe, set, update } = writable(defaultValue);
+  const { subscribe, set, update } = writable(false);
 
   // We cannot use browser because this is also imported in our showcase
   if (isNullish(globalThis.window)) {
@@ -27,25 +29,28 @@ const createFeatureFlagStore = (
       set,
       update,
       getFeatureFlag: () => undefined,
+      initialize: () => undefined,
     };
   }
 
   // Initialize feature flag object with value from localstorage
+  let initializedFeatureFlag: FeatureFlag;
+  const initialize = (): void => {
+    initializedFeatureFlag = new FeatureFlag(
+      window.localStorage,
+      LOCALSTORAGE_FEATURE_FLAGS_PREFIX + name,
+      getDefaultValue(),
+      { subscribe, set, update },
+    );
 
-  const initializedFeatureFlag: FeatureFlag = new FeatureFlag(
-    window.localStorage,
-    LOCALSTORAGE_FEATURE_FLAGS_PREFIX + name,
-    defaultValue,
-    { subscribe, set, update },
-  );
-
-  // Make feature flags configurable from browser console
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  if (typeof window.__featureFlags === "undefined") {
-    window.__featureFlags = {};
-  }
-  window.__featureFlags[name] = initializedFeatureFlag;
+    // Make feature flags configurable from browser console
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (typeof window.__featureFlags === "undefined") {
+      window.__featureFlags = {};
+    }
+    window.__featureFlags[name] = initializedFeatureFlag;
+  };
 
   const getFeatureFlag = () => {
     return initializedFeatureFlag;
@@ -56,44 +61,45 @@ const createFeatureFlagStore = (
     set,
     update,
     getFeatureFlag,
+    initialize,
   };
 };
 
 export const DOMAIN_COMPATIBILITY = createFeatureFlagStore(
   "DOMAIN_COMPATIBILITY",
-  true,
+  () => true,
 );
 
 export const OPENID_AUTHENTICATION = createFeatureFlagStore(
   "OPENID_AUTHENTICATION",
-  false,
+  () => false,
 );
 
 export const HARDWARE_KEY_TEST = createFeatureFlagStore(
   "HARDWARE_KEY_TEST",
-  false,
+  () => false,
 );
 
 export const DISCOVERABLE_PASSKEY_FLOW = createFeatureFlagStore(
   "DISCOVERABLE_PASSKEY_FLOW",
-  false,
+  () => false,
 );
 
 export const ENABLE_MIGRATE_FLOW = createFeatureFlagStore(
   "ENABLE_MIGRATE_FLOW",
-  false,
+  () => false,
 );
 export const ADD_ACCESS_METHOD = createFeatureFlagStore(
   "ADD_ACCESS_METHOD",
-  true,
+  () => true,
 );
 
 export const CONTINUE_FROM_ANOTHER_DEVICE = createFeatureFlagStore(
   "CONTINUE_FROM_ANOTHER_DEVICE",
-  false,
+  () => canisterConfig.feature_flag_continue_from_another_device[0] ?? false,
 );
 
-export const FLAIR = createFeatureFlagStore("FLAIR", false);
+export const FLAIR = createFeatureFlagStore("FLAIR", () => false);
 
 export default {
   DOMAIN_COMPATIBILITY,
