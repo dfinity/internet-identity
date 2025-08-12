990b4ec007eb98351a819656418b33f1ecf66c21
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2025-08-12 13:52:40 +0200

    Migration Success Screen UI (#3258)
    * Migration Success Screen UI
    
    * Break layout
    
    * Add language to script
    
    * Change text

---
+++ src/frontend/src/lib/components/illustrations/PasscodeIllustration.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import type { SVGAttributes } from "svelte/elements";
+
+  type Props = SVGAttributes<SVGSVGElement>;
+
+  const { class: className, ...props }: Props = $props();
+</script>
+
+<svg
+  {...props}
+  class={["fill-none stroke-current", className]}
+  viewBox="0 0 53 53"
+  xmlns="http://www.w3.org/2000/svg"
+  ><path
+    stroke-linecap="round"
+    vector-effect="non-scaling-stroke"
+    d="M20.873 9.586 9.73 20.729M42.873 31.586 31.73 42.729M18.873 26.158h-7.429M41.16 26.158h-7.43"
+  /><circle
+    cx="4.302"
+    cy="26.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="26.302"
+    cy="26.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="48.302"
+    cy="26.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="4.302"
+    cy="48.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="26.302"
+    cy="48.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="48.302"
+    cy="48.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="4.302"
+    cy="4.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="26.302"
+    cy="4.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /><circle
+    cx="48.302"
+    cy="4.158"
+    r="3.5"
+    vector-effect="non-scaling-stroke"
+  /></svg
+>
--- src/frontend/src/routes/(new-styling)/authorize/(authenticated)/upgrade-success/+page.svelte
+++ src/frontend/src/routes/(new-styling)/authorize/(authenticated)/upgrade-success/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
   import Button from "$lib/components/ui/Button.svelte";
   import { authorizationStore } from "$lib/stores/authorization.store";
+  import MigrationSuccessIllustration from "$lib/components/illustrations/MigrationSuccessIllustration.svelte";
 
   const handleRedirect = () => {
     authorizationStore.authorize(undefined);
   };
 </script>
 
-<Button onclick={handleRedirect}>Go to app</Button>
+<div class="flex flex-1 flex-col items-center justify-center p-4 sm:max-w-100">
+  <div class="text-text-primary flex h-50 items-center justify-center">
+    <MigrationSuccessIllustration />
+  </div>
+  <div class="mb-8 flex flex-col gap-2">
+    <h1 class="text-text-primary mb-3 text-center text-2xl font-medium">
+      Identity upgraded!
+    </h1>
+    <p class="text-md text-text-tertiary text-center font-medium text-balance">
+      You no longer need to remember your identity number and can use your
+      fingerprint, face or screen lock instead.
+    </p>
+  </div>
+  <Button variant="primary" onclick={handleRedirect}>Go to app</Button>
+</div>
