8d69c4e6137b590be102a7d2b3de6815060df376
Llorenç <llorenc.muntaner@dfinity.org>
2025-08-12 16:01:44 +0200

    Refactor layout structure

--- src/frontend/src/routes/(new-styling)/authorize/(panel)/+page.ts
+++ src/frontend/src/routes/(new-styling)/authorize/(panel)/+page.ts
@@ -1,8 +1,8 @@
-import type { PageLoad } from "./$types";
 import { get } from "svelte/store";
 import { lastUsedIdentitiesStore } from "$lib/stores/last-used-identities.store";
 import { redirect } from "@sveltejs/kit";
 import { nonNullish } from "@dfinity/utils";
+import type { PageLoad } from "./$types";
 
 let firstVisit = true;
 
