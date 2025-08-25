def0f4e580d89f14b118060524c879e2fa1626eb
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2025-08-25 21:29:01 +0200

    third

---
+++ src/frontend/src/routes/faq/+page.ts
@@ -0,0 +1,9 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+let firstVisit = true;
+
+export const load: PageLoad = () => {
+  // Redirect FAQ to external support site using SvelteKit's redirect
+      throw redirect(307, "https://identitysupport.dfinity.org/hc/en-us");
+};
