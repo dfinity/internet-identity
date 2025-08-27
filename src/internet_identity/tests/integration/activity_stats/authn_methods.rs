955f7f52190346a9e812ece2e72beea91a371407
sea-snake <sea-snake@outlook.com>
2025-08-27 16:42:22 +0200

    Duplicate `should_report_active_openid_authn_methods`.

--- src/internet_identity/tests/integration/activity_stats/authn_methods.rs
+++ src/internet_identity/tests/integration/activity_stats/authn_methods.rs
@@ -1,7 +1,7 @@
-use crate::google;
 use crate::v2_api::authn_method_test_helpers::{
     create_identity_with_authn_method, create_identity_with_openid_credential, test_authn_method,
 };
+use crate::{google, openid};
 use candid::Principal;
 use canister_tests::api::internet_identity as api;
 use canister_tests::api::internet_identity::api_v2;
@@ -243,6 +243,68 @@ fn should_keep_stats_across_upgrades() -> Result<(), RejectResponse> {
 /// TODO: ID-155 Create OpenID mock data on-demand and use this instead in unit/integration tests
 #[test]
 fn should_report_active_openid_authn_methods() {
+    // Create II instance that mocks Google certs
+    let env = env();
+    let canister_id = openid::setup_canister(&env);
+    api::init_salt(&env, canister_id).unwrap();
+
+    // OpenID test data and fetch Google certs (mock)
+    let (jwt, salt, _claims, test_time, test_principal, _test_authn_method) =
+        openid::openid_test_data();
+    env.advance_time(Duration::from_millis(test_time) - Duration::from_nanos(time(&env)));
+
+    // Ensure stats are initially absent
+    assert!(
+        !get_metrics(&env, canister_id).contains("internet_identity_daily_active_authn_methods")
+    );
+
+    // Create account with Google OpenID
+    let identity_number =
+        create_identity_with_openid_credential(&env, canister_id, &jwt, &salt, test_principal);
+
+    // Get OpenID delegation principal
+    let delegation_principal = Principal::self_authenticating(
+        api::openid_prepare_delegation(
+            &env,
+            canister_id,
+            test_principal,
+            &jwt,
+            &salt,
+            &ByteBuf::from([0u8; 32]),
+        )
+        .unwrap()
+        .unwrap()
+        .user_key,
+    );
+
+    // Repeated activity within the same period should not increase the counter
+    api_v2::identity_info(&env, canister_id, delegation_principal, identity_number)
+        .unwrap()
+        .unwrap();
+
+    // Check daily stats, some activity is required first to update the stats
+    env.advance_time(Duration::from_secs(DAY_SECONDS));
+    create_identity_with_authn_method(&env, canister_id, &test_authn_method());
+    assert_metric(
+        &get_metrics(&env, canister_id),
+        "internet_identity_daily_active_authn_methods{type=\"openid\",issuer=\"https://accounts.google.com\"}",
+        1f64,
+    );
+
+    // Check monthly stats, some activity is required first to update the stats
+    env.advance_time(Duration::from_secs(MONTH_SECONDS - DAY_SECONDS));
+    create_identity_with_authn_method(&env, canister_id, &test_authn_method());
+    assert_metric(
+        &get_metrics(&env, canister_id),
+        "internet_identity_monthly_active_authn_methods{type=\"openid\",issuer=\"https://accounts.google.com\"}",
+        1f64,
+    );
+}
+
+/// Tests that active Google authn_methods are counted correctly.
+/// TODO: ID-155 Create OpenID mock data on-demand and use this instead in unit/integration tests
+#[test]
+fn should_report_active_google_authn_methods() {
     // Create II instance that mocks Google certs
     let env = env();
     let canister_id = google::setup_canister(&env);
