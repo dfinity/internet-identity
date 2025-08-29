8e48f99bebc992a82b1cb5e6a8ec288a23a8ef6d
Llorenç <llorenc.muntaner@dfinity.org>
2025-08-29 08:06:01 +0200

    Change E2E

--- src/frontend/tests/e2e-playwright/authorize/continue.spec.ts
+++ src/frontend/tests/e2e-playwright/authorize/continue.spec.ts
@@ -26,9 +26,8 @@ test("Authorize by switching to another identity", async ({ page }) => {
   const otherPrincipal = await createIdentity(page, "Jane Doe", auth2);
   const principal = await authorize(page, async (authPage) => {
     await authPage.getByRole("button", { name: "Switch identity" }).click();
-    await authPage.getByRole("button", { name: "John Doe" }).click();
     auth1(authPage);
-    await authPage.getByRole("button", { name: "Primary account" }).click();
+    await authPage.getByRole("button", { name: "John Doe" }).click();
   });
   expect(principal).toBe(expectedPrincipal);
   expect(principal).not.toBe(otherPrincipal);
