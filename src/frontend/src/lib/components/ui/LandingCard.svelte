16b57a3ce89cb663f2c193c3382c94fa9d7b24a3
Dan Blackadder <danblackadder@gmail.com>
2025-09-03 15:23:12 +0100

    Update LandingCard to handle screens smaller than sm

--- src/frontend/src/lib/components/ui/LandingCard.svelte
+++ src/frontend/src/lib/components/ui/LandingCard.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="border-bg-disabled from-bg-primary_alt to-bg-secondary flex max-w-[394px] flex-col rounded-xl border bg-gradient-to-b px-6 pt-5 pb-8"
+  class="border-bg-disabled from-bg-primary_alt to-bg-secondary flex w-[280px] flex-col rounded-xl border bg-gradient-to-b px-6 pt-5 pb-8 sm:w-[320px] lg:w-[394px]"
 >
   <p class="text-text-tertiary text-sm uppercase">{header}</p>
   <div class="grid h-[280px] place-items-center">
