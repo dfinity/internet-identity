787a4d001385777aab9e23dfa15a5da0d56c512d
Dan Blackadder <danblackadder@gmail.com>
2025-09-03 15:24:00 +0100

    Fix header for consistent size and move children outside of logo

--- src/frontend/src/lib/components/layout/Header.svelte
+++ src/frontend/src/lib/components/layout/Header.svelte
@@ -12,11 +12,11 @@
   class={["flex items-center px-3 md:px-6 lg:px-8", className]}
 >
   <!-- TODO: Revert to anchor tag when we create the landing page -->
-  <div class="flex h-16 flex-1 items-center gap-4">
+  <div class="flex min-h-16 flex-1 items-center gap-4">
     <Logo class="text-fg-primary h-5.5" />
     <h1 class="text-md text-text-primary hidden font-semibold sm:block">
       Internet Identity
     </h1>
-    {@render children?.()}
   </div>
+  {@render children?.()}
 </header>
