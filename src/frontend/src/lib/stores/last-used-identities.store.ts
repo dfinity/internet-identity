de58e3e2962abcfff355ebde92c709fdd1dd13b5
Llorenç <llorenc.muntaner@dfinity.org>
2025-10-17 09:26:18 +0200

    Revert `updateLastUsedIdentity`

--- src/frontend/src/lib/stores/last-used-identities.store.ts
+++ src/frontend/src/lib/stores/last-used-identities.store.ts
@@ -49,12 +49,6 @@ type LastUsedIdentitiesStore = Readable<{
       "identityNumber" | "name" | "authMethod" | "createdAtMillis"
     >,
   ) => void;
-  updateLastUsedIdentity: (
-    identityNumber: bigint,
-    params: Partial<
-      Pick<LastUsedIdentity, "name" | "authMethod" | "createdAtMillis">
-    >,
-  ) => void;
   addLastUsedAccount: (
     params: Omit<LastUsedAccount, "lastUsedTimestampMillis">,
   ) => void;
@@ -102,19 +96,6 @@ export const initLastUsedIdentitiesStore = (): LastUsedIdentitiesStore => {
           accounts: identity?.accounts,
           ...params,
           lastUsedTimestampMillis: Date.now(),
-        };
-        return lastUsedIdentities;
-      });
-    },
-    updateLastUsedIdentity: (identityNumber, params) => {
-      lastUsedStore.update((lastUsedIdentities) => {
-        const identity = lastUsedIdentities[identityNumber.toString()];
-        if (isNullish(identity)) {
-          return lastUsedIdentities;
-        }
-        lastUsedIdentities[identityNumber.toString()] = {
-          ...identity,
-          ...params,
         };
         return lastUsedIdentities;
       });
