db8d9b6538c733a34c941e4539b99059e6d90942
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2025-10-22 11:34:13 +0200

    Improve error screen when browser is not supported. (#3431)
    * Setup unsupported page
    
    * Improve UI
    
    * Support no redirect
    
    * Add i18n
    
    * Redirect on orphan
    
    * Improve
    
    * Improve text
    
    * Simplify unsupported page
    
    * Add spanish translations
    
    * Move unsupported within authorize path
    
    * Change E2E expect
    
    * Improve UI, fix E2E
    
    * Change redirect
    
    * Remove E2E test
    
    * Npm run extract
    
    * Fix import
    
    * Fix E2E test
    
    * Npm run extract
    
    * Use Button
    
    * Center in the middle

--- src/frontend/tests/e2e/featureFlagRerouting.test.ts
+++ src/frontend/tests/e2e/featureFlagRerouting.test.ts
@@ -1,6 +1,6 @@
 import { runInBrowser } from "./util";
 import { II_URL } from "./constants";
-import { NewAuthorizeView, AuthenticateView } from "./views";
+import { AuthenticateView } from "./views";
 
 const checkIfHasTailwind = (browser: WebdriverIO.Browser) => {
   return browser.execute(async () => {
@@ -16,26 +16,6 @@ const checkIfHasTailwind = (browser: WebdriverIO.Browser) => {
     return false;
   });
 };
-
-test("Should redirect to new-styling authenticate with feature flag and load app.css", async () => {
-  await runInBrowser(async (browser: WebdriverIO.Browser) => {
-    // Visit the root with feature flag
-    await browser.url(
-      `${II_URL}/?feature_flag_discoverable_passkey_flow=true#authorize`,
-    );
-
-    // Check that we're redirected to new-authenticate page
-    const newAuthorizeView = new NewAuthorizeView(browser);
-    await newAuthorizeView.waitForDisplay();
-
-    // Verify URL shows only "/#authorize"
-    expect(await browser.getUrl()).toBe(`${II_URL}/#authorize`);
-
-    // Check that app.css is loaded by verifying it's in the document
-    const hasTailwind = await checkIfHasTailwind(browser);
-    expect(hasTailwind).toBe(true);
-  });
-}, 300_000);
 
 test("Should show regular view without feature flag and not load app.css", async () => {
   await runInBrowser(async (browser: WebdriverIO.Browser) => {
