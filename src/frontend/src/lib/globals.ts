01c05a94c0b335dc0e26692c917c0483afa223da
sea-snake <104725312+sea-snake@users.noreply.github.com>
2025-10-24 11:18:36 +0200

    Improve sign in speed and loading feedback (#3441)
    * WIP
    
    * Show loading indicator on enabling multiple accounts.
    
    * Show loading indicator on enabling multiple accounts.

--- src/frontend/src/lib/globals.ts
+++ src/frontend/src/lib/globals.ts
@@ -13,7 +13,6 @@ import {
 import { inferHost } from "$lib/utils/iiConnection";
 import { idlFactory as internet_identity_idl } from "$lib/generated/internet_identity_idl";
 import { features } from "$lib/legacy/features";
-import { LazyHttpAgent } from "$lib/utils/lazyHttpAgent";
 
 export let canisterId: Principal;
 export let canisterConfig: InternetIdentityInit;
@@ -29,7 +28,10 @@ export const initGlobals = () => {
     shouldFetchRootKey:
       features.FETCH_ROOT_KEY || (canisterConfig.fetch_root_key[0] ?? false),
   };
-  anonymousAgent = LazyHttpAgent.createLazy(agentOptions);
+  anonymousAgent = HttpAgent.createSync(agentOptions);
+  // Fetch subnet keys to speed up queries during authentication,
+  // this avoids having to fetch them later on user interaction.
+  void anonymousAgent.fetchSubnetKeys(canisterId);
   anonymousActor = Actor.createActor<_SERVICE>(internet_identity_idl, {
     agent: anonymousAgent,
     canisterId,
