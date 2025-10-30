4c5e725d04297076adb90c704e43cb79d853967d
Llorenç Muntaner <llorenc.muntaner@dfinity.org>
2025-10-30 11:31:12 +0100

    Recovery phrase page setup (#3451)
    * Setup recovery-phrase route
    
    * Setup recovery-phrase route
    
    * CR changes
    
    * Move on enter and space
    
    * Rename
    
    * Do not use tabindex
    
    * Remove unnecessary code
    
    * Use event.code for space

---
+++ src/frontend/src/routes/(new-styling)/recovery-phrase/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import AuthPanel from "$lib/components/layout/AuthPanel.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
+  import { t } from "$lib/stores/locale.store";
+  import { nonNullish } from "@dfinity/utils";
+
+  let inputContainerRef: HTMLDivElement | null = null;
+
+  const handleKeyDownInput = (event: KeyboardEvent, currentIndex: number) => {
+    if (event.key === "Enter" || event.code === "Space") {
+      event.preventDefault();
+      const nextTabIndex = currentIndex + 1;
+      const inputElements = inputContainerRef?.querySelectorAll(
+        "input",
+      ) as NodeListOf<HTMLInputElement>;
+      const nextElement = inputElements?.[nextTabIndex];
+      if (nonNullish(nextElement)) {
+        nextElement.focus();
+      }
+    }
+  };
+</script>
+
+<div
+  class="flex flex-1 flex-row items-end justify-center sm:max-w-120 sm:items-center"
+>
+  <AuthPanel>
+    <div class="flex flex-col gap-6">
+      <div class="flex flex-col gap-3">
+        <h1 class="text-text-primary text-2xl font-medium">
+          {$t`Secure recovery`}
+        </h1>
+        <p class="text-text-tertiary text-sm">
+          {$t`Enter your recovery phrase words in each box, spelled correctly and in order.`}
+        </p>
+      </div>
+      <div class="flex flex-col gap-3">
+        <div class="grid grid-cols-3 gap-3" bind:this={inputContainerRef}>
+          {#each Array.from({ length: 24 }) as _, i}
+            <label class="relative h-8">
+              <!-- Text input -->
+              <input
+                type="text"
+                id={`recovery-phrase-${i}`}
+                on:keydown={(e) => handleKeyDownInput(e, i)}
+                class="peer text-text-primary ring-border-secondary focus:ring-border-brand h-8 w-full rounded-full border-none bg-transparent pl-10 text-base ring outline-none ring-inset focus:ring-2"
+              />
+              <!-- Left slot -->
+              <!-- Reverse order to use "peer" class to change the border color when peer is focused -->
+              <span
+                class="peer peer-focus:border-border-brand text-text-secondary border-border-secondary absolute top-0 left-0 flex h-8 w-8 items-center border-r-1 px-2 text-center text-sm font-semibold peer-focus:border-r-2"
+              >
+                {String(i + 1).padStart(2, "0")}
+              </span>
+            </label>
+          {/each}
+        </div>
+        <div class="flex flex-row gap-2">
+          <Button class="w-full" variant="tertiary">{$t`Show all`}</Button>
+          <Button class="w-full" variant="tertiary">{$t`Clear all`}</Button>
+        </div>
+      </div>
+      <Button size="xl" variant="secondary">{$t`Cancel`}</Button>
+    </div>
+  </AuthPanel>
+</div>
