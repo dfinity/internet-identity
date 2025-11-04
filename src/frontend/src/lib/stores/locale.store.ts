2bcbd92b27470b93d5278a3595a9724b64f1f9d8
sea-snake <sea-snake@outlook.com>
2025-11-04 11:45:47 +0100

    Add $formatRelative and additional comments that explains usage.

--- src/frontend/src/lib/stores/locale.store.ts
+++ src/frontend/src/lib/stores/locale.store.ts
@@ -1,5 +1,5 @@
 import { storeLocalStorageKey } from "$lib/constants/store.constants";
-import { derived, get, Readable } from "svelte/store";
+import { derived, get, readable, Readable } from "svelte/store";
 import { writableStored } from "./writable.store";
 import { building } from "$app/environment";
 import { i18n } from "@lingui/core";
@@ -72,7 +72,9 @@ export const localeStore: LocaleStore = {
   },
 };
 
-// Derives based on localeStore so that translations update on language change
+/**
+ * Usage: <span>{$t`Hello ${planet}`}</span>
+ */
 export const t = derived(
   localeStore,
   () =>
@@ -84,6 +86,16 @@ export const t = derived(
       return i18n.t(arguments[0]);
     },
 );
+
+/**
+ * Usage:
+ * <p>
+ *   {$plural(numBooks, {
+ *     one: `One ${genre} book`,
+ *     other: `# ${genre} books`
+ *   })}
+ * </p>
+ */
 export const plural = derived(
   localeStore,
   () =>
@@ -95,13 +107,63 @@ export const plural = derived(
       return i18n.t(arguments[0]);
     },
 );
+
+/**
+ * Usage: <span>{$formatDate(dateObj, { dateStyle: "short" })}</span>
+ */
 export const formatDate = derived(
   localeStore,
   () => (value: string | Date, format?: Intl.DateTimeFormatOptions) =>
     i18n.date(value, format),
 );
+
+/**
+ * Usage: <span>{$formatNumber(12345.678)}</span>
+ */
 export const formatNumber = derived(
   localeStore,
   () => (value: number, format?: Intl.NumberFormatOptions) =>
     i18n.number(value, format),
+);
+
+// Current time, used in below `formatRelative`
+const now = readable(Date.now(), (set) => {
+  const interval = setInterval(() => set(Date.now()), 1000);
+  return () => clearInterval(interval);
+});
+
+/**
+ * Usage: <span>{$formatRelative(dateObj, { style: "long" })}</span>
+ */
+export const formatRelative = derived(
+  [localeStore, now],
+  ([locale, now]) =>
+    (
+      value: string | Date,
+      format?: Intl.RelativeTimeFormatOptions,
+      {
+        units = [
+          { unit: "year", ms: 1000 * 60 * 60 * 24 * 365 },
+          { unit: "month", ms: 1000 * 60 * 60 * 24 * 30 },
+          { unit: "day", ms: 1000 * 60 * 60 * 24 },
+          { unit: "hour", ms: 1000 * 60 * 60 },
+          { unit: "minute", ms: 1000 * 60 },
+          { unit: "second", ms: 1000 },
+        ],
+      } = {},
+    ) => {
+      const diffMs = new Date(value).getTime() - now;
+      const rtf = new Intl.RelativeTimeFormat(locale, format);
+
+      // Pick unit within threshold
+      for (const { unit, ms } of units) {
+        if (Math.abs(diffMs) >= ms || unit === "second") {
+          const value = Math.round(diffMs / ms);
+          return rtf.format(value, unit as Intl.RelativeTimeFormatUnit);
+        }
+      }
+
+      // Fallback
+      return i18n.date(value);
+    },
 );
