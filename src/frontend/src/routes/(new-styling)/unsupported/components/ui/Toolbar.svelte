0f3064aa32a6b3762f70d7b5e16fa9d51090463e
sea-snake <sea-snake@outlook.com>
2025-11-27 17:34:05 +0100

    Custom unsupported page for users trying to sign in from within X.

---
+++ src/frontend/src/routes/(new-styling)/unsupported/components/ui/Toolbar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { SvelteHTMLElements } from "svelte/elements";
+
+  const {
+    children,
+    class: className,
+    ...props
+  }: SvelteHTMLElements["div"] = $props();
+</script>
+
+<div
+  {...props}
+  class={[
+    "bg-surface-light-50 dark:bg-surface-dark-900 pointer-events-none flex flex-row gap-2 rounded-xl p-2.5",
+    className,
+  ]}
+>
+  {@render children?.()}
+</div>
---
+++ src/frontend/src/routes/(new-styling)/unsupported/components/ui/ToolbarHighlight.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { SvelteHTMLElements } from "svelte/elements";
+
+  const {
+    children,
+    class: className,
+    ...props
+  }: SvelteHTMLElements["div"] = $props();
+</script>
+
+<div {...props} class={["relative", className]}>
+  {@render children?.()}
+  <div
+    class="absolute -inset-[5px] rounded-full border-[1px] border-blue-700 dark:border-blue-300"
+  ></div>
+  <div
+    class="absolute -inset-[10px] rounded-full border-[0.75px] border-blue-500 dark:border-blue-500"
+  ></div>
+  <div
+    class="absolute -inset-[14px] rounded-full border-[0.5px] border-blue-300 dark:border-blue-700"
+  ></div>
+  <div
+    class="absolute -inset-[18px] rounded-full border-[0.25px] border-blue-200 dark:border-blue-800"
+  ></div>
+</div>
