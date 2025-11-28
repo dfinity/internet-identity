1622a785f2644e003a32d78ba8c2ed62c07471e0
sea-snake <sea-snake@outlook.com>
2025-11-28 18:18:07 +0100

    Redirect to landing page when unsupported page is visited directly.

--- src/frontend/src/routes/(new-styling)/unsupported/+page.svelte
+++ src/frontend/src/routes/(new-styling)/unsupported/+page.svelte
@@ -1,8 +1,43 @@
 <script lang="ts">
   import XBrowser from "./components/views/XBrowser.svelte";
   import Generic from "./components/views/Generic.svelte";
+  import { afterNavigate, beforeNavigate, goto } from "$app/navigation";
+  import type { AfterNavigate, BeforeNavigate } from "@sveltejs/kit";
 
   const isX = /\bTwitter/i.test(navigator.userAgent);
+
+  /**
+   * Store boolean in session storage if user leaves the page,
+   * this could be due to reload or closing the tab/window.
+   *
+   * Session storage is tied to the tab/window,
+   * so effectively it's only ever set on reload.
+   */
+  const trackPageReload = (navigation: BeforeNavigate) => {
+    const isReload = navigation.type === "leave";
+    if (!isReload) {
+      return;
+    }
+    sessionStorage.setItem("reloaded", "true");
+  };
+  /**
+   * Redirect to landing page when this page is visited directly,
+   * except if the page was reloaded after internal navigation.
+   */
+  const redirectOnEntryExceptReload = (navigation: AfterNavigate) => {
+    const isEntry = navigation.type === "enter";
+    const isReload = sessionStorage.getItem("reloaded") === "true";
+
+    sessionStorage.removeItem("reloaded"); // Always cleanup after it's read
+
+    if (!isEntry || isReload) {
+      return;
+    }
+    goto("/", { replaceState: true });
+  };
+
+  beforeNavigate(trackPageReload);
+  afterNavigate(redirectOnEntryExceptReload);
 </script>
 
 {#if isX}
