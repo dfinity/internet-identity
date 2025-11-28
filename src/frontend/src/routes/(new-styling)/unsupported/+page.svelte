0442e1de2452f775b2cab606e7a309cd13d47ec4
sea-snake <sea-snake@outlook.com>
2025-11-28 14:18:20 +0100

    Rename X to XBrowser for clarity based on feedback.

--- src/frontend/src/routes/(new-styling)/unsupported/+page.svelte
+++ src/frontend/src/routes/(new-styling)/unsupported/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import X from "./components/views/X.svelte";
+  import XBrowser from "./components/views/XBrowser.svelte";
   import Generic from "./components/views/Generic.svelte";
 
   const isX = /\bTwitter/i.test(navigator.userAgent);
 </script>
 
 {#if isX}
-  <X />
+  <XBrowser />
 {:else}
   <Generic />
 {/if}
