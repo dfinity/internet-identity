8e13c4299d4d648c49b3eb2b7fdae00c1e4d7389
sea-snake <sea-snake@outlook.com>
2025-12-01 17:51:08 +0100

    Refactor recovery phrase e2e and add more test cases.

--- src/frontend/tests/e2e-playwright/fixtures/index.ts
+++ src/frontend/tests/e2e-playwright/fixtures/index.ts
@@ -1,5 +1,5 @@
 import { mergeTests } from "@playwright/test";
 import { test as identityTest } from "./identity";
-import { test as recoveryPhrasePageTest } from "./recovery-phrase-page";
+import { test as recoveryPhrasePageTest } from "./recoveryPhrasePage";
 
 export const test = mergeTests(identityTest, recoveryPhrasePageTest);
