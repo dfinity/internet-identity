efaabc8460f298e2763998097ca0de6f81313a28
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2025-12-10 22:36:25 +0100

    third

--- src/internet_identity_frontend/src/main.rs
+++ src/internet_identity_frontend/src/main.rs
@@ -1,11 +1,182 @@
+use ic_asset_certification::{
+    Asset, AssetConfig, AssetEncoding, AssetFallbackConfig, AssetMap, AssetRedirectKind,
+    AssetRouter,
+};
+use ic_cdk::{init, post_upgrade};
 use ic_cdk_macros::query;
-use internet_identity_interface::http_gateway::{HttpRequest, HttpResponse};
+use ic_http_certification::{
+    utils::add_v2_certificate_header, DefaultCelBuilder, HeaderField, HttpCertification,
+    HttpCertificationPath, HttpCertificationTree, HttpCertificationTreeEntry, HttpRequest,
+    HttpResponse, StatusCode, CERTIFICATE_EXPRESSION_HEADER_NAME,
+};
+use include_dir::{include_dir, Dir};
+use std::{cell::RefCell, rc::Rc};
 
-mod http;
+thread_local! {
+    static HTTP_TREE: Rc<RefCell<HttpCertificationTree>> = Default::default();
+
+    // initializing the asset router with an HTTP certification tree is optional.
+    // if direct access to the HTTP certification tree is not needed for certifying
+    // requests and responses outside of the asset router, then this step can be skipped
+    // and the asset router can be initialized like so:
+    // ```
+    // static ASSET_ROUTER: RefCell<AssetRouter<'static>> = Default::default();
+    // ```
+    static ASSET_ROUTER: RefCell<AssetRouter<'static>> = RefCell::new(AssetRouter::with_tree(HTTP_TREE.with(|tree| tree.clone())));
+}
+
+static ASSETS_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../../dist");
+const IMMUTABLE_ASSET_CACHE_CONTROL: &str = "public, max-age=31536000, immutable";
+const NO_CACHE_ASSET_CACHE_CONTROL: &str = "public, no-cache, no-store";
+
+// Public methods
+#[init]
+fn init() {
+    certify_all_assets();
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    certify_all_assets();
+}
+
+fn certify_all_assets() {
+    // 1. Define the asset certification configurations.
+    let encodings = vec![
+        AssetEncoding::Brotli.default_config(),
+        AssetEncoding::Gzip.default_config(),
+    ];
+
+    let asset_configs = vec![
+        AssetConfig::File {
+            path: "index.html".to_string(),
+            content_type: Some("text/html".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                NO_CACHE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            fallback_for: vec![AssetFallbackConfig {
+                scope: "/".to_string(),
+                status_code: Some(StatusCode::OK),
+            }],
+            aliased_by: vec!["/".to_string()],
+            encodings: encodings.clone(),
+        },
+        AssetConfig::Pattern {
+            pattern: "**/*.js".to_string(),
+            content_type: Some("text/javascript".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                IMMUTABLE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            encodings: encodings.clone(),
+        },
+        AssetConfig::Pattern {
+            pattern: "**/*.css".to_string(),
+            content_type: Some("text/css".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                IMMUTABLE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            encodings,
+        },
+        AssetConfig::Pattern {
+            pattern: "**/*.ico".to_string(),
+            content_type: Some("image/x-icon".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                IMMUTABLE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            encodings: vec![],
+        },
+        AssetConfig::Redirect {
+            from: "/old-url".to_string(),
+            to: "/".to_string(),
+            kind: AssetRedirectKind::Permanent,
+            headers: get_asset_headers(vec![
+                ("content-type".to_string(), "text/plain".to_string()),
+                (
+                    "cache-control".to_string(),
+                    NO_CACHE_ASSET_CACHE_CONTROL.to_string(),
+                ),
+            ]),
+        },
+    ];
+
+    // 2. Collect all assets from the frontend build directory.
+    let mut assets = Vec::new();
+    collect_assets(&ASSETS_DIR, &mut assets);
+
+    // 3. Skip certification for the metrics endpoint.
+    HTTP_TREE.with(|tree| {
+        let mut tree = tree.borrow_mut();
+
+        let metrics_tree_path = HttpCertificationPath::exact("/metrics");
+        let metrics_certification = HttpCertification::skip();
+        let metrics_tree_entry =
+            HttpCertificationTreeEntry::new(metrics_tree_path, metrics_certification);
+
+        tree.insert(&metrics_tree_entry);
+    });
+
+    ASSET_ROUTER.with_borrow_mut(|asset_router| {
+        // 4. Certify the assets using the `certify_assets` function from the `ic-asset-certification` crate.
+        if let Err(err) = asset_router.certify_assets(assets, asset_configs) {
+            ic_cdk::trap(&format!("Failed to certify assets: {}", err));
+        }
+
+        // 5. Set the canister's certified data.
+        ic_cdk::api::set_certified_data(&asset_router.root_hash());
+    });
+}
+
+fn get_asset_headers(additional_headers: Vec<HeaderField>) -> Vec<HeaderField> {
+    // set up the default headers and include additional headers provided by the caller
+    let mut headers = vec![
+        ("strict-transport-security".to_string(), "max-age=31536000; includeSubDomains".to_string()),
+        ("x-frame-options".to_string(), "DENY".to_string()),
+        ("x-content-type-options".to_string(), "nosniff".to_string()),
+        ("content-security-policy".to_string(), "default-src 'self'; img-src 'self' data:; form-action 'self'; object-src 'none'; frame-ancestors 'none'; upgrade-insecure-requests; block-all-mixed-content".to_string()),
+        ("referrer-policy".to_string(), "no-referrer".to_string()),
+        ("permissions-policy".to_string(), "accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()".to_string()),
+        ("cross-origin-embedder-policy".to_string(), "require-corp".to_string()),
+        ("cross-origin-opener-policy".to_string(), "same-origin".to_string()),
+    ];
+    headers.extend(additional_headers);
+
+    headers
+}
+
+/// Recursively collect all assets from the provided directory
+fn collect_assets<'content, 'path>(
+    dir: &'content Dir<'path>,
+    assets: &mut Vec<Asset<'content, 'path>>,
+) {
+    for file in dir.files() {
+        assets.push(Asset::new(file.path().to_string_lossy(), file.contents()));
+    }
+
+    for dir in dir.dirs() {
+        collect_assets(dir, assets);
+    }
+}
 
 #[query]
 fn http_request(req: HttpRequest) -> HttpResponse {
-    http::http_request(req)
+    serve_asset(&req)
+}
+
+fn serve_asset(req: &HttpRequest) -> HttpResponse<'static> {
+    ASSET_ROUTER.with_borrow(|asset_router| {
+        if let Ok(response) = asset_router.serve_asset(
+            &ic_cdk::api::data_certificate().expect("No data certificate available"),
+            req,
+        ) {
+            response
+        } else {
+            ic_cdk::trap("Failed to serve asset");
+        }
+    })
 }
 
 fn main() {}
