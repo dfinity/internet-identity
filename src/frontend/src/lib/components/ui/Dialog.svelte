c489730bd6e80fee24aa4eb0383a50c754737a27
sea-snake <104725312+sea-snake@users.noreply.github.com>
2025-12-10 13:06:56 +0100

    Fix scroll and dimensions of Dialog component on smaller screens (mobile) (#3532)
    Fix scroll and dimensions of Dialog component on smaller screens
    (mobile)

--- src/frontend/src/lib/components/ui/Dialog.svelte
+++ src/frontend/src/lib/components/ui/Dialog.svelte
@@ -3,7 +3,6 @@
   import { scale, fly } from "svelte/transition";
   import type { HTMLAttributes } from "svelte/elements";
   import { nonNullish } from "@dfinity/utils";
-  import Button from "$lib/components/ui/Button.svelte";
   import { XIcon } from "@lucide/svelte";
   import { t } from "$lib/stores/locale.store";
 
@@ -25,6 +24,7 @@
   }: Props = $props();
 
   let dialogRef: HTMLDialogElement;
+  let contentRef: HTMLDivElement;
   let onCancel = (e: Event): void => {
     e.preventDefault();
     onClose?.();
@@ -56,7 +56,29 @@
           "--keyboard-inset-height",
           `${Math.max(window.innerHeight - window.visualViewport!.height, 0)}px`,
         );
+        dialogRef.style.setProperty(
+          "--max-content-height",
+          `${window.visualViewport!.height}px`,
+        );
       }, 100);
+    };
+    // If the API is not supported (e.g. iOS), prevent page scrolling
+    // and implement custom touch scroll handlers for dialog content.
+    const preventScroll = (event: TouchEvent) => {
+      event.preventDefault();
+    };
+    let lastY = 0;
+    const touchScrollStart = (event: TouchEvent) => {
+      lastY = event.touches[0].clientY;
+    };
+    const touchScrollMove = (event: TouchEvent) => {
+      const y = event.touches[0].clientY;
+      const dy = y - lastY;
+      lastY = y;
+      contentRef.scrollTo({
+        top: contentRef.scrollTop - dy,
+        behavior: "instant",
+      });
     };
     if ("virtualKeyboard" in navigator) {
       (
@@ -65,6 +87,15 @@
     } else {
       window.visualViewport?.addEventListener("resize", updateKeyboardInset);
       window.visualViewport?.addEventListener("scroll", updateKeyboardInset);
+      document.documentElement.addEventListener("touchmove", preventScroll, {
+        passive: false,
+      });
+      contentRef.addEventListener("touchstart", touchScrollStart, {
+        passive: false,
+      });
+      contentRef.addEventListener("touchmove", touchScrollMove, {
+        passive: false,
+      });
     }
     return () => {
       if ("virtualKeyboard" in navigator) {
@@ -80,6 +111,12 @@
           "scroll",
           updateKeyboardInset,
         );
+        document.documentElement.removeEventListener(
+          "touchmove",
+          preventScroll,
+        );
+        contentRef.removeEventListener("touchstart", touchScrollStart);
+        contentRef.removeEventListener("touchmove", touchScrollMove);
       }
     };
   });
@@ -91,14 +128,14 @@
   closedby={closeOnOutsideClick ? "any" : "none"}
   class={[
     // Layout base/dialog/bottomsheet
-    "fixed flex max-w-full flex-col overflow-hidden bg-transparent outline-none",
+    "fixed flex min-h-max max-w-full flex-col bg-transparent outline-none",
     "sm:m-auto sm:w-100",
-    "w-full max-sm:top-auto max-sm:bottom-0",
+    "max-sm:top-auto max-sm:bottom-0 max-sm:w-full",
     // Backdrop base/visible
     "backdrop:bg-bg-overlay backdrop:opacity-0 backdrop:transition-opacity backdrop:duration-200",
     backdrop && "[&[data-visible]]:backdrop:opacity-80",
   ]}
-  style="--keyboard-inset-height: env(keyboard-inset-height);"
+  style="--keyboard-inset-height: env(keyboard-inset-height);--max-content-height: calc(100dvh - var(--keyboard-inset-height))"
   transition:transitionFn|global
   onoutrostart={fadeOutBackDrop}
   {...props}
@@ -106,9 +143,9 @@
   <div
     class={[
       // Container base/dialog/bottomsheet
-      "bg-bg-primary_alt border-border-secondary relative flex max-h-screen flex-col overflow-hidden dark:sm:border",
-      "sm:m-auto sm:w-100 sm:rounded-2xl sm:px-6 sm:pt-6 sm:pb-8",
-      "w-full rounded-t-2xl px-4 pt-4 pb-6",
+      "bg-bg-primary_alt border-border-secondary relative flex flex-col overflow-hidden dark:sm:border",
+      "min-h-max sm:m-auto sm:w-100 sm:rounded-2xl",
+      "w-full rounded-t-2xl",
       className,
     ]}
   >
@@ -118,31 +155,36 @@
     >
       <div class="bg-bg-primary_alt h-24 rounded-t-2xl"></div>
     </div>
-    <div class="relative flex flex-1 flex-col">
-      {@render children?.()}
+    <div class="flex flex-1 flex-col">
+      <div
+        bind:this={contentRef}
+        class="relative max-h-[var(--max-content-height)] overflow-y-auto"
+      >
+        <div
+          class="flex flex-1 shrink-0 flex-col px-4 pt-4 pb-4 sm:px-6 sm:pt-6 sm:pb-8"
+        >
+          {@render children?.()}
+          {#if showCloseButton && nonNullish(onClose)}
+            <button
+              class="btn btn-tertiary btn-lg btn-icon absolute end-2 top-2 z-2 !rounded-full"
+              onclick={onClose}
+              aria-label={$t`Close`}
+            >
+              <XIcon class="size-5" aria-hidden="true" />
+            </button>
+          {/if}
+        </div>
+      </div>
       <!-- Element that pushes bottom sheet away from mobile keyboard or gesture navigation -->
       <div class="flex sm:hidden">
         <div class="h-[var(--keyboard-inset-height)]"></div>
         <div class="h-[env(safe-area-inset-bottom)]"></div>
       </div>
     </div>
-    {#if showCloseButton && nonNullish(onClose)}
-      <Button
-        variant="tertiary"
-        size="lg"
-        iconOnly
-        type="button"
-        class="absolute top-2 right-2 z-2 !rounded-full"
-        onclick={onClose}
-        aria-label={$t`Close`}
-      >
-        <XIcon class="size-5" aria-hidden="true" />
-      </Button>
-    {/if}
-  </div>
-  <!-- Element that pushes dialog away from mobile keyboard or gesture navigation -->
-  <div class="flex max-sm:hidden">
-    <div class="h-[var(--keyboard-inset-height)]"></div>
-    <div class="h-[env(safe-area-inset-bottom)]"></div>
+    <!-- Element that pushes dialog away from mobile keyboard or gesture navigation -->
+    <div class="flex max-sm:hidden">
+      <div class="h-[var(--keyboard-inset-height)]"></div>
+      <div class="h-[env(safe-area-inset-bottom)]"></div>
+    </div>
   </div>
 </dialog>
