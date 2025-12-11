3dafa639391646794c2e08b5fdfa0c5f86eb72e6
sea-snake <sea-snake@outlook.com>
2025-12-11 13:11:26 +0100

    Add navigation link to recovery in dashboard.

---
+++ src/frontend/src/routes/(new-styling)/manage/(authenticated)/(access-and-recovery)/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { LayoutProps } from "./$types";
+  import { t } from "$lib/stores/locale.store";
+  import Badge from "$lib/components/ui/Badge.svelte";
+  import { page } from "$app/state";
+
+  const { children }: LayoutProps = $props();
+</script>
+
+<nav class="tabs mb-8">
+  <a
+    href="/manage/access"
+    class={["tab", page.url.pathname === "/manage/access" && "tab-selected"]}
+  >
+    <span>{$t`Access methods`}</span>
+    <Badge size="sm">2</Badge>
+  </a>
+  <a
+    href="/manage/recovery"
+    class={["tab", page.url.pathname === "/manage/recovery" && "tab-selected"]}
+  >
+    {$t`Recovery phrase`}
+  </a>
+</nav>
+
+{@render children()}
