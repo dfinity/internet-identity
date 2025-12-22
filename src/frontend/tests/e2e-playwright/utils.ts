b31232ff3b83d020895f6112c1a44a6d78a370bf
sea-snake <sea-snake@outlook.com>
2025-12-22 10:10:58 +0100

    Fix e2e tests.

--- src/frontend/tests/e2e-playwright/utils.ts
+++ src/frontend/tests/e2e-playwright/utils.ts
@@ -241,12 +241,8 @@ export const removePasskey = async (
 };
 
 export const signOut = async (page: Page): Promise<void> => {
-  const menuButton = page.getByRole("button", { name: "Open menu" });
-  if (await menuButton.isVisible()) {
-    await menuButton.click();
-  }
-  await page.getByLabel("Switch identity").click();
-  await page.getByRole("button", { name: "Sign Out" }).click();
+  // Navigating to landing page (reloading) is sufficient for now to sign out
+  await page.goto(II_URL);
 };
 
 /**
