5e61878b268f157bc6db194cba2fa174487fdccf
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2026-01-07 00:23:14 +0100

    Fall back to reading from legacy memory if new anchor memory is unset

--- src/internet_identity/src/storage.rs
+++ src/internet_identity/src/storage.rs
@@ -745,10 +745,35 @@ impl<M: Memory + Clone> Storage<M> {
         Ok(())
     }
 
+    /// Reads the data of the specified anchor from legacy stable memory.
+    fn read_legacy(&self, anchor_number: AnchorNumber) -> Result<Anchor, StorageError> {
+        // Read fixed 4KB anchor
+        let record_number = self.anchor_number_to_record_number(anchor_number)?;
+
+        let address = self.record_address(record_number);
+
+        let mut reader = Reader::new(&self.anchor_memory, address);
+        let mut buf = vec![0; self.header.entry_size as usize];
+
+        reader.read_exact(&mut buf).expect("failed to read memory");
+
+        // Anchors that are allocated but have never been written to, due to a previously missing
+        // allocation cleanup implementation, are handled as if they don't exist.
+        if buf.iter().all(|&b| b == 0) {
+            return Err(StorageError::AnchorNotFound { anchor_number });
+        }
+
+        // Read unbounded stable structures anchor
+        let storable_fixed_anchor = StorableFixedAnchor::from_bytes(Cow::Owned(buf));
+
+        Ok(Anchor::from((anchor_number, storable_fixed_anchor, None)))
+    }
+
     /// Reads the data of the specified anchor from stable memory.
     pub fn read(&self, anchor_number: AnchorNumber) -> Result<Anchor, StorageError> {
         let record_number = self.anchor_number_to_record_number(anchor_number)?;
 
+        // This value is no longer used for reading, but we keep the check for consistency.
         let num_anchors = self.header.num_anchors;
 
         if record_number >= num_anchors {
@@ -763,7 +788,11 @@ impl<M: Memory + Clone> Storage<M> {
         }
 
         let Some(storable_anchor) = self.stable_anchor_memory.get(&anchor_number) else {
-            return Err(StorageError::AnchorNotFound { anchor_number });
+            ic_cdk::println!(
+                "Falling back to legacy read for anchor number {}",
+                anchor_number
+            );
+            return self.read_legacy(anchor_number);
         };
 
         Ok(Anchor::from((anchor_number, storable_anchor)))
