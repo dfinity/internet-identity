1d89da8ccbe63914c86c6e31c1f9e81bfe6120bc
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2026-02-12 15:47:19 +0100

    first

--- src/frontend/src/lib/legacy/callback-flow.ts
+++ src/frontend/src/lib/legacy/callback-flow.ts
@@ -4,7 +4,6 @@ import { createSpa } from "./spa";
 
 void createSpa((connection) => {
   initAnalytics(connection.canisterConfig.analytics_config[0]?.[0]);
-  analytics.pageView();
   analytics.event("page-redirect-callback");
   return callbackFlow();
 });
