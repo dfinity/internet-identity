eecea35302ea269616260588983ce2b2c1e32200
sea-snake <sea-snake@outlook.com>
2026-02-13 15:23:42 +0100

    Remove unnecessary comments from DirectOpenIdFunnel.ts

--- src/frontend/src/lib/utils/analytics/DirectOpenIdFunnel.ts
+++ src/frontend/src/lib/utils/analytics/DirectOpenIdFunnel.ts
@@ -1,42 +1,5 @@
 import { Funnel } from "./Funnel";
 
-/**
- * Registration flow events:
- *
- * Square brackets [] indicate optional events.
- *
- * Authentication flow events:
- *    continue-as-screen
- *      use-another
- *        -> Move to select-method-screen
- *      continue-as-passkey
- *        auth-success
- *        go-to-dashboard
- *      continue-as-openid
- *        auth-success
- *        go-to-dashboard
- *    select-method-screen
- *      continue-with-openid
- *        register-with-openid
- *          successful-openid-registration
- *            auth-success
- *          jwt-verification-failed
- *          jwt-verification-expired
- *        login-with-openid
- *          auth-success
- *          jwt-verification-failed
- *          jwt-verification-expired
- *      continue-with-passkey-screen
- *        enter-name-screen
- *          start-webauthn-creation
- *            register-with-passkey
- *              successful-passkey-registration
- *                auth-success
- *                go-to-dashboard
- *        use-existing-passkey
- *          auth-success
- *          go-to-dashboard
- */
 export const DirectOpenIdEvents = {
   RedirectToOpenId: "redirect-to-openid",
   CallbackFromOpenId: "callback-from-openid",
