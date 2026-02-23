2ffc2fa1ef8712b380380828a64d2a4a5ae9c74a
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2026-02-23 14:59:18 +0100

    third

--- src/internet_identity_frontend/src/main.rs
+++ src/internet_identity_frontend/src/main.rs
@@ -424,4 +424,28 @@ fn http_request(request: HttpRequest) -> HttpResponse {
     })
 }
 
+// Order dependent: do not move above any exposed canister method!
+candid::export_service!();
+
+#[cfg(test)]
+mod test {
+    use crate::__export_service;
+    use candid_parser::utils::{service_equal, CandidSource};
+    use std::path::Path;
+
+    /// Checks candid interface type equality by making sure that the service in the did file is
+    /// a subtype of the generated interface and vice versa.
+    #[test]
+    fn check_candid_interface_compatibility() {
+        let canister_interface = __export_service();
+        service_equal(
+            CandidSource::Text(&canister_interface),
+            CandidSource::File(Path::new("internet_identity_frontend.did")),
+        )
+        .unwrap_or_else(|e| {
+            panic!("the canister code interface is not equal to the did file: {e:?}")
+        });
+    }
+}
+
 fn main() {}
