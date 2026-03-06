78a9556f30e532e23319c0a1fdc15b28a8d480e3
sea-snake <sea-snake@outlook.com>
2026-03-06 15:55:34 +0100

    Always show identity switcher

--- src/frontend/src/routes/(new-styling)/manage/(authenticated)/+layout.svelte
+++ src/frontend/src/routes/(new-styling)/manage/(authenticated)/+layout.svelte
@@ -331,7 +331,7 @@
       <button
         bind:this={identityButtonRef}
         onclick={() => (isIdentityPopoverOpen = true)}
-        class="btn btn-tertiary gap-2.5 pr-3 max-sm:hidden sm:-mr-3"
+        class="btn btn-tertiary gap-2.5 pr-3 sm:-mr-3"
         aria-label={$t`Switch identity`}
       >
         <Avatar size="xs">
