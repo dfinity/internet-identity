22e2314bdde6ba415ca8098df6ce1eb0660bbef8
sea-snake <sea-snake@outlook.com>
2026-04-15 13:26:04 +0200

    Manually debug and fix

--- src/frontend/src/routes/(new-styling)/(pending-channel)/+layout.svelte
+++ src/frontend/src/routes/(new-styling)/(pending-channel)/+layout.svelte
@@ -2,6 +2,7 @@
   import type { LayoutProps } from "./$types";
   import { channelErrorStore, channelStore } from "$lib/stores/channelStore";
   import { goto } from "$app/navigation";
+  import ChannelError from "$lib/components/ui/ChannelError.svelte";
 
   const { children }: LayoutProps = $props();
   channelStore.establish({ pending: true });
@@ -13,6 +14,8 @@
   });
 </script>
 
-{#if $channelStore !== undefined}
+{#if $channelErrorStore !== undefined && $channelErrorStore !== "unsupported-browser"}
+  <ChannelError error={$channelErrorStore} />
+{:else if $channelStore !== undefined}
   {@render children()}
 {/if}
--- src/frontend/src/routes/(new-styling)/(resuming-channel)/+layout.svelte
+++ src/frontend/src/routes/(new-styling)/(resuming-channel)/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
   import type { LayoutProps } from "./$types";
   import { channelErrorStore, channelStore } from "$lib/stores/channelStore";
-  import Dialog from "$lib/components/ui/Dialog.svelte";
-  import { t } from "$lib/stores/locale.store";
-  import FeaturedIcon from "$lib/components/ui/FeaturedIcon.svelte";
-  import { CircleAlertIcon, RotateCcwIcon } from "@lucide/svelte";
-  import Button from "$lib/components/ui/Button.svelte";
+  import ChannelError from "$lib/components/ui/ChannelError.svelte";
   import { goto } from "$app/navigation";
 
   const pendingChannelOrigin = sessionStorage.getItem(
@@ -25,22 +21,8 @@
   });
 </script>
 
-{#if pendingChannelOrigin !== null && $channelStore !== undefined}
+{#if $channelStore !== undefined}
   {@render children()}
-{:else}
-  <Dialog>
-    <FeaturedIcon size="lg" variant="error" class="mb-4 self-start">
-      <CircleAlertIcon class="size-6" />
-    </FeaturedIcon>
-    <h1 class="text-text-primary mb-3 text-2xl font-medium">
-      {$t`Connection closed`}
-    </h1>
-    <p class="text-text-tertiary mb-6 text-base font-medium">
-      {$t`It seems like the connection with the service could not be re-established.`}
-    </p>
-    <Button onclick={() => window.close()} variant="secondary">
-      <RotateCcwIcon class="size-4" />
-      <span>{$t`Return to app`}</span>
-    </Button>
-  </Dialog>
+{:else if $channelErrorStore !== undefined || pendingChannelOrigin === null}
+  <ChannelError error={$channelErrorStore ?? "connection-closed"} />
 {/if}
